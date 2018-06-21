701db76e8b4c703f7034c1cc475fd62cd7f946fa
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-21 16:38:28 -0500

    Created Application.testable(services:environment:environmentArguments:) method

---
+++ Tests/PayPalTests/Application+Testable.swift
@@ -0,0 +1,20 @@
+import Vapor
+
+extension Application {
+    static func testable(
+        services: Services = .default()
+        environment: Environment = .testing,
+        environmentArguments: [String]? = nil
+    )throws -> Application {
+        let config = Config.default()
+        var env = environment
+        
+        if let arguments = environmentArguments {
+            env.arguments = arguments
+        } else {
+            env.arguments = ["vapor", "serve"]
+        }
+        
+        return try Application(config: config, environment: env, services: services)
+    }
+}
--- Tests/PayPalTests/ProviderTests.swift
+++ Tests/PayPalTests/ProviderTests.swift
@@ -12,14 +12,10 @@ final class ProviderTests: XCTestCase {
         setenv("PAYPAL_CLIENT_ID", "fake_paypal_id", 1)
         setenv("PAYPAL_CLIENT_SECRET", "fake_paypal_secret", 1)
         
-        let config = Config.default()
         var services = Services.default()
-        
-        environment.arguments = ["vapor", "serve"]
         try! services.register(PayPal.Provider())
         
-        let app = try! Application(config: config, environment: environment, services: services)
-        self.app = app
+        self.app = try! Application.testable(services: services, environment: self.environment)
     }
     
     func testBootSucceeds()throws {
