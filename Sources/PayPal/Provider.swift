37641c9baaef6159094a439f03f266cd2c249ed1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-21 15:46:17 -0500

    Created Configuration class

--- Sources/PayPal/Provider.swift
+++ Sources/PayPal/Provider.swift
@@ -9,3 +9,26 @@ public final class Provider: Vapor.Provider {
         return container.future()
     }
 }
+
+/// Global configuration data required to use the PayPal API.
+public final class Configuration: Service {
+    
+    /// Your PayPal client ID.
+    public let id: String
+    
+    /// Your PayPal client secret value.
+    public let secret: String
+    
+    /// The PayPal environment to send requests to.
+    /// This value is based on the app current environment.
+    ///
+    /// If the app was boot in the a release environment, it will
+    /// be `.production`, otherwise it will be `.sandbox`.
+    public let environment: PayPal.Environment
+    
+    init(id: String, secret: String, environment: Environment) {
+        self.id = id
+        self.secret = secret
+        self.environment = environment
+    }
+}
