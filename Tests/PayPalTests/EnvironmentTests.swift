f2df562fb960a59a87e8638c040666199b90376e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-06-21 15:08:25 -0500

    Created EnvironmentTests.testCaseDomains test case

--- Tests/PayPalTests/EnvironmentTests.swift
+++ Tests/PayPalTests/EnvironmentTests.swift
@@ -7,8 +7,14 @@ final class EnvironmentTests: XCTestCase {
         XCTAssertEqual(Environment.sandbox.rawValue, "sandbox")
     }
     
+    func testCaseDomains() {
+        XCTAssertEqual(Environment.production.domain, "https://api.paypal.com")
+        XCTAssertEqual(Environment.sandbox.domain, "https://api.sandbox.paypal.com")
+    }
+    
     static var allTests: [(String, (EnvironmentTests) -> ()throws -> ())] = [
-        ("testCaseRawValues", testCaseRawValues)
+        ("testCaseRawValues", testCaseRawValues),
+        ("testCaseDomains", testCaseDomains)
     ]
 }
 
