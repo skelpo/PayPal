3a2e5a3490b431f3bd098e385c42857de33c24e8
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-09 15:50:24 -0600

    Documented QueryParamaters initializer method. Rewrote struct doc block

--- Sources/PayPal/API/QueryParameters.swift
+++ Sources/PayPal/API/QueryParameters.swift
@@ -1,9 +1,13 @@
 import Vapor
 
-/// All valid query string parameters for any given PayPal API request.
+/// For most REST `GET` calls, you can specify one or more optional query parameters
+/// on the request URI to [filter](https://developer.paypal.com/docs/api/glossary/#filtering),
+/// [limit the size of](https://developer.paypal.com/docs/api/glossary/#pagination),
+/// and sort the data in an API response. For filter parameters, see the individual GET calls.
 ///
-/// - Note: Not all query strings will effect a request.
-///   Some pagination paramaters will not effect certain endpoints.
+/// To limit, or _page_, and sort the data that is returned in some API responses, use these, or similar, query parameters:
+///
+/// - Note: Not all pagination parameters are available for all APIs.
 public struct QueryParamaters: Content {
     
     /// The number of items to list in the response.
@@ -44,6 +48,36 @@ public struct QueryParamaters: Content {
     /// in [Internet date and time format](https://tools.ietf.org/html/rfc3339#section-5.6).
     /// For example, `start_time=2016-03-06T11:00:00Z`.
     public var startTime: String?
+    
+    /// Creates a new `QueryParamaters` instance
+    ///
+    /// All paramaters have a default `nil` value, so you only need to pass
+    /// in the values that you need.
+    ///
+    ///     QueryParamaters(page: 0, pageSize: 25, sortOrder: .ascending)
+    public init(
+        count: Int? = nil,
+        endTime: String? = nil,
+        page: Int? = nil,
+        pageSize: Int? = nil,
+        totalCountRequired: Bool? = nil,
+        sortBy: String?,
+        sortOrder: ResponseSortOrder? = nil,
+        startID: String? = nil,
+        startIndex: Int? = nil,
+        startTime: String? = nil
+    ) {
+        self.count = count
+        self.endTime = endTime
+        self.page = page
+        self.pageSize = pageSize
+        self.totalCountRequired = totalCountRequired
+        self.sortBy = sortBy
+        self.sortOrder = sortOrder
+        self.startID = startID
+        self.startIndex = startIndex
+        self.startTime = startTime
+    }
 }
 
 /// To specify which order the items in a response should be received from the PayPal API.
@@ -52,6 +86,6 @@ public enum ResponseSortOrder: String, Codable {
     ///
     case ascending
     
-    /// 
+    ///
     case descending
 }
