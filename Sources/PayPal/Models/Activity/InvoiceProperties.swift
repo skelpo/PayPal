7f6c66ba8bf5cc3fe7919de1802e74373d4dfa69
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-10 17:06:08 -0500

    Created InvoiceProperties class

---
+++ Sources/PayPal/Models/Activity/InvoiceProperties.swift
@@ -0,0 +1,19 @@
+import Vapor
+
+/// Additional information about an invoice activity object.
+public final class InvoiceProperties: Content {
+    
+    /// The invoice role.
+    public var role: Role?
+    
+    /// The merchant-provided invoice number for the transaction.
+    public var invoiceNumber: String?
+    
+    /// Creates a new `InvoicProperties` instance
+    ///
+    ///     InvoiceProperties(role: .payee, invoiceNumber: "<something-here>")
+    public init(role: Role?, invoiceNumber: String?) {
+        self.role = role
+        self.invoiceNumber = invoiceNumber
+    }
+}
