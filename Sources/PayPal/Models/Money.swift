fa0d162f4377a7fde3c961ab5eddbcbc45ce30c4
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-10 12:42:35 -0500

    Changed Money.currencyCode:String to .currency:Currency. Documented Money class

--- Sources/PayPal/Models/Money.swift
+++ Sources/PayPal/Models/Money.swift
@@ -2,13 +2,36 @@ import Vapor
 
 internal let moneyValuePattern = "^((-?[0-9]+)|(-?([0-9]+)?[.][0-9]+))$"
 
+/// An amount of a specified currency.
 public final class Money: Content {
-    public private(set) var currencyCode: String
+    
+    /// The currency that the `Money` instance represents.
+    public var currency: Currency
+    
+    /// The amount of the currency that the class instance represents.
+    ///
+    /// The value, which might be:
+    /// - An integer for currencies like JPY that are not typically fractional.
+    /// - A decimal fraction for currencies like TND that are subdivided into thousandths.
+    /// For the required number of decimal places for a currency code, see [Currency codes - ISO 4217](https://www.iso.org/iso-4217-currency-codes.html).
+    ///
+    /// Maximum length is 32, and is must match this RegEx pattern `^((-?[0-9]+)|(-?([0-9]+)?[.][0-9]+))$`.
     public private(set) var value: String
     
-    public init(currencyCode: String, value: String)throws {
-        guard let _ = Currency.allKeyedCases[currencyCode.lowercased()] else {
-            throw Abort(.badRequest, reason: "Attempted to use invalid currency code '\(currencyCode)'")
+    /// Creates a new `Money` instance.
+    ///
+    ///     try Money(code: "USD", value: "12.25")
+    ///
+    /// - Parameters:
+    ///   - code: The code for the currency of the money.
+    ///   - value: The amount of the currency to represent.
+    ///
+    /// - Throws:
+    ///   - `Abort(.badRequest, "Attempted to use invalid currency code '{code}'")`
+    ///   - `Abort(.badRequest, "Attempted to use malformed mony amount value '{value}'")`
+    public init(code: String, value: String)throws {
+        guard let currency = Currency(code: code) else {
+            throw Abort(.badRequest, reason: "Attempted to use invalid currency code '\(code)'")
         }
         guard
             value.range(of: moneyValuePattern, options: .regularExpression, range: nil, locale: nil) != nil &&
@@ -17,18 +40,25 @@ public final class Money: Content {
             throw Abort(.badRequest, reason: "Attempted to use malformed mony amount value '\(value)'")
         }
         
-        self.currencyCode = currencyCode
+        self.currency = currency
         self.value = value
     }
     
+    /// Creates a `Money` instance from the data container in a decoder's keyed container.
+    ///
+    /// This initializer uses the validations of the main `init(code:value:)` method.
     public convenience init(from decoder: Decoder)throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         try self.init(
-            currencyCode: container.decode(String.self, forKey: .currencyCode),
+            code: container.decode(String.self, forKey: .currency),
             value: container.decode(String.self, forKey: .value)
         )
     }
     
+    /// Sets the instance's `value` property if the string passed in
+    /// passes both length and structure validations.
+    ///
+    /// - Parameter value: The new value to validate and assing to the `value` property.
     public func setValue(to value: String)throws {
         guard
             value.range(of: moneyValuePattern, options: .regularExpression, range: nil, locale: nil) != nil &&
@@ -37,12 +67,5 @@ public final class Money: Content {
                 throw Abort(.badRequest, reason: "Attempted to use malformed mony amount value '\(value)'")
         }
         self.value = value
-    }
-    
-    public func setCode(to code: String)throws {
-        guard let _ = Currency.allKeyedCases[currencyCode.lowercased()] else {
-            throw Abort(.badRequest, reason: "Attempted to use invalid currency code '\(currencyCode)'")
-        }
-        self.currencyCode = code
     }
 }
