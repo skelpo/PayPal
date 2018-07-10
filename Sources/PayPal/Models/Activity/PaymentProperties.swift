4a4c2dcc835614b70c387b7756aea749c83367b2
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-10 13:16:03 -0500

    Documented PaymentProperties class

--- Sources/PayPal/Models/Activity/PaymentProperties.swift
+++ Sources/PayPal/Models/Activity/PaymentProperties.swift
@@ -1,13 +1,27 @@
 import Vapor
 
+/// Additional information about a payment activity.
 public final class PaymentProperties: Content {
+    
+    /// The direction of the money transfer for the transaction.
     public var creditDebitCode: CreditDebitCode?
+    
+    /// The sender message for the transaction.
     public var buyerNotes: String?
+    
+    /// The ID of the order.
     public var orderID: String?
+    
+    /// The ID of the billing agreement.
     public var billingAgreementID: String?
+    
+    /// The type of monitary transaction that occured for the transaction.
     public var externalSubType: ExternalSubType?
+    
+    /// The merchant ID for the transaction.
     public var invoiceNumber: String?
     
+    /// Creates a new `PaymentProperties` instance.
     public init(
         creditDebitCode: CreditDebitCode?,
         buyerNotes: String?,
@@ -24,16 +38,37 @@ public final class PaymentProperties: Content {
         self.invoiceNumber = invoiceNumber
     }
     
+    /// Defines what monitary transaction occured for a transaction.
     public enum ExternalSubType: String, Hashable, CaseIterable, Content {
+        
+        ///
         case unknown = "UNKNOWN"
+        
+        ///
         case purchase = "PURCHASE"
+        
+        ///
         case merchandiseReturn = "MERCHANDISE_RETURN"
+        
+        ///
         case cashWithdrawl = "CASH_WITHDRAWAL"
+        
+        ///
         case cashDeposit = "CASH_DEPOSIT"
+        
+        ///
         case debitTransfer = "DEBIT_TRANSFER"
+        
+        ///
         case creditTransfer = "CREDIT_TRANSFER"
+        
+        ///
         case debitPayment = "DEBIT_PAYMENT"
+        
+        ///
         case creditPayment = "CREDIT_PAYMENT"
+        
+        ///
         case purchaseWithCashback = "PURCHASE_WITH_CASHBACK"
     }
 }
