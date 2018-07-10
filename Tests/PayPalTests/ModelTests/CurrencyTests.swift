957295e343a9e5602dfeea7bb19efd5c871e70af
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-10 07:06:20 -0500

    Created CurrencyTests test suite

---
+++ Tests/PayPalTests/ModelTests/CurrencyTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import PayPal
+
+final class CurrencyTests: XCTestCase {
+    func testFindByCode() {
+        XCTAssertEqual(Currency(code: "USD"), Currency(code: "USD", number: 840, e: 2, name: "United States dollar"))
+        XCTAssertEqual(Currency(code: "XXX"), Currency(code: "XXX", number: 999, e: nil, name: "No currency"))
+        XCTAssertEqual(Currency(code: "EUR"), Currency(code: "EUR", number: 978, e: 2, name: "Euro"))
+        
+        measure {
+            for _ in 0...1_000_000 {
+                _ = Currency(code: "XXX")
+            }
+        }
+    }
+    
+    static var allTests: [(String, (CurrencyTests) -> ()throws -> ())] = [
+        ("testFindByCode", testFindByCode)
+    ]
+}
