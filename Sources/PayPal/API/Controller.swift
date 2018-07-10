c6378a338f8a5eac64ceffa3b02a7f60c5ae6a1f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-10 10:38:08 -0500

    Documented PayPalController protocol

--- Sources/PayPal/API/Controller.swift
+++ Sources/PayPal/API/Controller.swift
@@ -1,8 +1,19 @@
 import Vapor
 
+/// An interface to a PayPal REST API resource.
 public protocol PayPalController: ServiceType {
+    
+    /// The worker that controller is connected to. This is typically a `Request` object.
     var container: Container { get }
+    
+    /// The API resource that the controller connects to.
     var resource: String { get }
     
+    /// Creates an instance of the controller on a given container.
+    ///
+    /// Instead of directly initializing the controller, you should register
+    /// the provider and get the controller using `container.make(Controller.self)`.
+    ///
+    /// - Parameter container: The container that the controller belongs to.
     init(container: Container)
 }
