ab7c7ead6521f8f213e0f5526e2131c1f5e0d76b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-10 12:26:07 -0500

    Created Money.setValue(to:) and .setCode(to:) methods to verify property values before assigning them

--- Sources/PayPal/Models/Money.swift
+++ Sources/PayPal/Models/Money.swift
@@ -3,8 +3,8 @@ import Vapor
 internal let moneyValuePattern = "^((-?[0-9]+)|(-?([0-9]+)?[.][0-9]+))$"
 
 public final class Money: Content {
-    public var currencyCode: String
-    public var value: String
+    public private(set) var currencyCode: String
+    public private(set) var value: String
     
     public init(currencyCode: String, value: String)throws {
         guard let _ = Currency.allKeyedCases[currencyCode.lowercased()] else {
@@ -19,5 +19,22 @@ public final class Money: Content {
         
         self.currencyCode = currencyCode
         self.value = value
+    }
+    
+    public func setValue(to value: String)throws {
+        guard
+            value.range(of: moneyValuePattern, options: .regularExpression, range: nil, locale: nil) != nil &&
+                value.count <= 32
+            else {
+                throw Abort(.badRequest, reason: "Attempted to use malformed mony amount value '\(value)'")
+        }
+        self.value = value
+    }
+    
+    public func setCode(to code: String)throws {
+        guard let _ = Currency.allKeyedCases[currencyCode.lowercased()] else {
+            throw Abort(.badRequest, reason: "Attempted to use invalid currency code '\(currencyCode)'")
+        }
+        self.currencyCode = code
     }
 }
