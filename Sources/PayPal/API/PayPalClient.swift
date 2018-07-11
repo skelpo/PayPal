70800a8356bd51f373b11d8fc222b48b2d590483
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-11 09:53:55 -0500

    Created PayPalClient.send(_:_:headers:body:as:) and .send(_:_:headers:as:) methods

--- Sources/PayPal/API/PayPalClient.swift
+++ Sources/PayPal/API/PayPalClient.swift
@@ -20,4 +20,43 @@ public final class PayPalClient: ServiceType {
     public static func makeService(for worker: Container) throws -> Self {
         return self.init(container: worker)
     }
+    
+    /// Sends a request to the PayPal REST API, using the configured environment.
+    ///
+    /// - Parameters:
+    ///   - method: The HTTP method for the endpoint.
+    ///   - path: The endpoint's URL path, i.e. `/v1/activities/activities`
+    ///   - headers: Additional headers to send with the request.
+    ///     The `Authorization` header is added automatically.
+    ///   - body: The body of the request. This will encoded to JSON.
+    ///     Required for most GET, POST, PUT, and PATCH calls.
+    ///   - response: The type that the response JSON should be decoded to.
+    /// - Returns: The endpoint's response, encoded to the `Result` type.
+    public func send<Body, Result>(
+        _ method: HTTPMethod,
+        _ path: String,
+        headers: HTTPHeaders = [:],
+        body: Body?,
+        as response: Result.Type = Result.self
+    ) -> Future<Result> where Body: Content, Result: Content {
+        return self.container.paypal(method, path, headers: headers, body: body, as: Result.self)
+    }
+    
+    /// Sends a request to the PayPal REST API, using the configured environment.
+    ///
+    /// - Parameters:
+    ///   - method: The HTTP method for the endpoint.
+    ///   - path: The endpoint's URL path, i.e. `/v1/activities/activities`
+    ///   - headers: Additional headers to send with the request.
+    ///     The `Authorization` header is added automatically.
+    ///   - response: The type that the response JSON should be decoded to.
+    /// - Returns: The endpoint's response, encoded to the `Result` type.
+    public func send<Result>(
+        _ method: HTTPMethod,
+        _ path: String,
+        headers: HTTPHeaders = [:],
+        as response: Result.Type = Result.self
+    ) -> Future<Result> where Result: Content {
+        return self.container.paypal(method, path, headers: headers, as: Result.self)
+    }
 }
