60c702c3984a4fb2e7e516cd817c3d261e3137b4
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-11 17:37:32 -0500

    Created RoleTests.testEncoding and .testDecoding test cases

--- Tests/PayPalTests/ModelTests/RoleTests.swift
+++ Tests/PayPalTests/ModelTests/RoleTests.swift
@@ -2,6 +2,10 @@ import XCTest
 @testable import PayPal
 
 final class RoleTests: XCTestCase {
+    struct User: Codable {
+        let role: Role
+    }
+    
     func testCaseRawValues() {
         XCTAssertEqual(Role.requester.rawValue, "REQUESTER")
         XCTAssertEqual(Role.requestee.rawValue, "REQUESTEE")
@@ -14,9 +18,37 @@ final class RoleTests: XCTestCase {
         XCTAssertEqual(Role.allCases, [.requester, .requestee, .payer, .payee])
     }
     
+    func testEncoding()throws {
+        let encoder = JSONEncoder()
+        let requester = try String(data: encoder.encode(User(role: .requester)), encoding: .utf8)
+        let payee = try String(data: encoder.encode(User(role: .payee)), encoding: .utf8)
+        
+        XCTAssertEqual(requester, "{\"role\":\"REQUESTER\"}")
+        XCTAssertEqual(payee, "{\"role\":\"PAYEE\"}")
+    }
+    
+    func testDecoding()throws {
+        let decoder = JSONDecoder()
+        let requestee = """
+        {
+            "role": "REQUESTEE"
+        }
+        """.data(using: .utf8)!
+        let payer = """
+        {
+            "role": "PAYER"
+        }
+        """
+        
+        try XCTAssertEqual(decoder.decode(User.self, from: requestee).role, .requestee)
+        try XCTAssertEqual(decoder.decode(User.self, from: payer).role, .payer)
+    }
+    
     static var allTests: [(String, (RoleTests) -> ()throws -> ())] = [
         ("testCaseRawValues", testCaseRawValues),
-        ("testAllCase", testAllCase)
+        ("testAllCase", testAllCase),
+        ("testEncoding", testEncoding),
+        ("testDecoding", testDecoding)
     ]
 }
 
