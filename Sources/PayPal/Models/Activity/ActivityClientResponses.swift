a8ad54d3695a441427399080811bbc74fdbf5a37
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-11 09:41:11 -0500

    Created ActivitiesResponse class

---
+++ Sources/PayPal/Models/Activity/ActivityClientResponses.swift
@@ -0,0 +1,20 @@
+import Vapor
+
+/// The response structure returned from the `GET /v1/activities/activities` endpoint
+/// and `Activities.activities()` client method.
+public final class ActivitiesResponse: Content {
+    
+    /// A list of the financial activities for a user.
+    public var items: [Activity]?
+    
+    /// The [HATEOAS links](https://developer.paypal.com/docs/api/overview/#hateoas-links) related to the request.
+    public let links: [LinkDescription]?
+    
+    /// Creates a new `ActivitiesResponse` instance.
+    ///
+    ///     ActivitiesResponse(items: [], links: [])
+    public init(items: [Activity]?, links: [LinkDescription]?) {
+        self.items = items
+        self.links = links
+    }
+}
