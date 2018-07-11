59b21708a6efffb2204ecaf65885d3b71565ced1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-11 12:05:42 -0500

    Check for 'next_page_token' key instead of 'page' in encoded QueryParameters string

--- Tests/PayPalTests/ControllerTests/APITests.swift
+++ Tests/PayPalTests/ControllerTests/APITests.swift
@@ -20,7 +20,7 @@ final class APITests: XCTestCase {
         let parameters = QueryParamaters(page: 0, pageSize: 25, sortOrder: .ascending)
         let elements = parameters.encode().split(separator: "&").map(String.init)
         
-        XCTAssert(elements.contains("page=0"), "Paramaters does not contain pair 'page=0'")
+        XCTAssert(elements.contains("next_page_token=0"), "Paramaters does not contain pair 'page=0'")
         XCTAssert(elements.contains("page_size=25"), "Paramaters does not contain pair 'page_size=25'")
         XCTAssert(elements.contains("sort_order=ascending"), "Paramaters does not contain pair 'sort_order=ascending'")
     }
