b6975990e0c3c52b86065a0f6fdd802af07759e0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-12 11:36:59 -0500

    Created OverrideCharge class

---
+++ Sources/PayPal/Models/BillingAgreement/OverrideChargeModel.swift
@@ -0,0 +1,19 @@
+import Vapor
+
+/// A charge model that overrides that default charge model of a `BillingAgreement` object.
+public final class OverrideCharge: Content {
+    
+    /// The ID of the charge model.
+    public var id: String
+    
+    /// The updated amount and currency for this charge model.
+    public var amount: Money
+    
+    /// Creates a new `OverrideCharge` instance.
+    ///
+    ///     OverrideCharge(id: "4F64CB9E-C9B8-49E9-8217-83CDBB857534", amount: Money(currency: .usd, value: "99.99"))
+    public init(id: String, amount: Money) {
+        self.id = id
+        self.amount = amount
+    }
+}
