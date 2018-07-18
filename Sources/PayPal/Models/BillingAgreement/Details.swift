a6e0d8e38d24d7fed0ffbf62a150ae6da5aa6fbc
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-18 17:39:40 -0500

    Documented Details struct

--- Sources/PayPal/Models/BillingAgreement/Details.swift
+++ Sources/PayPal/Models/BillingAgreement/Details.swift
@@ -1,15 +1,46 @@
 import Vapor
 
+
+/// Details for a billing agreement object.
 public struct Details: Content, Equatable {
+    
+    /// The currency and amount of the outstanding balance for this agreement.
     public var oustanding: Money?
+    
+    /// The number of payment cycles remaining for this agreement.
     public var cyclesRemaining: String?
+    
+    /// The number of payment cycles completed for this agreement.
     public var cyclesComplete: String?
+    
+    /// The next billing date and time for this agreement.
     public var nextBilling: Date?
+    
+    /// The last payment date and time for this agreement.
     public var lastPaymentDate: Date?
+    
+    /// The currency and amount of the last payment amount for this agreement.
     public var lastPaymentAmount: Money?
+    
+    /// The final payment date and time for this agreement.
     public var finalPaymentDate: Date?
+    
+    /// The total number of failed payments for this agreement.
     public var failedPaymentCount: String?
     
+    
+    /// Creates a new `Details` struct.
+    ///
+    ///     Details(
+    ///         outstanding: Money(currency: .usd, value: "599.00"),
+    ///         cyclesRemaining: "30",
+    ///         cyclesComplete: "45",
+    ///         nextBilling: Date() + (60 * 60 * 24 * 365),
+    ///         lastPaymentDate: Date(),
+    ///         lastPaymentAmount: Money(currency: .usd, value: "19.97"),
+    ///         finalPaymentDate: Date.distantFuture,
+    ///         failedPaymentCount: "5"
+    ///     )
     public init(
         outstanding: Money?,
         cyclesRemaining: String?,
