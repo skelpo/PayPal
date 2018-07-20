be3b435ae49be72ad4de657a40944ef71d18b656
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-20 09:56:04 -0500

    Created BillingAgreementsTests.testCreateEndpoint test case

--- Tests/PayPalTests/ControllerTests/BillingAgreementsTests.swift
+++ Tests/PayPalTests/ControllerTests/BillingAgreementsTests.swift
@@ -20,8 +20,35 @@ final class BillingAgreementsTests: XCTestCase {
         _ = try app.make(BillingAgreements.self)
     }
     
+    func testCreateEndpoint()throws {
+        let agreements = try app.make(BillingAgreements.self)
+        let new = try NewAgreement(
+            name: "Nia's Maggot Loaf",
+            description: "Weekly maggot loaf subscription",
+            start: Date().iso8601,
+            payer: Payer(
+                method: .paypal,
+                fundingInstruments: nil,
+                info: nil
+            ),
+            plan: Plan(
+                name: "Nia's Maggot Loaf",
+                description: "Weekly maggot loaf subscription",
+                type: .infinate,
+                payments: nil,
+                preferances: nil
+            )
+        )
+        
+        let agreement = try agreements.create(with: new).wait()
+        
+        XCTAssertNotEqual(agreement.id, nil)
+        XCTAssertEqual(agreement.name, "Nia's Maggot Loaf")
+    }
+    
     static var allTests: [(String, (BillingAgreementsTests) -> ()throws -> ())] = [
-        ("testServiceExists", testServiceExists)
+        ("testServiceExists", testServiceExists),
+        ("testCreateEndpoint", testCreateEndpoint)
     ]
 }
 
