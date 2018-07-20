ea1611d27c2aa60d5460828c913e018efa9d1ea3
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-20 08:56:02 -0500

    Verify JSON values in PatchTest test suite

--- Tests/PayPalTests/ModelTests/PatchTests.swift
+++ Tests/PayPalTests/ModelTests/PatchTests.swift
@@ -1,28 +1,29 @@
 import XCTest
+import JSON
 @testable import PayPal
 
 final class PatchTests: XCTestCase {
     func testInit()throws {
-        let patchStr = Patch(operation: .move, path: "/name", value: "Oskar Reteep", from: "/full_name")
+        let patchStr = try Patch(operation: .move, path: "/name", value: "Oskar Reteep", from: "/full_name")
         
         XCTAssertEqual(patchStr.operation, .move)
         XCTAssertEqual(patchStr.path, "/name")
-        XCTAssertEqual(patchStr.value, "Oskar Reteep")
+        XCTAssertEqual(patchStr.value, .string("Oskar Reteep"))
         XCTAssertEqual(patchStr.from, "/full_name")
         
         
-        let patchArr = Patch(operation: .add, path: "/cityIDs", value: [1, 2, 6])
+        let patchArr = try Patch(operation: .add, path: "/cityIDs", value: [1, 2, 6])
         
         XCTAssertEqual(patchArr.operation, .add)
         XCTAssertEqual(patchArr.path, "/cityIDs")
-        XCTAssertEqual(patchArr.value, [1, 2, 6])
+        XCTAssertEqual(patchArr.value, .array([.number(.int(1)), .number(.int(2)), .number(.int(6))]))
         XCTAssertEqual(patchArr.from, nil)
     }
     
     func testEncoding()throws {
         let encoder = JSONEncoder()
-        let patchStr = Patch(operation: .move, path: "/name", value: "Oskar Reteep", from: "/full_name")
-        let patchArr = Patch(operation: .add, path: "/city_ids", value: [1, 2, 6])
+        let patchStr = try Patch(operation: .move, path: "/name", value: "Oskar Reteep", from: "/full_name")
+        let patchArr = try Patch(operation: .add, path: "/city_ids", value: [1, 2, 6])
         
         let strJson = try String(data: encoder.encode(patchStr), encoding: .utf8)!
         let arrJson = try String(data: encoder.encode(patchArr), encoding: .utf8)!
@@ -33,8 +34,8 @@ final class PatchTests: XCTestCase {
     
     func testDecoding()throws {
         let decoder = JSONDecoder()
-        let patchStr = Patch(operation: .move, path: "/name", value: "Oskar Reteep", from: "/full_name")
-        let patchArr = Patch(operation: .add, path: "/city_ids", value: [1, 2, 6])
+        let patchStr = try Patch(operation: .move, path: "/name", value: "Oskar Reteep", from: "/full_name")
+        let patchArr = try Patch(operation: .add, path: "/city_ids", value: [1, 2, 6])
         
         let strJson = """
         {
@@ -52,8 +53,8 @@ final class PatchTests: XCTestCase {
         }
         """.data(using: .utf8)!
         
-        try XCTAssertEqual(patchStr, decoder.decode(Patch<String>.self, from: strJson))
-        try XCTAssertEqual(patchArr, decoder.decode(Patch<[Int]>.self, from: arrJson))
+        try XCTAssertEqual(patchStr, decoder.decode(Patch.self, from: strJson))
+        try XCTAssertEqual(patchArr, decoder.decode(Patch.self, from: arrJson))
     }
     
     static var allTests: [(String, (PatchTests) -> ()throws -> ())] = [
