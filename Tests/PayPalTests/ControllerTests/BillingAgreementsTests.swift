5e1e107e702753474daaddf6347f3d660bd1d5bc
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-20 10:13:26 -0500

    Created BillingAgreementsTests.testGetEndpoint test case

--- Tests/PayPalTests/ControllerTests/BillingAgreementsTests.swift
+++ Tests/PayPalTests/ControllerTests/BillingAgreementsTests.swift
@@ -5,6 +5,7 @@ import Vapor
 final class BillingAgreementsTests: XCTestCase {
     
     var app: Application!
+    var id: String!
     
     override func setUp() {
         super.setUp()
@@ -44,43 +45,32 @@ final class BillingAgreementsTests: XCTestCase {
         
         XCTAssertNotEqual(agreement.id, nil)
         XCTAssertEqual(agreement.name, "Nia's Maggot Loaf")
+        
+        self.id = agreement.id
     }
     
     func testUpdateEndpoint()throws {
         let agreements = try app.make(BillingAgreements.self)
-        let new = try NewAgreement(
-            name: "Nia's Maggot Loaf",
-            description: "Weekly maggot loaf subscription",
-            start: Date().iso8601,
-            payer: Payer(
-                method: .paypal,
-                fundingInstruments: nil,
-                info: nil
-            ),
-            plan: Plan(
-                name: "Nia's Maggot Loaf",
-                description: "Weekly maggot loaf subscription",
-                type: .infinate,
-                payments: nil,
-                preferances: nil
-            )
-        )
-        
-        let agreement = try agreements.create(with: new).wait()
-        
-        XCTAssertNotEqual(agreement.id, nil)
-        XCTAssertEqual(agreement.name, "Nia's Maggot Loaf")
         
         let patch = try Patch(operation: .replace, path: "/name", value: "Igby Maggot")
-        let updated = try agreements.update(agreement: agreement.id!, with: [patch]).wait()
+        let updated = try agreements.update(agreement: self.id, with: [patch]).wait()
         
         XCTAssertEqual(updated, .ok)
+    }
+    
+    func testGetEndpoint()throws {
+        let agreements = try app.make(BillingAgreements.self)
+        let agreement = try agreements.get(agreement: self.id).wait()
+        
+        XCTAssertNotEqual(agreement.id, nil)
+        XCTAssert(agreement.name == "Nia's Maggot Loaf" || agreement.name == "Igby Maggot")
     }
     
     static var allTests: [(String, (BillingAgreementsTests) -> ()throws -> ())] = [
         ("testServiceExists", testServiceExists),
         ("testCreateEndpoint", testCreateEndpoint),
-        ("testUpdateEndpoint", testUpdateEndpoint)
+        ("testUpdateEndpoint", testUpdateEndpoint),
+        ("testGetEndpoint", testGetEndpoint)
     ]
 }
 
