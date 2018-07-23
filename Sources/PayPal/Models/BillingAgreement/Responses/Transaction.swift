e0333630a78a213ecf4caf0a5425b5cf621f8e62
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-23 11:41:25 -0500

    Created Transaction struct

--- Sources/PayPal/Models/BillingAgreement/Responses/Transaction.swift
+++ Sources/PayPal/Models/BillingAgreement/Responses/Transaction.swift
@@ -1,3 +1,29 @@
 import Vapor
 
-public struct Transaction: Content, Equatable {}
+public struct Transaction: Content, Equatable {
+    public let id: String?
+    public let state: State?
+    public let type: String?
+    public let email: String?
+    public let name: String?
+    public let timestamp: String?
+    public let timezone: String?
+    
+    public var amount: Money
+    public var fee: Money
+    public var net: Money
+    
+    public init(amount: Money, fee: Money, net: Money) {
+        self.id = nil
+        self.state = nil
+        self.type = nil
+        self.email = nil
+        self.name = nil
+        self.timestamp = nil
+        self.timezone = nil
+        
+        self.amount = amount
+        self.fee = fee
+        self.net = net
+    }
+}
