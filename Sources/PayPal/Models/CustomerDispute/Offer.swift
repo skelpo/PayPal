da7d92604610e6ca7dca5cfead2bc0a14f3844c1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-24 11:03:39 -0500

    Created Offer struct

--- Sources/PayPal/Models/CustomerDispute/Offer.swift
+++ Sources/PayPal/Models/CustomerDispute/Offer.swift
@@ -1,3 +1,13 @@
 import Vapor
 
-public struct Offer {}
+public struct Offer: Content, Equatable {
+    public var buyerAmount: Money?
+    public var sellerAmount: Money?
+    public var type: OfferType?
+    
+    public init(buyerAmount: Money?, sellerAmount: Money?, type: OfferType?) {
+        self.buyerAmount = buyerAmount
+        self.sellerAmount = sellerAmount
+        self.type = type
+    }
+}
