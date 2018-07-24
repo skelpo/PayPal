6afe158021b19c416e96cf7301d54585918094cf
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-24 12:04:32 -0500

    Created Message struct

--- Sources/PayPal/Models/Message/Message.swift
+++ Sources/PayPal/Models/Message/Message.swift
@@ -1,3 +1,13 @@
 import Vapor
 
-public struct Message {}
+public struct Message: Content, Equatable {
+    public let poster: Poster?
+    public let posted: String?
+    public var content: String?
+    
+    public init(content: String?) {
+        self.poster = nil
+        self.posted = nil
+        self.content = content
+    }
+}
