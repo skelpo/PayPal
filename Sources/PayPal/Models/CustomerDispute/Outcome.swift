56daddc30378aacf8c9c7ffbde8019786f629458
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-24 15:00:17 -0500

    Created CustomerDispute.Outcome struct

--- Sources/PayPal/Models/CustomerDispute/Outcome.swift
+++ Sources/PayPal/Models/CustomerDispute/Outcome.swift
@@ -2,5 +2,21 @@ import Vapor
 
 extension CustomerDispute {
     
-    public struct Outcome {}
+    /// The outcome of a customer dispute.
+    public struct Outcome: Content, Equatable {
+        
+        /// The outcome code of a resolved dispute. If the value is `nil`, the dispute is not resolved.
+        public var code: Code?
+        
+        /// The amount that either the merchant or PayPal refunds the customer.
+        public var refunded: Money?
+        
+        /// Creates a new `CustomerDispute.Outcome` instance.
+        ///
+        ///     CustomerDispute.Outcome(code: .buyer, refunded: Money(currency: .usd, value: "56.72"))
+        public init(code: Code?, refunded: Money?) {
+            self.code = code
+            self.refunded = refunded
+        }
+    }
 }
