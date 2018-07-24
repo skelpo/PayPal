e61cd55ad8b65a3ac5cd88bf74b7226987d01358
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-24 16:56:17 -0500

    Conformed Buyer struct to ValidationSetable protocol

--- Sources/PayPal/Models/CustomerDispute/Buyer.swift
+++ Sources/PayPal/Models/CustomerDispute/Buyer.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 /// The buyer party of a customer dispute.
-public struct Buyer: Content, Equatable {
+public struct Buyer: Content, ValidationSetable, Equatable {
     
     /// The email address for the customer's PayPal account.
     ///
@@ -20,5 +20,21 @@ public struct Buyer: Content, Equatable {
     public init(email: String?, name: String?) {
         self.email = email
         self.name = name
+    }
+    
+    public func setterValidations() -> SetterValidations<Buyer> {
+        var validations = SetterValidations(Buyer.self)
+        
+        validations.set(\.email) { email in
+            guard let email = email else { return }
+            guard let _ = email.range(of: "^.+@[^\"\\-].+$", options: .regularExpression) else {
+                throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`email` property value must match '^.+@[^\"\\-].+$' RegEx pattern")
+            }
+            guard email.count >= 3 && email.count <= 254 else {
+                throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`email` property must have a length between 3 and 254")
+            }
+        }
+        
+        return validations
     }
 }
