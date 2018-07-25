41aa1a47adaa675ef40511df0c3c1c2b6c220e07
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-25 12:11:22 -0500

    Created CustomerDispute struct

--- Sources/PayPal/Models/CustomerDispute/CustomerDispute.swift
+++ Sources/PayPal/Models/CustomerDispute/CustomerDispute.swift
@@ -1,3 +1,37 @@
 import Vapor
 
-public struct CustomerDispute {}
+public struct CustomerDispute: Content, Equatable {
+    public let id: String?
+    public let created: String?
+    public let updated: String?
+    public let status: Status?
+    public let outcome: Outcome?
+    public let lifecycleStage: LifeCycleStage?
+    public let channel: Channel?
+    public let offer: Offer?
+    public let links: [LinkDescription]?
+    
+    public var transactions: [TransactionInfo]?
+    public var reason: Item.Reason?
+    public var amount: Money?
+    public var messages: [Message]?
+    public var responseDue: String?
+    
+    public init(transactions: [TransactionInfo]?, reason: Item.Reason?, amount: Money?, messages: [Message]?, responseDue: String?) {
+        self.id = nil
+        self.status = nil
+        self.outcome = nil
+        self.lifecycleStage = nil
+        self.channel = nil
+        self.offer = nil
+        self.links = nil
+        self.created = nil
+        self.updated = nil
+        
+        self.transactions = transactions
+        self.reason = reason
+        self.amount = amount
+        self.messages = messages
+        self.responseDue = responseDue
+    }
+}
--- Sources/PayPal/Utilities/ISO8601.swift
+++ Sources/PayPal/Utilities/ISO8601.swift
@@ -3,7 +3,7 @@ import Foundation
 // Credit to @LeoDabus on StackOverflow for this formatter.
 // https://stackoverflow.com/questions/28016578/swift-how-to-create-a-date-time-stamp-and-format-as-iso-8601-rfc-3339-utc-tim#28016692
 extension DateFormatter {
-    static let iso8601: Formatter = {
+    public static let iso8601: Formatter = {
         if #available(OSX 10.12, *) {
             return ISO8601DateFormatter()
         } else {
@@ -18,7 +18,7 @@ extension DateFormatter {
 }
 
 extension Date {
-    var iso8601: String {
+    public var iso8601: String {
         return DateFormatter.iso8601.string(for: self) ?? self.description
     }
 }
