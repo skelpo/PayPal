024e871dbcb355f6fc4020894fb4f7a8f2bd741f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-26 11:39:18 -0500

    Created AcceptDisputeBody struct

--- Sources/PayPal/Models/CustomerDispute/AcceptBody.swift
+++ Sources/PayPal/Models/CustomerDispute/AcceptBody.swift
@@ -1,3 +1,17 @@
 import Vapor
 
-public struct AcceptDisputeBody {}
+public struct AcceptDisputeBody: Content, Equatable {
+    public var note: String?
+    public var reason: Reason?
+    public var invoiceID: String?
+    public var returnAddress: Address?
+    public var refund: Money?
+    
+    public init(note: String?, reason: Reason?, invoiceID: String?, returnAddress: Address?, refund: Money?) {
+        self.note = note
+        self.reason = reason
+        self.invoiceID = invoiceID
+        self.returnAddress = returnAddress
+        self.refund = refund
+    }
+}
