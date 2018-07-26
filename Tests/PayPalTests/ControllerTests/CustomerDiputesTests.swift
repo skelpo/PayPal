b04867ecc42e7d792db254e7e712344c17f05582
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-26 10:02:47 -0500

    Created CustomerDisputesTests.testListEndpoint and .testDetailsEndpoint test cases

--- Tests/PayPalTests/ControllerTests/CustomerDiputesTests.swift
+++ Tests/PayPalTests/ControllerTests/CustomerDiputesTests.swift
@@ -21,8 +21,26 @@ final class CustomerDisputesTests: XCTestCase {
         _ = try app.make(CustomerDisputes.self)
     }
     
+    func testListEndpoint()throws {
+        let disputes = try self.app.make(CustomerDisputes.self)
+        let _ = try disputes.list().wait()
+    }
+    
+    func testDetailsEndpoint()throws {
+        let disputes = try self.app.make(CustomerDisputes.self)
+        guard let id = self.id else {
+            throw Abort(.internalServerError, reason: "Cannot get dispute ID")
+        }
+        
+        let details = try disputes.details(for: id).wait()
+        
+        XCTAssertEqual(details.id, id)
+    }
+    
     static var allTests: [(String, (CustomerDisputesTests) -> ()throws -> ())] = [
         ("testServiceExists", testServiceExists),
+        ("testListEndpoint", testListEndpoint),
+        ("testDetailsEndpoint", testDetailsEndpoint)
     ]
 }
 
