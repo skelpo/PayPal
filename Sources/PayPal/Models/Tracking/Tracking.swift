4566eefe81312cc0ecef42c4baebab30bca71175
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-30 07:47:32 -0500

    Documented Tracking struct

--- Sources/PayPal/Models/Tracking/Tracking.swift
+++ Sources/PayPal/Models/Tracking/Tracking.swift
@@ -1,11 +1,23 @@
 import Vapor
 
+/// The tracking information for ordered and shipped merchandise.
 public struct Tracking: Content, Equatable {
+    
+    /// The name of the carrier for the shipment of the transaction for this dispute.
     public var carrier: Carrier?
+    
+    /// This field capture the name of carrier in free form text for unavailable carriers from existing list.
     public var carrierOther: String?
+    
+    /// The URL to track the dispute-related transaction shipment.
     public var url: String?
+    
+    /// The number to track the dispute-related transaction shipment.
     public var number: String?
     
+    /// Creates a new `Tracking` instance.
+    ///
+    ///     Tracking(carrier: .usps, url: "https://whoshippedit.com/shippment/9163524667210796186056", number: "9163524667210796186056")
     public init(carrier: Carrier?, other: String?, url: String?, number: String?) {
         self.carrier = carrier
         self.carrierOther = other
