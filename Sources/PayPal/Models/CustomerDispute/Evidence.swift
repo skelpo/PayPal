133a751939f594cf28d4d513730fff391b8557bc
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-30 13:49:57 -0500

    Documented Evidence struct

--- Sources/PayPal/Models/CustomerDispute/Evidence.swift
+++ Sources/PayPal/Models/CustomerDispute/Evidence.swift
@@ -1,12 +1,43 @@
 import Vapor
 
+/// Evidance for a party in a dispute.
 public struct Evidence: Content, Equatable {
+    
+    /// The type of evidence.
     public var type: EvidenceType?
+    
+    /// The evidence-related information.
     public var info: Info?
+    
+    /// An array of evidence documents.
     public var documents: [Document]?
+    
+    /// Any evidence-related notes.
+    ///
+    /// Maximum length: 2000.
     public var notes: String?
+    
+    /// The item ID. If the merchant provides multiple pieces of evidence and the transaction has multiple item IDs,
+    /// the merchant can use this value to associate a piece of evidence with an item ID.
     public var itemID: String?
     
+    
+    /// Creates a new `Evidence` instance.
+    ///
+    ///     Evidence(
+    ///         type: .proofOfFulfillment,
+    ///         info: Info(
+    ///             tracking: [
+    ///                 Tracking(carrier: .usps, other: nil, url: "https://whoshippedit.com/shippment/9163524667210796186056", number: "9163524667210796186056")
+    ///             ],
+    ///             refunds: [
+    ///                 "2F214F48-2651-498B-9D06-150BF00E85DA"
+    ///             ]
+    ///         ),
+    ///         documents: [Document(name: "README.md", size: "65kb")],
+    ///         notes: "I win. Ha!",
+    ///         itemID: "4FB4018C-F925-4FC6-B44B-0174C1B59F17"
+    ///     )
     public init(type: EvidenceType?, info: Info?, documents: [Document]?, notes: String?, itemID: String?) {
         self.type = type
         self.info = info
