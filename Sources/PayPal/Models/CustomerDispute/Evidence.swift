e4249dce3e3965c3ad10bb604b36cd78fca6ae49
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-30 11:10:15 -0500

    Created Evidence struct

--- Sources/PayPal/Models/CustomerDispute/Evidence.swift
+++ Sources/PayPal/Models/CustomerDispute/Evidence.swift
@@ -1,3 +1,17 @@
 import Vapor
 
-public struct Evidence {}
+public struct Evidence: Content, Equatable {
+    public var type: EvidenceType?
+    public var info: Info?
+    public var documents: [Document]?
+    public var notes: String?
+    public var itemID: String?
+    
+    public init(type: EvidenceType?, info: Info?, documents: [Document]?, notes: String?, itemID: String?) {
+        self.type = type
+        self.info = info
+        self.documents = documents
+        self.notes = notes
+        self.itemID = itemID
+    }
+}
