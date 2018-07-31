9d460fd00f1a399e94ed0885ed411a5300ebf897
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-07-31 10:14:28 -0500

    Created Future<Dictionary>[_:default:] subscript

--- Sources/PayPal/Utilities/Sequence+Future.swift
+++ Sources/PayPal/Utilities/Sequence+Future.swift
@@ -20,6 +20,17 @@ extension Future {
     ///
     /// - Returns: The value of the key passed in, wrapped in a future.
     public subscript<Key, Value>(_ key: Key) -> Future<Value?> where T == Dictionary<Key, Value> {
-        return self.map(to: T.Value?.self) { this in return this[key] }
+        return self.map(to: Value?.self) { this in return this[key] }
+    }
+    
+    /// Gets the value from a dictionary wrapped in a future. If no value is found, the default value is used.
+    ///
+    /// - Parameters:
+    ///   - key: The key of the value in the dictionary to fetch.
+    ///   - default: The value to return if none is found for the key passed in.
+    ///
+    /// - Returns: The value of the key passed in or the default value, wrapped in a future.
+    public subscript<Key, Value>(_ key: Key, `default`: Value) -> Future<Value> where T == Dictionary<Key, Value> {
+        return self.map(to: Value.self) { this in return this[key, default: `default`] }
     }
 }
