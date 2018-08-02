5f733e9ceca634ea6c24e32a48bd12583415cf0e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-02 16:38:46 -0500

    Created 'Amount' struct to use 'currency' as raw value for .currecny property

--- Sources/PayPal/Models/Money.swift
+++ Sources/PayPal/Models/Money.swift
@@ -3,10 +3,13 @@ import Vapor
 internal let moneyValuePattern = "^((-?[0-9]+)|(-?([0-9]+)?[.][0-9]+))$"
 
 /// An amount of a specified currency.
-public struct Money: Content, ValidationSetable, Equatable {
+public protocol Monitary: Content, ValidationSetable, Equatable {
+    
+    /// The coding keys used for encoding/decoding the object.
+    associatedtype CodingKeys: CodingKey
     
     /// The currency that the `Money` instance represents.
-    public var currency: Currency
+    var currency: Currency { get set }
     
     /// The amount of the currency that the class instance represents.
     ///
@@ -15,11 +18,11 @@ public struct Money: Content, ValidationSetable, Equatable {
     /// - A decimal fraction for currencies like TND that are subdivided into thousandths.
     /// For the required number of decimal places for a currency code, see [Currency codes - ISO 4217](https://www.iso.org/iso-4217-currency-codes.html).
     ///
-    /// This property can be set using the `Money.set(_:)` method, which will validate the new value
+    /// This property should be set using the `Money.set(_:)` method, which will validate the new value
     /// before assigning it to the property.
     ///
     /// Maximum length is 32, and is must match this RegEx pattern `^((-?[0-9]+)|(-?([0-9]+)?[.][0-9]+))$`.
-    public private(set) var value: String
+    var value: String { get set }
     
     /// Creates a new `Money` instance.
     ///
@@ -29,9 +32,45 @@ public struct Money: Content, ValidationSetable, Equatable {
     ///   - code: The code for the currency of the money.
     ///   - value: The amount of the currency to represent.
     ///
-    /// - Throws:
-    ///   - `Abort(.badRequest, "Attempted to use invalid currency code '{code}'")`
-    ///   - `Abort(.badRequest, "Attempted to use malformed mony amount value '{value}'")`
+    /// - Throws: `Abort(.badRequest, "Attempted to use malformed mony amount value '{value}'")`
+    init(currency: Currency, value: String)throws
+}
+
+extension Monitary {
+    
+    /// Compares two `Money` objects, checking that the `currency`
+    /// and `value` properties are equal.
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        return (lhs.currency == rhs.currency) && (lhs.value == rhs.value)
+    }
+    
+    /// See `ValidationSetable.setterValidations()`
+    public func setterValidations() -> SetterValidations<Self> {
+        var validations = SetterValidations(Self.self)
+        validations.set(\Self.value) { value in
+            guard
+                value.range(of: moneyValuePattern, options: .regularExpression, range: nil, locale: nil) != nil &&
+                value.count <= 32
+            else {
+                throw Abort(.badRequest, reason: "Attempted to use malformed money amount value '\(value)'")
+            }
+        }
+        return validations
+    }
+}
+
+/// An amount of a specified currency.
+///
+/// Uses `currency_code` as the `currency` property coding key.
+public struct Money: Monitary {
+    
+    /// See `Monitary.currency`.
+    public var currency: Currency
+    
+    /// See `Monitary.value`.
+    public var value: String
+    
+    /// See `Monitary.init(currency:value:)`.
     public init(currency: Currency, value: String)throws {
         self.currency = currency
         self.value = value
@@ -50,27 +89,47 @@ public struct Money: Content, ValidationSetable, Equatable {
         )
     }
     
-    /// Compares two `Money` objects, checking that the `currency`
-    /// and `value` properties are equal.
-    public static func == (lhs: Money, rhs: Money) -> Bool {
-        return (lhs.currency == rhs.currency) && (lhs.value == rhs.value)
-    }
-    
-    public func setterValidations() -> SetterValidations<Money> {
-        var validations = SetterValidations(Money.self)
-        validations.set(\.value) { value in
-            guard
-                value.range(of: moneyValuePattern, options: .regularExpression, range: nil, locale: nil) != nil &&
-                    value.count <= 32
-                else {
-                    throw Abort(.badRequest, reason: "Attempted to use malformed money amount value '\(value)'")
-            }
-        }
-        return validations
-    }
-    
-    enum CodingKeys: String, CodingKey {
+    /// See `Monitary.CodingKeys`.
+    public enum CodingKeys: String, CodingKey {
         case value
         case currency = "currency_code"
+    }
+}
+
+/// An amount of a specified currency.
+///
+/// Uses `currency` as the `currency` property coding key.
+public struct Amount: Monitary {
+
+    /// See `Monitary.currency`.
+    public var currency: Currency
+    
+    /// See `Monitary.value`.
+    public var value: String
+    
+    /// See `Monitary.init(currency:value:)`.
+    public init(currency: Currency, value: String)throws {
+        self.currency = currency
+        self.value = value
+        
+        try self.set(\.value <~ value)
+    }
+    
+    /// Creates a `Money` instance from the data container in a decoder's keyed container.
+    ///
+    /// This initializer uses the validations of the main `init(code:value:)` method.
+    public init(from decoder: Decoder)throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        try self.init(
+            currency: container.decode(Currency.self, forKey: .currency),
+            value: container.decode(String.self, forKey: .value)
+        )
+    }
+    
+    /// See `Monitary.CodingKeys`.
+    public enum CodingKeys: String, CodingKey {
+        case value
+        case currency = "currency"
     }
 }
