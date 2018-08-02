81599cb818c12e0ad246375a6b9f6f5b929d0eaf
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-02 16:36:04 -0500

    Made Frequency enum case decoding case-insensative

--- Sources/PayPal/Models/Frequency.swift
+++ Sources/PayPal/Models/Frequency.swift
@@ -15,4 +15,13 @@ public enum Frequency: String, Hashable, CaseIterable, Content {
     
     /// The interval is how often in years an action should occur.
     case year = "YEAR"
+    
+    public init(from decoder: Decoder)throws {
+        let container = try decoder.singleValueContainer()
+        let raw = try container.decode(String.self).uppercased()
+        guard let freq = Frequency.init(rawValue: raw) else {
+            throw PayPalError(status: .badRequest, identifier: "badCase", reason: "Cannot get `Frequency` case from value '\(raw)'")
+        }
+        self = freq
+    }
 }
