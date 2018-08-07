ea197e1ff339cf89c27d8df2053c60cb9490169d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-07 07:52:55 -0500

    Created custom UserInfo.CodingKeys struct

--- Sources/PayPal/Models/User/UserInfo.swift
+++ Sources/PayPal/Models/User/UserInfo.swift
@@ -9,7 +9,7 @@ public struct UserInfo: Content, Equatable {
     public let middleName: String?
     public let picture: String?
     public let email: String?
-    public let verifiedEmail: Bool?
+    public let emailVerified: Bool?
     public let gender: String?
     public let birthdate: String?
     public let zoneinfo: String?
@@ -19,5 +19,19 @@ public struct UserInfo: Content, Equatable {
     public let verified: Bool?
     public let accountType: AccountType?
     public let ageRange: String?
-    public let playerID: String?
+    public let payerID: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case sub, name, picture, email, gender, birthdate, zoneinfo, locale, address
+        case id = "user_id"
+        case givenName = "given_name"
+        case familyName = "family_name"
+        case middleName = "middle_name"
+        case emailVerified = "email_verified"
+        case phoneNumber = "phone_number"
+        case verified = "verified_account"
+        case accountType = "account_type"
+        case ageRange = "age_range"
+        case payerID = "payer_id"
+    }
 }
