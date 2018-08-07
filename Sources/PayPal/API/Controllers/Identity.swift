7c48feb9e57c124e928e85e112792226535a8142
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-07 10:14:44 -0500

    Created Identity.info() method

--- Sources/PayPal/API/Controllers/Identity.swift
+++ Sources/PayPal/API/Controllers/Identity.swift
@@ -20,4 +20,15 @@ public final class Identity: PayPalController {
         self.container = container
         self.resource = "identity"
     }
+    
+    /// Retrieves the authenticated user's profile attributes.
+    ///
+    /// - Returns: A `UserInfo` object, containing user profile attributes. The attributes returned depend on the scopes configured for the REST app.
+    ///   For example, if the `address` scope is not configured for the app, the response does not include the `address` attribute.
+    public func info() -> Future<UserInfo> {
+        return Future.flatMap(on: self.container) { () -> Future<UserInfo> in
+            let client = try self.container.make(PayPalClient.self)
+            return try client.get(self.path() + "openidconnect/userinfo", parameters: QueryParamaters(custom: ["schema": "openid"]), as: UserInfo.self)
+        }
+    }
 }
