a14de6afbb047bc0164b86b221021bf26f345f48
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-07 06:49:14 -0500

    Created User.AccountType enum

---
+++ Sources/PayPal/Models/User/AccountType.swift
@@ -0,0 +1,17 @@
+import Vapor
+
+extension User {
+    
+    /// That type of account that a user has.
+    public enum AccountType: String, Hashable, CaseIterable, Content {
+        
+        /// A personal account. `PERSONAL`
+        case personal = "PERSONAL"
+        
+        /// A business account. `BUSINESS`
+        case business = "BUSINESS"
+        
+        /// `PREMIER`
+        case premier = "PREMIER"
+    }
+}
