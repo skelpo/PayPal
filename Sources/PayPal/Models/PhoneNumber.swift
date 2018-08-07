23d736aa88b7016cb6d008138e615ab24cd14f85
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-07 12:07:38 -0500

    Created PhoneNumber struct

---
+++ Sources/PayPal/Models/PhoneNumber.swift
@@ -0,0 +1,30 @@
+import Vapor
+
+/// A phone number's country code and local number.
+public struct PhoneNumber {
+    
+    /// The country code portion of the phone number, in [E.164 format](https://www.itu.int/rec/T-REC-E.164-201011-I).
+    ///
+    /// This property can be set using the `PhoneNumber.set(_:)`. This method will validate
+    /// the new value before assigning it to the property.
+    ///
+    /// Minimum length: 1. Maximum length: 3. Pattern: `^[0-9]{1,3}?$`.
+    public var country: String
+    
+    
+    /// The in-country phone number, in [E.164 format](https://www.itu.int/rec/T-REC-E.164-201011-I).
+    ///
+    /// This property can be set using the `PhoneNumber.set(_:)`. This method will validate
+    /// the new value before assigning it to the property.
+    ///
+    /// Minimum length: 1. Maximum length: 14. Pattern: `^[0-9]{1,14}?$`.
+    public var number: String
+    
+    /// Creates a new instance of `PhoneNumber`.
+    ///
+    ///     PhoneNumber(country: "1", number: "9963191901")
+    public init(country: String, number: String)throws {
+        self.country = country
+        self.number = number
+    }
+}
