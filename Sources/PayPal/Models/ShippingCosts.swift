6cd9beebbf405e9ad0e5f6d0c878de39f00a7ae4
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-08 16:55:59 -0500

    Created ShippingCosts struct

---
+++ Sources/PayPal/Models/ShippingCosts.swift
@@ -0,0 +1,19 @@
+import Vapor
+
+/// The shipping costs for an invoice.
+public struct ShippingCosts: Content, Equatable {
+    
+    /// The currency and amount of the shipping charge. Value is from `0` to `1,000,000`. Supports up to two decimal places.
+    public var amount: Amount?
+    
+    /// The tax percentage on the shipping amount.
+    public var tax: Tax?
+    
+    /// Creates a new `ShippingCosts` instance.
+    ///
+    ///     ShippingCosts(amount: Amount(currency: .usd, value: "2.50"), tax: Tax(name: "Shipping", percent: 7.5, amount: Amount(currency: .usd, value: "0.18")))
+    public init(amount: Amount?, tax: Tax?) {
+        self.amount = amount
+        self.tax = tax
+    }
+}
