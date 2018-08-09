c3d897712b24ce3d4829fd6c4b1b901bb1f0cfcf
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-09 11:55:08 -0500

    Created a custom Invoice.Metadata.CodingKeys enum

--- Sources/PayPal/Models/Payment/Metadata.swift
+++ Sources/PayPal/Models/Payment/Metadata.swift
@@ -24,15 +24,28 @@ extension Invoice {
         public let lastUpdatedBy: String?
         
         /// The date and time when the resource was first sent, in [Internet date and time format](https://tools.ietf.org/html/rfc3339#section-5.6).
-        public let firstSentDate: String?
+        public let firstSentAt: String?
         
         /// The date and time when the resource was last sent, in [Internet date and time format](https://tools.ietf.org/html/rfc3339#section-5.6).
-        public let lastSentDate: String?
+        public let lastSentAt: String?
         
         /// The email address of the account that last sent the resource.
         public let lastSentBy: String?
         
         /// The URL for the payer's view of the invoice.
         public let payerView: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case createdAt = "created_date"
+            case createdBy = "created_by"
+            case cancelledAt = "cancelled_date"
+            case cancelledBy = "cancelled_by"
+            case lastUpdatedAt = "last_updated_date"
+            case lastUpdatedBy = "last_updated_by"
+            case firstSentAt = "first_sent_date"
+            case lastSentAt = "last_sent_date"
+            case lastSentBy = "last_sent_by"
+            case payerView = "payer_view_url"
+        }
     }
 }
