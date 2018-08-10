a96c253ddebfd84c92523af89d7560c91ad94210
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-10 08:16:26 -0500

    Conformed Participant.Invoice struct to ValidationSetable protocol

--- Sources/PayPal/Models/Invoice/Participant.swift
+++ Sources/PayPal/Models/Invoice/Participant.swift
@@ -3,7 +3,7 @@ import Vapor
 extension Invoice {
     
     /// Information for people who are connected to the invoice and will receive a copy of it.
-    public struct Participant: Content, Equatable {
+    public struct Participant: Content, ValidationSetable, Equatable {
         
         /// The email address of the person who receives a copy of the invoice.
         ///
@@ -15,6 +15,19 @@ extension Invoice {
         ///     Invoice.Participant(email: "participant@example.com")
         public init(email: String)throws {
             self.email = email
+        }
+        
+        /// See `ValidationSetable.setterValidations()`.
+        public func setterValidations() -> SetterValidations<Invoice.Participant> {
+            var validations = SetterValidations(Participant.self)
+            
+            validations.set(\.email) { email in
+                guard email.count <= 260 else {
+                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`email` property must have a length between 0 and 260")
+                }
+            }
+            
+            return validations
         }
     }
 }
