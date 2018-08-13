6ef10542162fd079f6de3b72ed1e6fac5dd01529
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-13 14:39:39 -0500

    Created Invoices.create(invoice:) method

--- Sources/PayPal/API/Controllers/Invoices.swift
+++ Sources/PayPal/API/Controllers/Invoices.swift
@@ -18,4 +18,23 @@ public class Invoices: PayPalController {
         self.container = container
         self.resource = "invoicing/invoices"
     }
+    
+    /// Creates a draft invoice. To move the invoice from a draft to payable state,
+    /// you must [send the invoice](https://developer.paypal.com/docs/api/invoicing/v1/#invoices_send).
+    ///
+    /// In the JSON request body, include invoice details including merchant information. The `invoice` object must include an `items` array.
+    ///
+    /// A successful request returns the HTTP `201 Created` status code and a JSON response body that shows invoice details.
+    ///
+    /// - Note: The merchant that you specify in an invoice must have a PayPal account in good standing.
+    ///
+    /// - Parameter invoice: The invoice data to save as a new invoice.
+    ///
+    /// - Returns: The saved Invoice. If an error response was sent back instead, it gets converted to a Swift error and the future wraps that instead.
+    public func create(invoice: Invoice) -> Future<Invoice> {
+        return Future.flatMap(on: self.container) { () -> EventLoopFuture<Invoice> in
+            let client = try self.container.make(PayPalClient.self)
+            return try client.post(self.path(), body: invoice, as: Invoice.self)
+        }
+    }
 }
