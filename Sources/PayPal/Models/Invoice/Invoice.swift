f11023e1911c87dd9db389dfb6855164f28af998
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-13 08:19:53 -0500

    Conformed Invoice struct to ValidationSetable protocol

--- Sources/PayPal/Models/Invoice/Invoice.swift
+++ Sources/PayPal/Models/Invoice/Invoice.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 /// An invoice, sent by a merchant to a client, for the merchant to receive a payment.ai
-public struct Invoice: Content, Equatable {
+public struct Invoice: Content, ValidationSetable, Equatable {
     
     /// The ID of the invoice.
     public let id: String?
@@ -45,8 +45,11 @@ public struct Invoice: Content, Equatable {
     
     /// The invoice number. If you omit this value, the default is the number that the API automatically increments from the last number.
     ///
+    /// This property can be set using the `Invoice.set(_:)` method
+    /// which will validate the new valie before asigning it to the property.
+    ///
     /// Maximum length: 25.
-    public var number: String?
+    public private(set) var number: String?
     
     /// The merchant information, such as business name, email, address, and so on.
     public var merchant: MerchantInfo
@@ -73,8 +76,11 @@ public struct Invoice: Content, Equatable {
     
     /// The reference data, such as PO number.
     ///
+    /// This property can be set using the `Invoice.set(_:)` method
+    /// which will validate the new valie before asigning it to the property.
+    ///
     /// Maximum length: 60.
-    public var reference: String?
+    public private(set) var reference: String?
     
     /// The invoice level discount, as a percent or an amount value.
     public var discount: Discount<Amount>?
@@ -102,23 +108,35 @@ public struct Invoice: Content, Equatable {
     
     /// The general terms of the invoice.
     ///
+    /// This property can be set using the `Invoice.set(_:)` method
+    /// which will validate the new valie before asigning it to the property.
+    ///
     /// Maximum length: 4000.
-    public var terms: String?
+    public private(set) var terms: String?
     
     /// A note to the invoice recipient. The note also appears on the invoice notification email.
     ///
+    /// This property can be set using the `Invoice.set(_:)` method
+    /// which will validate the new valie before asigning it to the property.
+    ///
     /// Maximum length: 4000.
-    public var note: String?
+    public private(set) var note: String?
     
     /// A private bookkeeping memo for the merchant.
     ///
+    /// This property can be set using the `Invoice.set(_:)` method
+    /// which will validate the new valie before asigning it to the property.
+    ///
     /// Maximum length: 500.
-    public var memo: String?
+    public private(set) var memo: String?
     
     /// The full URL to an external logo image. The logo must not be larger than 250 pixels wide by 90 pixels high. The logo must be stored on a secure server.
     ///
+    /// This property can be set using the `Invoice.set(_:)` method
+    /// which will validate the new valie before asigning it to the property.
+    ///
     /// Maximum length: 4000.
-    public var logo: String?
+    public private(set) var logo: String?
     
     /// Indicates whether the invoice enables the customer to enter a tip amount during payment.
     /// If `true`, the invoice shows a tip amount field so that the customer can enter a tip amount. If `false`, the invoice does not show a tip amount field.
@@ -132,4 +150,45 @@ public struct Invoice: Content, Equatable {
     /// - Note: If you are just using the Invoicing APIs to create and send invoices, leave this field blank.
     ///   The `template_id` is only needed if you use the Invoicing API to build a full invoicing solution that includes templates.
     public var template: String?
+    
+    /// See `ValidationSetable.setterValidations()`.
+    public func setterValidations() -> SetterValidations<Invoice> {
+        var validations = SetterValidations(Invoice.self)
+        
+        validations.set(\.number) { number in
+            guard number?.map(String.init).compactMap(Int.init).count == number?.count else {
+                throw PayPalError(status: .badRequest, identifier: "badValue", reason: "`number` property must container a whole number value")
+            }
+            guard number?.count ?? 0 <= 25 else {
+                throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`number` property must have a length of 25 or less")
+            }
+        }
+        validations.set(\.reference) { reference in
+            guard reference?.count ?? 0 <= 60 else {
+                throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`reference` property must have a length of 60 or less")
+            }
+        }
+        validations.set(\.terms) { terms in
+            guard terms?.count ?? 0 <= 4_000 else {
+                throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`terms` property must have a length of 4,000 or less")
+            }
+        }
+        validations.set(\.note) { note in
+            guard note?.count ?? 0 <= 4_000 else {
+                throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`note` property must have a length of 4,000 or less")
+            }
+        }
+        validations.set(\.memo) { memo in
+            guard memo?.count ?? 0 <= 500 else {
+                throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`memo` property must have a length of 500 or less")
+            }
+        }
+        validations.set(\.logo) { logo in
+            guard logo?.count ?? 0 <= 4_000 else {
+                throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`logo` property must have a length of 4,000 or less")
+            }
+        }
+        
+        return validations
+    }
 }
