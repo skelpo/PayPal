ae1b5fe213f4f2805f95c839627d1d02836cc1f7
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-14 11:14:24 -0500

    Created conformed CCEmail struct to ValidationSetable protocol

--- Sources/PayPal/Models/CCEmail.swift
+++ Sources/PayPal/Models/CCEmail.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 /// An email container for for sending notiifications email.
-public struct CCEmail: Content, Equatable {
+public struct CCEmail: Content, ValidationSetable, Equatable {
     
     /// A CC: email to which to send a notification email.
     ///
@@ -13,5 +13,22 @@ public struct CCEmail: Content, Equatable {
     ///     CCEmail(email: "holmer@shlock.com")
     public init(email: String?) {
         self.email = email
+    }
+    
+    /// See `ValidationSetable.setterValidations()`
+    public func setterValidations() -> SetterValidations<CCEmail> {
+        var validations = SetterValidations(CCEmail.self)
+        
+        validations.set(\.email) { email in
+            guard let email = email else { return }
+            guard email.count <= 3 && email.count >= 254 else {
+                throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`email` property must have a length between 3 and 254")
+            }
+            guard email.range(of: "^.+@[^\"\\-].+$", options: .regularExpression) != nil else {
+                throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`email` property must match RegEx pattern `^.+@[^\"\\-].+$`")
+            }
+        }
+        
+        return validations
     }
 }
