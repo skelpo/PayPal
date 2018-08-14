9d009247ea6da77bcf552c4a574f120d0a5a3363
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-14 17:16:04 -0500

    Documented Template.Settings.Metadata struct

--- Sources/PayPal/Models/Template/TemplateSettingsMetadata.swift
+++ Sources/PayPal/Models/Template/TemplateSettingsMetadata.swift
@@ -1,9 +1,16 @@
 import Vapor
 
 extension Template.Settings {
+    
+    /// Additional metadata for a `Template.Settings. object.
     public struct Metadata: Content, Equatable {
+        
+        /// Indicates whether to show or hide this field.
         public var hidden: Bool?
         
+        /// Creates a new `Template.Settings.Metadata` instance.
+        ///
+        ///     Template.Settings.Metadata(hidden: true)
         public init(hidden: Bool?) {
             self.hidden = hidden
         }
