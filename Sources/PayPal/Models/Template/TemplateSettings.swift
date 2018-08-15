bcf25bb5f2d5396c528daebb7cee1bbcb0bd4ae2
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-15 06:47:59 -0500

    Created Template.Settings struct

--- Sources/PayPal/Models/Template/TemplateSettings.swift
+++ Sources/PayPal/Models/Template/TemplateSettings.swift
@@ -1,5 +1,13 @@
+import Vapor
+
 extension Template {
-    public struct Settings {
+    public struct Settings: Content, Equatable {
+        public var field: Field?
+        public var preference: Metadata?
         
+        public init(field: Field?, preference: Metadata?) {
+            self.field = field
+            self.preference = preference
+        }
     }
 }
