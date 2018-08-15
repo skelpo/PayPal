5a7fa99eb053e8968e61daf3f1a2c50a7ab99001
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-15 09:58:52 -0500

    Created Template struct

--- Sources/PayPal/Models/Template/Template.swift
+++ Sources/PayPal/Models/Template/Template.swift
@@ -1,1 +1,25 @@
-public struct Template {}
+import Vapor
+
+public struct Template: Content, Equatable {
+    public let id: String?
+    public let custom: Bool?
+    public let links: [LinkDescription]?
+    
+    public var name: String?
+    public var `default`: Bool?
+    public var data: Data?
+    public var settings: [Settings]?
+    public var measureUnit: Invoice.Measure?
+    
+    public init(name: String?, default: Bool?, data: Data?, settings: [Settings]?, measureUnit: Invoice.Measure?) {
+        self.id = nil
+        self.custom = nil
+        self.links = nil
+        
+        self.name = name
+        self.default = `default`
+        self.data = data
+        self.settings = settings
+        self.measureUnit = measureUnit
+    }
+}
