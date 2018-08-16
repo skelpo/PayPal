d3cd5ffb3aeec7798f41b71de28ccc9abf2135d6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-16 18:04:05 -0500

    Conformed BusinessOwner.Address struct to ValidationSetable protocol

--- Sources/PayPal/Models/BusinessOwner/BusinessOwnerAddress.swift
+++ Sources/PayPal/Models/BusinessOwner/BusinessOwnerAddress.swift
@@ -3,36 +3,51 @@ import Vapor
 extension BusinessOwner {
     
     /// The address object structure used for a business owner.
-    public struct Address: Content, Equatable {
+    public struct Address: Content, ValidationSetable, Equatable {
         
         /// The address type.
         public var type: AddressType
         
         /// The first line of the address. For example, number, street, and so on.
         ///
+        /// This property can be set using the `Address.set(_:)` method. This method
+        /// validates the new value before assigning it to the property.
+        ///
         /// Maximum length: 300.
-        public var line1: String
+        public private(set) var line1: String
         
         /// The second line of the address. For example, suite, apt number, and so on.
         ///
+        /// This property can be set using the `Address.set(_:)` method. This method
+        /// validates the new value before assigning it to the property.
+        ///
         /// Maximum length: 300.
-        public var line2: String?
+        public private(set) var line2: String?
         
         /// The third line of the address. For example, the street complement for Brazil, direction text, such as next to Walmart,
         /// or a landmark in an Indian address.
         ///
+        /// This property can be set using the `Address.set(_:)` method. This method
+        /// validates the new value before assigning it to the property.
+        ///
         /// Maximum length: 300.
-        public var line3: String?
+        public private(set) var line3: String?
         
         /// The suburb or neighborhood.
         ///
+        /// This property can be set using the `Address.set(_:)` method. This method
+        /// validates the new value before assigning it to the property.
+        ///
         /// Maximum length: 300.
-        public var suburb: String?
+        public private(set) var suburb: String?
         
         /// The city.
         ///
+        /// This property can be set using the `Address.set(_:)` method. This method
+        /// validates the new value before assigning it to the property.
+        ///
         /// Maximum length: 120.
-        public var city: String
+        public private(set) var city: String
         
         /// The [code](https://developer.paypal.com/docs/integration/direct/rest/state-codes/) for a US state or the
         /// equivalent for other countries. Required for transactions if the address is in one of these countries:
@@ -96,6 +111,53 @@ extension BusinessOwner {
             self.state = state
             self.country = country
             self.postalCode = postalCode
+        }
+        
+        
+        /// See `ValidationSetable.setterValidations()`
+        public func setterValidations() -> SetterValidations<BusinessOwner.Address> {
+            var validations = SetterValidations(Address.self)
+            
+            validations.set(\.line1) { line1 in
+                guard line1.count <= 300 else {
+                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`line1` property length can be no longer 300")
+                }
+            }
+            validations.set(\.line2) { line2 in
+                guard line2?.count ?? 0 <= 300 else {
+                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`line2` property length can be no longer 300")
+                }
+            }
+            validations.set(\.line3) { line3 in
+                guard line3?.count ?? 0 <= 300 else {
+                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`line3` property length can be no longer 300")
+                }
+            }
+            validations.set(\.suburb) { suburb in
+                guard suburb?.count ?? 0 <= 300 else {
+                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`suburb` property length can be no longer 300")
+                }
+            }
+            validations.set(\.city) { city in
+                guard city.count <= 120 else {
+                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`city` property length can be no longer 120")
+                }
+            }
+            validations.set(\.state) { state in
+                guard state?.count ?? 0 <= 40 else {
+                    throw PayPalError(
+                        status: .badRequest,
+                        identifier: "invalidLength", reason: "`state` property length can be no longer than 40 1-byte characters"
+                    )
+                }
+            }
+            validations.set(\.country) { code in
+                guard code.range(of: "^([A-Z]{2}|C2)$", options: .regularExpression) != nil else {
+                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`country` property must match `([A-Z]{2}|C2)$` RegEx pattern")
+                }
+            }
+            
+            return validations
         }
     }
 }
