528cd0c5a85216d16279c6bc1935c09b45357099
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-20 16:01:43 -0500

    Documented PhoneType enum

--- Sources/PayPal/Models/PhoneNumber/PhoneType.swift
+++ Sources/PayPal/Models/PhoneNumber/PhoneType.swift
@@ -1,12 +1,29 @@
 import Vapor
 
+/// The type purpose of the phone that the phone number is connected to.
 public enum PhoneType: String, Hashable, CaseIterable, Content {
+    
+    /// `HOME`
     case home = "HOME"
+    
+    /// `MOBILE`
     case mobile = "MOBILE"
+    
+    /// `BUSINESS`
     case business = "BUSINESS"
+    
+    /// `WORK`
     case work = "WORK"
+    
+    /// `CUSTOMER_SERVICE`
     case customerService = "CUSTOMER_SERVICE"
+    
+    /// `FAX`
     case fax = "FAX"
+    
+    /// `OTHER`
     case other = "OTHER"
+    
+    /// `PAGER`
     case pager = "PAGER"
 }
