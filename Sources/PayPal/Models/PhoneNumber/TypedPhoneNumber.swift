f80a8ca4a9d36e40fcf65f8ecee80eb7359084c8
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-20 16:40:53 -0500

    Created TypedPhoneNumber struct

--- Sources/PayPal/Models/PhoneNumber/TypedPhoneNumber.swift
+++ Sources/PayPal/Models/PhoneNumber/TypedPhoneNumber.swift
@@ -1,1 +1,15 @@
+import Vapor
 
+public struct TypedPhoneNumber: Content, Equatable {
+    public var type: PhoneType
+    public var country: String
+    public var nationalNumber: String
+    public var `extension`: String?
+    
+    public init(type: PhoneType, country: String, nationalNumber: String, `extension`: String?) {
+        self.type = type
+        self.country = country
+        self.nationalNumber = nationalNumber
+        self.extension = `extension`
+    }
+}
