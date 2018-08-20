f575b778d0f0208ebc9665fa78b1bc4519c015f7
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-20 09:59:52 -0500

    Created BusinessOwner.ID struct

--- Sources/PayPal/Models/BusinessOwner/BusinessOwnerID.swift
+++ Sources/PayPal/Models/BusinessOwner/BusinessOwnerID.swift
@@ -1,3 +1,34 @@
+import Vapor
+
 extension BusinessOwner {
-    public struct ID {}
+    public struct ID: Content, Equatable {
+        public var type: IDType
+        public var value: String
+        public var masked: Bool?
+        public var issuerCountry: String
+        public var issuerState: String?
+        public var issuerCity: String?
+        public var placeOfIssue: String?
+        public var description: String?
+        
+        public init(
+            type: IDType,
+            value: String,
+            masked: Bool?,
+            issuerCountry: String,
+            issuerState: String?,
+            issuerCity: String?,
+            placeOfIssue: String?,
+            description: String?
+        ) {
+            self.type = type
+            self.value = value
+            self.masked = masked
+            self.issuerCountry = issuerCountry
+            self.issuerState = issuerState
+            self.issuerCity = issuerCity
+            self.placeOfIssue = placeOfIssue
+            self.description = description
+        }
+    }
 }
