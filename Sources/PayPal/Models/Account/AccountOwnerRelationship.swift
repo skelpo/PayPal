5791c79b5d5b5b6a8cd44e4d62439d954fc08c79
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-21 08:18:26 -0500

    Documented AccountOwnerRelationship struct

--- Sources/PayPal/Models/Account/AccountOwnerRelationship.swift
+++ Sources/PayPal/Models/Account/AccountOwnerRelationship.swift
@@ -1,10 +1,23 @@
 import Vapor
 
+/// A family relationship of an account owner.
 public struct AccountOwnerRelationship: Content, Equatable {
+    
+    /// The name of the familial relation.
     public var name: Name
+    
+    /// The type of familial relationship. Value is `MOTHER`.
     public var relation: String
+    
+    /// The country code for the nationality of the familial relation.
+    ///
+    /// Length: 2. Pattern: `^([A-Z]{2}|C2)$`.
     public var country: String
     
+    
+    /// Creates a new `AccountOwnerRelationship` instance.
+    ///
+    ///     AccountOwnerRelationship(name: Name(prefix: nil, given: "Abe", surname: "Lincon", middle: nil, suffix: nil, full: "Abe Lincon", country: "US")
     public init(name: Name, country: String) {
         self.name = name
         self.country = country
