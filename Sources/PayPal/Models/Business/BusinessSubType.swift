cee7e00b41ab7c21c047b1c47cdbd7321ebea790
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-21 17:46:07 -0500

    Fixed cases in Business.SubType enum cases

--- Sources/PayPal/Models/Business/BusinessSubType.swift
+++ Sources/PayPal/Models/Business/BusinessSubType.swift
@@ -6,27 +6,27 @@ extension Business {
     public enum SubType: String, Hashable, CaseIterable, Content {
         
         /// `ASSO_TYPE_INCORPORATED`
-        case ASSO_TYPE_INCORPORATED = "ASSO_TYPE_INCORPORATED"
+        case assoIncorporated = "ASSO_TYPE_INCORPORATED"
         
         /// `ASSO_TYPE_NON_INCORPORATED`
-        case ASSO_TYPE_NON_INCORPORATED = "ASSO_TYPE_NON_INCORPORATED"
+        case assoNonIncorporated = "ASSO_TYPE_NON_INCORPORATED"
         
         /// `GOVT_TYPE_EMANATION`
-        case GOVT_TYPE_EMANATION = "GOVT_TYPE_EMANATION"
+        case govtEmanation = "GOVT_TYPE_EMANATION"
         
         /// `GOVT_TYPE_ENTITY`
-        case GOVT_TYPE_ENTITY = "GOVT_TYPE_ENTITY"
+        case govtEntity = "GOVT_TYPE_ENTITY"
         
         /// `GOVT_TYPE_ESTD_COMM`
-        case GOVT_TYPE_ESTD_COMM = "GOVT_TYPE_ESTD_COMM"
+        case govtEstdComm = "GOVT_TYPE_ESTD_COMM"
         
         /// `GOVT_TYPE_ESTD_FC`
-        case GOVT_TYPE_ESTD_FC = "GOVT_TYPE_ESTD_FC"
+        case govtEstdFc = "GOVT_TYPE_ESTD_FC"
         
         /// `GOVT_TYPE_ESTD_ST_TR`
-        case GOVT_TYPE_ESTD_ST_TR = "GOVT_TYPE_ESTD_ST_TR"
+        case govtEstdStTr = "GOVT_TYPE_ESTD_ST_TR"
         
         /// `UNSELECTED`
-        case UNSELECTED = "UNSELECTED"
+        case unselected = "UNSELECTED"
     }
 }
