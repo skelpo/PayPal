6dbe77481cd8bd379cd0dc93ab500a01166f3aac
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-22 16:18:29 -0500

    Created CustomerService struct

--- Sources/PayPal/Models/CustomerService/CustomerService.swift
+++ Sources/PayPal/Models/CustomerService/CustomerService.swift
@@ -1,1 +1,13 @@
-public struct CustomerService {}
+import Vapor
+
+public struct CustomerService: Content, Equatable {
+    public var email: EmailAddress
+    public var phone: PhoneNumber?
+    public var message: [Message]?
+    
+    public init(email: EmailAddress, phone: PhoneNumber?, message: [Message]?) {
+        self.email = email
+        self.phone = phone
+        self.message = message
+    }
+}
