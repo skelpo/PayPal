58b610d0aeaa5a07e675345a9ca4e6331f2bd6e8
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-22 12:46:08 -0500

    Created CustomerService.Message struct

--- Sources/PayPal/Models/CustomerService/ServiceMessage.swift
+++ Sources/PayPal/Models/CustomerService/ServiceMessage.swift
@@ -1,7 +1,27 @@
 import Vapor
 
 extension CustomerService {
-    public struct Message {}
+    public struct Message {
+        public var type: MessageType
+        public var headline: String?
+        public var logo: String?
+        public var serviceImage: String?
+        public var sellerMessage: String
+        
+        public init(
+            type: MessageType,
+            headline: String?,
+            logo: String?,
+            serviceImage: String?,
+            sellerMessage: String
+        )throws {
+            self.type = type
+            self.headline = headline
+            self.logo = logo
+            self.serviceImage = serviceImage
+            self.sellerMessage = sellerMessage
+        }
+    }
 }
 
 extension CustomerService.Message {
