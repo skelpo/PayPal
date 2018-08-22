4ab34687b56c8f4e64d5f9e21b45d81a2bbf369f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-22 17:00:43 -0500

    Conformed TimelessDate struct to ValidationSetable protocol

--- Sources/PayPal/Models/TimelessDate.swift
+++ Sources/PayPal/Models/TimelessDate.swift
@@ -1,14 +1,17 @@
 import Vapor
 
 /// Date information without a timestamp.
-public struct TimelessDate: Content, Equatable {
+public struct TimelessDate: Content, ValidationSetable, Equatable {
     
     /// The stand-alone date, in [Internet date and time format](https://tools.ietf.org/html/rfc3339#section-5.6). To represent special legal values,
     /// such as a date of birth, you should use dates with no associated time or time-zone data. Whenever possible, use the standard `date_time` type.
     /// This regular expression does not validate all dates. For example, February 31 is valid and nothing is known about leap years.
     ///
+    /// This property can be set using the `TimelessDate.set(_:)` method, This method
+    /// validates the new value before assigning it to the property.
+    ///
     /// Length: 10. Pattern: `^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$`.
-    public var date: String?
+    public private(set) var date: String?
     
     
     /// Creates a new `TimelessDate` instance.
@@ -16,5 +19,19 @@ public struct TimelessDate: Content, Equatable {
     ///     TimelessDate(date: "1517-10-31")
     public init(date: String?)throws {
         self.date = date
+    }
+    
+    /// See `ValidationSetable.setterValidations()`
+    public func setterValidations() -> SetterValidations<TimelessDate> {
+        var validations = SetterValidations(TimelessDate.self)
+        
+        validations.set(\.date) { date in
+            let pattern = "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$"
+            guard date?.range(of: pattern, options: .regularExpression) != nil else {
+                throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`date` value must match RegEx pattern `\(pattern)`")
+            }
+        }
+        
+        return validations
     }
 }
