3eea88b38f3ea79d8989a55a1ac34a21e209dce3
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-23 10:09:04 -0500

    Conformed PercentRange to RangeExpression and RandomAccessCollection protocols

--- Sources/PayPal/Models/Money/PercentRange.swift
+++ Sources/PayPal/Models/Money/PercentRange.swift
@@ -9,3 +9,41 @@ public struct PercentRange: Content, Equatable {
         self.maximum = max
     }
 }
+
+extension PercentRange: RangeExpression {
+    public typealias Bound = Int
+    
+    public func relative<C>(to collection: C) -> Range<Int> where C : Collection, PercentRange.Bound == C.Index {
+        let start: Int = collection.startIndex > self.minimum && collection.startIndex < 100 ? collection.startIndex : self.minimum
+        let end: Int = collection.endIndex < self.maximum && collection.endIndex > 0 ? collection.endIndex : self.maximum
+        
+        return Range<Int>.init(uncheckedBounds: (start, end))
+    }
+    
+    public func contains(_ element: Int) -> Bool {
+        return element >= minimum && element <= maximum
+    }
+}
+
+extension PercentRange: RandomAccessCollection {
+    public typealias Index = Int
+    public typealias Element = Int
+    
+    public var startIndex: Int { return self.minimum }
+    public var endIndex: Int { return self.maximum }
+    
+    public subscript(position: Int) -> Int {
+        guard self.contains(position) else {
+            return Array(self.minimum...self.maximum)[position]
+        }
+        return position
+    }
+    
+    public func index(before i: Int) -> Int {
+        return i - 1
+    }
+    
+    public func index(after i: Int) -> Int {
+        return i + 1
+    }
+}
