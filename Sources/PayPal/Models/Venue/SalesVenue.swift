3639f5d24f729b78bf097cb94aa1db589c7f9f0e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-23 08:23:08 -0500

    Documented SalesVenue struct

--- Sources/PayPal/Models/Venue/SalesVenue.swift
+++ Sources/PayPal/Models/Venue/SalesVenue.swift
@@ -1,10 +1,21 @@
 import Vapor
 
+/// A sales venue for a business to seel products through.
 public struct SalesVenue: Content, Equatable {
+    
+    /// The type of sales venue for the business.
     public var type: Venue?
+    
+    /// The eBay ID.
     public var ebayID: String?
+    
+    /// The description of the business sales venue.
     public var description: String?
     
+    
+    /// Creates a new `SalesVenue` instance.
+    ///
+    ///     SalesVenue(type: .ebay, ebayID: "D6013453-B76E-4CDD-A021-DFD31A4031FE", description: "Online Ebay Store")
     public init(type: Venue?, ebayID: String?, description: String?) {
         self.type = type
         self.ebayID = ebayID
