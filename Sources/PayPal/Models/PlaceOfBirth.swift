a08db228ba05deba9886a2e984ec721c54c13bcd
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-08-24 07:45:22 -0500

    Created BirthPlace struct

---
+++ Sources/PayPal/Models/PlaceOfBirth.swift
@@ -0,0 +1,25 @@
+import Vapor
+
+/// The birthplace of a human, robot, orangutang, or anything else that originates on planet earth.
+public struct BirthPlace: Content, Equatable {
+    
+    /// The two-character ISO 3166-1 code that identifies the country or region.
+    ///
+    /// - Note: he country code for Great Britain is `GB` and not `UK` as used in the top-level domain names for that country.
+    ///   Use the `C2` country code for China worldwide for comparable uncontrolled price (CUP) method, bank card, and cross-border transactions.
+    ///
+    /// Length: 2. Pattern: `^([A-Z]{2}|C2)$`.
+    public var country: String?
+    
+    /// The city of birth.
+    public var city: String?
+    
+    
+    /// Creates a new `BirthPlace` instance.
+    ///
+    ///     BirthPlace(country: "US", city: "Seattle")
+    public init(country: String?, city: String?)throws {
+        self.country = country
+        self.city = city
+    }
+}
