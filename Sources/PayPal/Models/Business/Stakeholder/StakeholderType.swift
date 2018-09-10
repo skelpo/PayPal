fb5141e6ac6fde639cfaed9cc9548f60b015226c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-10 08:06:41 -0500

    Documented StakeholderType enum

--- Sources/PayPal/Models/Business/Stakeholder/StakeholderType.swift
+++ Sources/PayPal/Models/Business/Stakeholder/StakeholderType.swift
@@ -1,14 +1,32 @@
 import Vapor
 
 extension Business.Stakeholder {
+    
+    /// The type of stakeholder in a business.
     public enum StakeholderType: String, Hashable, CaseIterable, Content {
+        
+        /// `CHAIRMAN`
         case chairman = "CHAIRMAN"
+        
+        /// `PARTNER`
         case partner = "PARTNER"
+        
+        /// `PARTNER_BUSINESS`
         case partnerBusiness = "PARTNER_BUSINESS"
+        
+        /// `SECRETARY`
         case secretary = "SECRETARY"
+        
+        /// `TREASURER`
         case treasurer = "TREASURER"
+        
+        /// `DIRECTOR`
         case director = "DIRECTOR"
+        
+        /// `BENEFICIAL_OWNER`
         case beneficialOwner = "BENEFICIAL_OWNER"
+        
+        /// `BENEFICIAL_OWNER_BUSINESS`
         case beneficialOwnerBusiness = "BENEFICIAL_OWNER_BUSINESS"
     }
 }
