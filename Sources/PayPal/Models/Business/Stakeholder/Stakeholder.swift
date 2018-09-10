1424a9819d1c8d7a0d365fe7ee8dbca0a54d5ec6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-10 09:30:26 -0500

    Conformed Stakeholder struct to ValidationSetable protocol

--- Sources/PayPal/Models/Business/Stakeholder/Stakeholder.swift
+++ Sources/PayPal/Models/Business/Stakeholder/Stakeholder.swift
@@ -3,7 +3,7 @@ import Vapor
 extension Business {
     
     /// A person or business that acts as a stakeholder to a business.
-    public struct Stakeholder: Content, Equatable {
+    public struct Stakeholder: Content, ValidationSetable, Equatable {
         
         /// The percentage ownership for a stakeholder. Pertains only to the `BENEFICIAL_OWNER` type.
         public var ownership: String?
@@ -12,7 +12,12 @@ extension Business {
         public var type: StakeholderType?
         
         /// The [two-character IS0-3166-1 country code](https://developer.paypal.com/docs/integration/direct/rest/country-codes/) of the country of residence.
-        public var country: String?
+        ///
+        /// This property can be set using the `Stakeholder.set(_:)` method. This method
+        /// verifies the new value before assigning it to the property.
+        ///
+        /// Pattern: `^([A-Z]{2}|C2)$`
+        public private(set) var country: String?
         
         /// The date of birth, in [Internet date and time `full-date` format](https://tools.ietf.org/html/rfc3339#section-5.6). Supports the `YYYY-MM-DD` format.
         public var birth: TimelessDate?
@@ -65,6 +70,20 @@ extension Business {
             self.phones = phones
             self.ids = ids
             self.birthplace = birthplace
+        }
+        
+        /// See `ValidationSetable.setterValidations()`.
+        public func setterValidations() -> SetterValidations<Business.Stakeholder> {
+            var validations = SetterValidations(Stakeholder.self)
+            
+            validations.set(\.country) { country in
+                guard let country = country else { return }
+                guard country.range(of: "^([A-Z]{2}|C2)$", options: .regularExpression) != nil else {
+                    throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`country` value must match RegEx pattern `^([A-Z]{2}|C2)$`")
+                }
+            }
+            
+            return validations
         }
     }
 }
