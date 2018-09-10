34d75e3119fbc1619c09902dd65961e59434c970
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-10 08:50:18 -0500

    Created Business.Stakeholder struct

--- Sources/PayPal/Models/Business/Stakeholder/Stakeholder.swift
+++ Sources/PayPal/Models/Business/Stakeholder/Stakeholder.swift
@@ -1,3 +1,37 @@
+import Vapor
+
 extension Business {
-    public struct Stakeholder {}
+    public struct Stakeholder: Content, Equatable {
+        public var ownership: String?
+        public var type: StakeholderType?
+        public var country: String?
+        public var birth: TimelessDate?
+        public var name: PayPal.Name?
+        public var addresses: [Address]?
+        public var phones: [TypedPhoneNumber]?
+        public var ids: [Identification]?
+        public var birthplace: BirthPlace?
+        
+        public init(
+            ownership: String?,
+            type: StakeholderType?,
+            country: String?,
+            birth: TimelessDate?,
+            name: PayPal.Name?,
+            addresses: [Address]?,
+            phones: [TypedPhoneNumber]?,
+            ids: [Identification]?,
+            birthplace: BirthPlace?
+        ) {
+            self.ownership = ownership
+            self.type = type
+            self.country = country
+            self.birth = birth
+            self.name = name
+            self.addresses = addresses
+            self.phones = phones
+            self.ids = ids
+            self.birthplace = birthplace
+        }
+    }
 }
