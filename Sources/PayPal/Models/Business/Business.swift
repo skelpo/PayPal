a9e984d3999ba00a973fc6018b37bca186f61542
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-10 12:41:40 -0500

    Conformed Business struct to ValidationSetable protocol

--- Sources/PayPal/Models/Business/Business.swift
+++ Sources/PayPal/Models/Business/Business.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 /// Business information, primerily used for a merchant account.
-public struct Business: Content, Equatable {
+public struct Business: Content, ValidationSetable, Equatable {
     
     /// The type of business, such as corporation or sole proprietorship.
     public var type: BusinessType
@@ -30,18 +30,27 @@ public struct Business: Content, Equatable {
     
     /// The category of the business. Either `merchant_category_code` or both `category` and `sub_category` are required.
     ///
+    /// This property can be set using the `Business.set(_:)` method.
+    /// This method validates the new value before assigning it to the property.
+    ///
     /// Length: 4.
-    public var category: String?
+    public private(set) var category: String?
     
     /// The sub-category of the business. Either `merchant_category_code` or both `category` and `sub_category` are required.
     ///
+    /// This property can be set using the `Business.set(_:)` method.
+    /// This method validates the new value before assigning it to the property.
+    ///
     /// Length: 4.
-    public var subCategory: String?
+    public private(set) var subCategory: String?
     
     /// A merchant category code. Either `merchant_category_code` or both `category` and `sub_category` are required.
     ///
+    /// This property can be set using the `Business.set(_:)` method.
+    /// This method validates the new value before assigning it to the property.
+    ///
     /// Length: 4.
-    public var merchantCategory: String?
+    public private(set) var merchantCategory: String?
     
     /// The date when the merchant's business was established, in [Internet date and time `full-date` format](https://tools.ietf.org/html/rfc3339#section-5.6).
     public var establishedDate: TimelessDate?
@@ -64,9 +73,12 @@ public struct Business: Content, Equatable {
     
     /// The [two-character ISO 3166-1 code](https://developer.paypal.com/docs/integration/direct/rest/country-codes/) that identifies the country or region.
     ///
+    /// This property can be set using the `Business.set(_:)` method.
+    /// This method validates the new value before assigning it to the property.
+    ///
     /// - Note: The country code for Great Britain is GB and not UK as used in the top-level domain names for that country.
     ///   Use the C2 country code for China worldwide for comparable uncontrolled price (CUP) method, bank card, and cross-border transactions.
-    public var country: String?
+    public private(set) var country: String?
     
     /// An array of business stakeholder information.
     public var stakeholders: [Stakeholder]?
@@ -148,6 +160,38 @@ public struct Business: Content, Equatable {
         self.country = country
         self.stakeholders = stakeholders
         self.designation = designation
+    }
+    
+    /// See `ValidationSetable.setterValidations()`.
+    public func setterValidations() -> SetterValidations<Business> {
+        var validations = SetterValidations(Business.self)
+        
+        validations.set(\.category) { category in
+            guard let category = category else { return }
+            guard category.count == 4 else {
+                throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`category` value must have a length of 4")
+            }
+        }
+        validations.set(\.subCategory) { category in
+            guard let category = category else { return }
+            guard category.count == 4 else {
+                throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`sub_category` value must have a length of 4")
+            }
+        }
+        validations.set(\.merchantCategory) { category in
+            guard let category = category else { return }
+            guard category.count == 4 else {
+                throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`merchant_category` value must have a length of 4")
+            }
+        }
+        validations.set(\.country) { country in
+            guard let country = country else { return }
+            guard country.range(of: "^([A-Z]{2}|C2)$", options: .regularExpression) != nil else {
+                throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`country` value must match RegEx pattern `^([A-Z]{2}|C2)$`")
+            }
+        }
+        
+        return validations
     }
     
     enum CodingKeys: String, CodingKey {
