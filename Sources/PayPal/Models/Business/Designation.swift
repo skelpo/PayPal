ef89ccb02a79a99a4c6f2e4d42edfcdb83c2e902
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-10 10:36:29 -0500

    Dodumented Business.Designation struct

--- Sources/PayPal/Models/Business/Designation.swift
+++ Sources/PayPal/Models/Business/Designation.swift
@@ -1,10 +1,20 @@
 import Vapor
 
 extension Business {
+    
+    /// The business owner's title and his/her specific area of responsibility.
     public struct Designation: Content, Equatable {
+        
+        /// The business owner title.
         public var title: String?
+        
+        /// The organizational unit that corresponds to the specific business segment or area of responsibility in a company.
         public var area: String?
         
+        
+        /// Creates a new `Business.Designation` struct.
+        ///
+        ///     Designation(title: "CTO", area: "Software Engineering")
         public init(title: String?, area: String?) {
             self.title = title
             self.area = area
