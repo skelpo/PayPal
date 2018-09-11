38b035b8d3bae6279079ef3f349e31ce6ea39852
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-11 10:06:27 -0500

    Documented PartnerOptions struct

--- Sources/PayPal/Models/Options/PartnerOptions.swift
+++ Sources/PayPal/Models/Options/PartnerOptions.swift
@@ -1,8 +1,16 @@
 import Vapor
 
+/// Partner-specific options for an account.
 public struct PartnerOptions: Content, Equatable {
+    
+    /// An array of key-and-value pairs that contain custom partner information.
     public var fields: [KeyValue]
     
+    /// Creates a new `PartnerOptions` instance.
+    ///
+    ///     PartnerOption(fields: ["key": "value"])
+    ///
+    /// - Parameter fields: An array of key-and-value pairs that contain custom partner information.
     public init(fields: [KeyValue]) {
         self.fields = fields
     }
