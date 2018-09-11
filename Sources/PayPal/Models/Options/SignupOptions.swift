21ce4851b7fc5778a8174d3ddcda1eae348ba410
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-11 10:55:43 -0500

    Documented SignupOptions struct

--- Sources/PayPal/Models/Options/SignupOptions.swift
+++ Sources/PayPal/Models/Options/SignupOptions.swift
@@ -1,11 +1,28 @@
 import Vapor
 
+/// Options, preferences, and agreements for an account.
 public struct SignupOptions: Content, Equatable {
+    
+    /// The partner-specific options for the account.
     public var partner: PartnerOptions?
+    
+    /// An array of legal agreements.
     public var legal: [LegalAgreement]?
+    
+    /// The web experience preferences for the customer.
     public var web: WebExperiencePreference?
+    
+    /// The notification options.
     public var notification: NotificationOptions?
     
+    
+    /// Creates a new `SignupOptions` instance.
+    ///
+    /// - Parameters:
+    ///   - partner: The partner-specific options for the account.
+    ///   - legal: An array of legal agreements.
+    ///   - web: The web experience preferences for the customer.
+    ///   - notification: The notification options.
     public init(
         partner: PartnerOptions?,
         legal: [LegalAgreement]?,
