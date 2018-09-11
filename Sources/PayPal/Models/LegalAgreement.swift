f6c17dfc4b1675e17165a6e7d530fc645558db7b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-11 08:46:15 -0500

    Documented LegalAgreement.AgreementType enum

--- Sources/PayPal/Models/LegalAgreement.swift
+++ Sources/PayPal/Models/LegalAgreement.swift
@@ -3,8 +3,14 @@ import Vapor
 public struct LegalAgreement {}
 
 extension LegalAgreement {
+    
+    /// The type of legal agreement between a merchant and partner.
     public enum AgreementType: String, Hashable, CaseIterable, Content {
+        
+        /// `USER_AGREEMENT`
         case userAgreement = "USER_AGREEMENT"
+        
+        /// `FINANCIAL_BINDING_AUTHORITY`
         case financialAuthority = "FINANCIAL_BINDING_AUTHORITY"
     }
 }
