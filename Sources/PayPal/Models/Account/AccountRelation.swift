4564432dffdf2a47b88f6917fdb7e23f01b5df64
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-11 17:26:43 -0500

    Created AccountRelation struct

--- Sources/PayPal/Models/Account/AccountRelation.swift
+++ Sources/PayPal/Models/Account/AccountRelation.swift
@@ -1,1 +1,11 @@
-public struct AccountRelation {}
+import Vapor
+
+public struct AccountRelation: Content, Equatable {
+    public var type: RelationType
+    public var payer: String?
+    
+    public init(type: RelationType, payer: String?) {
+        self.type = type
+        self.payer = payer
+    }
+}
