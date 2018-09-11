2a0cc9f6119f881f82b29132025503bc18110fb4
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-11 11:37:05 -0500

    Documented FinancialInstrument struct

--- Sources/PayPal/Models/FinancialInstrument.swift
+++ Sources/PayPal/Models/FinancialInstrument.swift
@@ -1,10 +1,21 @@
 import Vapor
 
+/// A financial instrument that can be used to make and receive payments (or something like that, Paypal doesn't say).
 public struct FinancialInstrument: Content, Equatable {
+    
+    /// The financial instrument type. Currently supports `BANK` only.
     public let type: String?
+    
+    /// The bank account type.
     public let accountType: String
+    
+    /// The PayPal-generated financial instrument ID.
     public var id: String?
     
+    
+    /// Creates a new `FinancialInstrument` instance.
+    ///
+    /// - Parameter id: The PayPal-generated financial instrument ID. Defaults to `nil`.
     public init(id: String? = nil) {
         self.type = "BANK"
         self.id = id
