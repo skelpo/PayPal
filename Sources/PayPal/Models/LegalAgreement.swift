3595a5ee446fac3c90e3ce9cc7cfeef8132e4e60
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-11 08:51:14 -0500

    Created LegalAgreement struct

--- Sources/PayPal/Models/LegalAgreement.swift
+++ Sources/PayPal/Models/LegalAgreement.swift
@@ -1,6 +1,27 @@
 import Vapor
 
-public struct LegalAgreement {}
+/// A legal agreement between a maerchant and a partner.
+public struct LegalAgreement {
+    
+    /// Indicates whether the merchant accepted the legal agreement.
+    public var accepted: Bool
+    
+    /// The type of legal agreement between the merchant and partner.
+    public var type: AgreementType
+    
+    
+    /// Creates a new `LegalAgreement` instance.
+    ///
+    ///     LegalAgreement(accepted: false, type: .userAgreement)
+    ///
+    /// - Parameters:
+    ///   - accepted: Indicates whether the merchant accepted the legal agreement.
+    ///   - type: The type of legal agreement between the merchant and partner.
+    public init(accepted: Bool, type: AgreementType) {
+        self.accepted = accepted
+        self.type = type
+    }
+}
 
 extension LegalAgreement {
     
