5f0f39a617ce9872667e345a3aa2dbb1beb4e826
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-12 11:50:24 -0500

    Documented CreatedMerchantResponse struct and removed public initializer

--- Sources/PayPal/Models/Account/Responses/CreatedMerchantResponse.swift
+++ Sources/PayPal/Models/Account/Responses/CreatedMerchantResponse.swift
@@ -1,20 +1,27 @@
 import Vapor
 
+/// The body of the response when a new merchant account is created.
 public struct CreatedMerchantResponse: Content, Equatable {
+    
+    /// An array of request-related [HATEOAS links](https://developer.paypal.com/docs/api/overview/#hateoas-links).
     public let links: [LinkDescription]?
     
+    /// The payer ID. If the account was not created, this value is blank.
+    ///
+    /// Maximum length: 127.
     public var payer: String?
-    public var partnerExternalID: String?
-    public var authCode: String?
-    public var custom: [KeyValue]?
-    public var errors: [AccountError]?
     
-    public init(payer: String?, partnerExternalID: String?, authCode: String?, custom: [KeyValue]?, errors: [AccountError]?) {
-        self.links = nil
-        self.payer = payer
-        self.partnerExternalID = partnerExternalID
-        self.authCode = authCode
-        self.custom = custom
-        self.errors = errors
-    }
+    /// The partner-specified ID for the account, which was passed in the `partner_merchant_external_id` request parameter.
+    ///
+    /// Maximum length: 127.
+    public var partnerExternalID: String?
+    
+    /// The merchant authorization code.
+    public var authCode: String?
+    
+    /// An array of key-and-value pairs that contain custom data. For example, `aa_token`.
+    public var custom: [KeyValue]?
+    
+    /// An array of errors, if any, that occurred during account creation.
+    public var errors: [AccountError]?
 }
