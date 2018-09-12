51709070e51255a14d4a0efdbae28ae611a31f5d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-12 06:09:42 -0500

    Created AccountError struct

--- Sources/PayPal/Models/Account/AccountError.swift
+++ Sources/PayPal/Models/Account/AccountError.swift
@@ -1,1 +1,21 @@
-public struct AccountError {}
+import Vapor
+
+public struct AccountError: Content, Error, Equatable {
+    public let information: String?
+    public let links: [LinkDescription]?
+    
+    public var name: String
+    public var message: String
+    public var debug: String
+    public var details: [Details]?
+    
+    public init(name: String, message: String, debug: String, details: [Details]?) {
+        self.information = nil
+        self.links = nil
+        
+        self.name = name
+        self.message = message
+        self.debug = debug
+        self.details = details
+    }
+}
