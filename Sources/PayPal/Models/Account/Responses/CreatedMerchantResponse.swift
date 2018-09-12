912c7537deac1124dad77fddb801c115a8582a08
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-12 11:57:19 -0500

    Conformed CreatedMerchantResponse struct to ValidationSetable protocol

--- Sources/PayPal/Models/Account/Responses/CreatedMerchantResponse.swift
+++ Sources/PayPal/Models/Account/Responses/CreatedMerchantResponse.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 /// The body of the response when a new merchant account is created.
-public struct CreatedMerchantResponse: Content, Equatable {
+public struct CreatedMerchantResponse: Content, ValidationSetable, Equatable {
     
     /// An array of request-related [HATEOAS links](https://developer.paypal.com/docs/api/overview/#hateoas-links).
     public let links: [LinkDescription]?
@@ -24,6 +24,26 @@ public struct CreatedMerchantResponse: Content, Equatable {
     
     /// An array of errors, if any, that occurred during account creation.
     public var errors: [AccountError]?
+    
+    /// See `ValidationSetable.setterValidations()`.
+    public func setterValidations() -> SetterValidations<CreatedMerchantResponse> {
+        var validations = SetterValidations(CreatedMerchantResponse.self)
+        
+        validations.set(\.payer) { payer in
+            guard let payer = payer else { return }
+            guard payer.count <= 127 else {
+                throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`payer` value length must be 127 or less")
+            }
+        }
+        validations.set(\.partnerExternalID) { partnerExternalID in
+            guard let partnerExternalID = partnerExternalID else { return }
+            guard partnerExternalID.count <= 127 else {
+                throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`partnerExternalID` value length must be 127 or less")
+            }
+        }
+        
+        return validations
+    }
     
     enum CodingKeys: String, CodingKey {
         case links, errors
