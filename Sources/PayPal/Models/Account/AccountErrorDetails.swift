f1f1cb19dcaa1a0b9984ad4c7837d29c349908ca
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-11 19:00:38 -0500

    Fixed location of doc comments for AccountError.Details properties

--- Sources/PayPal/Models/Account/AccountErrorDetails.swift
+++ Sources/PayPal/Models/Account/AccountErrorDetails.swift
@@ -5,18 +5,18 @@ extension AccountError {
     /// Additional details about an error.
     public struct Details: Content, Equatable {
         
-        /// The field that caused the error. If the field is in the body, set this value to the JSON pointer to that field. Required for client-side errors.
+        /// The unique and fine-grained application-level error code.
         public var issue: String
         
-        /// The value of the field that caused the error.
+        /// The field that caused the error. If the field is in the body, set this value to the JSON pointer to that field. Required for client-side errors.
         public var field: String?
+        
+        /// The value of the field that caused the error.
+        public var value: String?
         
         /// The location of the field that caused the error. Value is `body`, `path`, or `query`.
         ///
         /// Default: `body`.
-        public var value: String?
-        
-        /// The unique and fine-grained application-level error code.
         public var location: String?
         
         /// The human-readable description for an issue. The description MAY change over the lifetime of an API, so clients **MUST NOT** depend on this value.
@@ -26,12 +26,12 @@ extension AccountError {
         /// Creates a new `AccountError.Details` instance.
         ///
         /// - Parameters:
-        ///   - issue: The field that caused the error.
-        ///   - field: The value of the field that caused the error.
-        ///   - value: The location of the field that caused the error.
-        ///   - location: The unique and fine-grained application-level error code.
+        ///   - issue: The unique and fine-grained application-level error code.
+        ///   - field: The field that caused the error.
+        ///   - value: The value of the field that caused the error.
+        ///   - location: The location of the field that caused the error.
         ///   - description: The human-readable description for an issue.
-        public init(issue: String, field: String? = nil, value: String? = nil, location: String? = nil, description: String? = nil) {
+        public init(issue: String, field: String? = nil, value: String? = nil, location: String? = "body", description: String? = nil) {
             self.issue = issue
             self.field = field
             self.value = value
