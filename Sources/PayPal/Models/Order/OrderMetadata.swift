7515f84661f548b9f749369a1bd7bc1c5bb8a0d1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-13 15:29:26 -0500

    Documented Order.Metadata struct

--- Sources/PayPal/Models/Order/OrderMetadata.swift
+++ Sources/PayPal/Models/Order/OrderMetadata.swift
@@ -1,9 +1,16 @@
 import Vapor
 
 extension Order {
+    
+    /// The name-and-value pairs that contain external data, such as user, user feedback, score, and so on.
     public struct Metadata: Content, Equatable {
+        
+        /// An array of name-and-value pairs that contain data required by PayPal for transaction processing.
         public var data: [NameValue]?
         
+        /// Creates a new `Order.Metadata` instance.
+        ///
+        /// - Parameter data: An array of name-and-value pairs that contain data required by PayPal for transaction processing.
         public init(data: [NameValue]?) {
             self.data = data
         }
