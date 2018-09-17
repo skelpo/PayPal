2bef40d34fbda2d3b66ac6645ea937695b26baee
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-17 10:58:24 -0500

    Documented DisplayPhone struct

--- Sources/PayPal/Models/PhoneNumber/DisplayPhone.swift
+++ Sources/PayPal/Models/PhoneNumber/DisplayPhone.swift
@@ -1,9 +1,19 @@
 import Vapor
 
+/// A country specific phone number.
 public struct DisplayPhone: Content, Equatable {
+    
+    /// The [two-character IS0-3166-1 country code](https://developer.paypal.com/docs/integration/direct/rest/country-codes/) of the payee's country.
     public var country: String?
+    
+    /// The in-country phone number, in [E.164 numbering plan format](https://www.itu.int/rec/T-REC-E.164-201011-I).
     public var number: String?
     
+    /// Creates a new `DisplayPhone` instance.
+    ///
+    /// - Parameters:
+    ///   - country: The country code of the payee's country.
+    ///   - number: The in-country phone number.
     public init(country: String?, number: String?) {
         self.country = country
         self.number = number
