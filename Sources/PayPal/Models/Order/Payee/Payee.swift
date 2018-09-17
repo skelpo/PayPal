1b82debe7062601827d6a96d6008499bb0409560
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-17 12:48:54 -0500

    Created Payee struct

--- Sources/PayPal/Models/Order/Payee/Payee.swift
+++ Sources/PayPal/Models/Order/Payee/Payee.swift
@@ -1,1 +1,13 @@
-public struct Payee {}
+import Vapor
+
+public struct Payee: Content, Equatable {
+    public var email: String?
+    public var merchant: String?
+    public var metadata: Metadata?
+    
+    public init(email: String?, merchant: String?, metadata: Metadata?) {
+        self.email = email
+        self.merchant = merchant
+        self.metadata = metadata
+    }
+}
