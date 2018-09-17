32b15a3e20dbf294e003e169ef05a4e1696ecea9
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-17 12:12:12 -0500

    Added property validation to Payee.Metadata initializers

--- Sources/PayPal/Models/Order/Payee/PayeeMetadata.swift
+++ Sources/PayPal/Models/Order/Payee/PayeeMetadata.swift
@@ -23,10 +23,22 @@ extension Payee {
         ///   - email: The email address for the payer.
         ///   - phone: The payee's phone number.
         ///   - brand: The payer's business name.
-        public init(email: String?, phone: DisplayPhone?, brand: String?) {
+        public init(email: String?, phone: DisplayPhone?, brand: String?)throws {
             self.email = email
             self.phone = phone
             self.brand = brand
+            
+            try self.set(\.email <~ email)
+        }
+        
+        /// See [`Decodable.init(from:)`](https://developer.apple.com/documentation/swift/decodable/2894081-init).
+        public init(from decoder: Decoder)throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            try self.init(
+                email: container.decodeIfPresent(String.self, forKey: .email),
+                phone: container.decodeIfPresent(DisplayPhone.self, forKey: .phone),
+                brand: container.decodeIfPresent(String.self, forKey: .brand)
+            )
         }
         
         /// See `ValidationSetable.setterValidations()`.
