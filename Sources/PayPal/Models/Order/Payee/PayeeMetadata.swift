d8f093608ee4f62c6cc1746b091813a131301e6c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-17 11:56:00 -0500

    Documented Payee.Metadata struct

--- Sources/PayPal/Models/Order/Payee/PayeeMetadata.swift
+++ Sources/PayPal/Models/Order/Payee/PayeeMetadata.swift
@@ -1,11 +1,25 @@
 import Vapor
 
 extension Payee {
+    
+    /// The display-only metadata for a payee.
     public struct Metadata: Content, Equatable {
+        
+        /// The email address for the payer. Maximum length is 127 characters.
         public var email: String?
+        
+        /// The payee's phone number.
         public var phone: DisplayPhone?
+        
+        /// The payer's business name.
         public var brand: String?
         
+        /// Creates a new `Payee.Metadata` instance.
+        ///
+        /// - Parameters:
+        ///   - email: The email address for the payer.
+        ///   - phone: The payee's phone number.
+        ///   - brand: The payer's business name.
         public init(email: String?, phone: DisplayPhone?, brand: String?) {
             self.email = email
             self.phone = phone
