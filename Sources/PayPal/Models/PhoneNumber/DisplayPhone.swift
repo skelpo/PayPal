65d87feb49fd41f6e61d62ea6b7af4f9afc8675b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-17 11:09:38 -0500

    Conformed DisplayPhone struct to ValidationSetable protocol

--- Sources/PayPal/Models/PhoneNumber/DisplayPhone.swift
+++ Sources/PayPal/Models/PhoneNumber/DisplayPhone.swift
@@ -1,10 +1,13 @@
 import Vapor
 
 /// A country specific phone number.
-public struct DisplayPhone: Content, Equatable {
+public struct DisplayPhone: Content, ValidationSetable, Equatable {
     
     /// The [two-character IS0-3166-1 country code](https://developer.paypal.com/docs/integration/direct/rest/country-codes/) of the payee's country.
-    public var country: String?
+    ///
+    /// This property can be set using the `DisplayPhone.set(_:)` method.
+    /// This method validates the new value before assigning it to the property.
+    public private(set) var country: String?
     
     /// The in-country phone number, in [E.164 numbering plan format](https://www.itu.int/rec/T-REC-E.164-201011-I).
     public var number: String?
@@ -17,6 +20,20 @@ public struct DisplayPhone: Content, Equatable {
     public init(country: String?, number: String?) {
         self.country = country
         self.number = number
+    }
+    
+    /// See `ValidationSetable.setterValidations()`.
+    public func setterValidations() -> SetterValidations<DisplayPhone> {
+        var validations = SetterValidations(DisplayPhone.self)
+        
+        validations.set(\.country) { country in
+            guard let country = country else { return }
+            guard country.range(of: "^([A-Z]{2}|C2)$", options: .regularExpression) != nil else {
+                throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`country` value must match RegEx pattern '^([A-Z]{2}|C2)$'")
+            }
+        }
+        
+        return validations
     }
     
     enum CodingKeys: String, CodingKey {
