3b5f16735df1ec57c7170b489ea5ea0ced178f1c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-17 12:08:16 -0500

    Conformed Payee.Metadata struct to ValidationSetable protocol

--- Sources/PayPal/Models/Order/Payee/PayeeMetadata.swift
+++ Sources/PayPal/Models/Order/Payee/PayeeMetadata.swift
@@ -3,10 +3,13 @@ import Vapor
 extension Payee {
     
     /// The display-only metadata for a payee.
-    public struct Metadata: Content, Equatable {
+    public struct Metadata: Content, ValidationSetable, Equatable {
         
         /// The email address for the payer. Maximum length is 127 characters.
-        public var email: String?
+        ///
+        /// This property can be set using the `Metadata.set(_:)`. This method
+        /// validates the new value before assigning it to the property.
+        public private(set) var email: String?
         
         /// The payee's phone number.
         public var phone: DisplayPhone?
@@ -24,6 +27,20 @@ extension Payee {
             self.email = email
             self.phone = phone
             self.brand = brand
+        }
+        
+        /// See `ValidationSetable.setterValidations()`.
+        public func setterValidations() -> SetterValidations<Payee.Metadata> {
+            var validations = SetterValidations(Payee.Metadata.self)
+            
+            validations.set(\.email) { email in
+                guard let email = email else { return }
+                guard email.count <= 127 else {
+                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`email` value length must be 127 or less")
+                }
+            }
+            
+            return validations
         }
         
         enum CodingKeys: String, CodingKey {
