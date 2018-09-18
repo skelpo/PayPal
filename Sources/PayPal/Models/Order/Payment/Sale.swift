30ee6aa3f40a1c61f72d01f227f85f84184abac0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-18 16:04:39 -0500

    Documented Sale struct

--- Sources/PayPal/Models/Order/Payment/Sale.swift
+++ Sources/PayPal/Models/Order/Payment/Sale.swift
@@ -1,15 +1,42 @@
 import Vapor
 
+/// A sale or authorization for a purchase unit in an order.
 public struct Sale {
+    
+    /// The ID of the sale transaction.
     public let id: String?
+
+    /// The status of the sale transaction. 
     public let status: Status?
+
+    /// The date and time when the resource was created, in [Internet date and time format](https://tools.ietf.org/html/rfc3339#section-5.6).
     public let created: String?
+    
+    /// The date and time when the resource was last updated, in [Internet date and time format](https://tools.ietf.org/html/rfc3339#section-5.6).
     public let updated: String?
+    
+    /// An array of request-related [HATEOAS links](https://developer.paypal.com/docs/api/overview/#hateoas-links).
     public let links: [LinkDescription]?
     
+    
+    /// The amount to collect. Maximum length is 10 characters, which includes:
+    /// - Seven digits before the decimal point.
+    /// - The decimal point.
+    /// - Two digits after the decimal point.
     public var amount: DetailedAmount?
+    
+    /// The currency and amount of the transaction fee. Maximum length is 10 characters, which includes:
+    /// - Seven digits before the decimal point.
+    /// - The decimal point.
+    /// - Two digits after the decimal point.
     public var transaction: Amount?
     
+    
+    /// Creates a new `Sale` instance.
+    ///
+    /// - Parameters:
+    ///   - amount: The amount to collect.
+    ///   - transaction: The currency and amount of the transaction fee.
     public init(amount: DetailedAmount?, transaction: Amount?) {
         self.id = nil
         self.status = nil
