6eb08ed7da79c510a05ab5877e76bd8fecd05b10
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-18 16:40:10 -0500

    Created Order.Payment struct

--- Sources/PayPal/Models/Order/Payment/OrderPayment.swift
+++ Sources/PayPal/Models/Order/Payment/OrderPayment.swift
@@ -1,3 +1,17 @@
+import Vapor
+
 extension Order {
-    public struct Payment {}
+    public struct Payment: Content, Equatable {
+        public var captures: [Capture]?
+        public var refunds: [Refund]?
+        public var sales: [Sale]?
+        public var authorizations: [Sale]?
+        
+        public init(captures: [Capture]?, refunds: [Refund]?, sales: [Sale]?, authorizations: [Sale]?) {
+            self.captures = captures
+            self.refunds = refunds
+            self.sales = sales
+            self.authorizations = authorizations
+        }
+    }
 }
