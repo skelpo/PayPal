91d72f1948809d0f314c8f7646169b7531d04c44
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-18 10:44:18 -0500

    Created Capture.Reason enum

--- Sources/PayPal/Models/Order/Payment/Capture.swift
+++ Sources/PayPal/Models/Order/Payment/Capture.swift
@@ -25,3 +25,49 @@ extension Capture {
         case denied = "DENIED"
     }
 }
+
+extension Capture {
+    
+    /// A reason code that indicates the reason for a transaction state of `PENDING` or `REVERSED`.
+    public enum Reason: String, Hashable, CaseIterable, Content {
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a chargeback.
+        case chargeback = "CHARGEBACK"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a guarantee.
+        case guarantee = "GUARANTEE"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a buyer complaint.
+        case buyerComplaint = "BUYER_COMPLAINT"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a refund.
+        case refund = "REFUND"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to an unconfirmed shipping address.
+        case unconfirmedAddress = "UNCONFIRMED_SHIPPING_ADDRESS"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to an e-check.
+        case echeck = "ECHECK"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to an international withdrawal.
+        case internationalWithdrawal = "INTERNATIONAL_WITHDRAWAL"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a receiving preference that mandates manual action.
+        case manualAction = "RECEIVING_PREFERENCE_MANDATES_MANUAL_ACTION"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a payment review.
+        case review = "PAYMENT_REVIEW"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a regulatory review.
+        case regulatory = "REGULATORY_REVIEW"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a unilateral reason.
+        case unilateral = "UNILATERAL"
+        
+        /// The transaction state is `PENDING` or `REVERSED` because verification is required.
+        case verification = "VERIFICATION_REQUIRED"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a delayed disbursement.
+        case disbursement = "DELAYED_DISBURSEMENT"
+    }
+}
