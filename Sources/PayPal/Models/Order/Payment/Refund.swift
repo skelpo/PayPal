4a862a9832bdebd6b31d9bdeb4f2a0572646741b
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-18 12:49:07 -0500

    Documented Refund struct

--- Sources/PayPal/Models/Order/Payment/Refund.swift
+++ Sources/PayPal/Models/Order/Payment/Refund.swift
@@ -1,14 +1,36 @@
 import Vapor
 
+/// A refund for a purchased unit.
 public struct Refund {
+    
+    /// The ID of the refund transaction. Maximum length is 17 characters.
     public let id: String?
+    
+    /// The ID of the sale transaction to refund.
     public let capture: String?
+    
+    /// The ID of the sale transaction to refund.
     public let sale: String?
+    
+    /// The status of the refund.
     public let status: Status?
+    
+    /// An array of request-related [HATEOAS links](https://developer.paypal.com/docs/api/overview/#hateoas-links).
     public let links: [LinkDescription]?
     
+    
+    /// The amount that is refunded to the payer and the amount that is refunded to the payee.
+    ///
+    /// Maximum length is 10 characters, which includes:
+    /// - Seven digits before the decimal point.
+    /// - The decimal point.
+    /// - Two digits after the decimal point./// -
     public let amount: DetailedAmount?
     
+    
+    /// Creates a new `Refund` instance.
+    ///
+    /// - Parameter amount: The amount that is refunded to the payer and the amount that is refunded to the payee.
     public init(amount: DetailedAmount?) {
         self.id = nil
         self.capture = nil
