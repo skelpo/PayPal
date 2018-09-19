b84abc1faafa3d420ebd28e16136bf79711a73d1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-19 07:22:58 -0500

    Created Order.Unit.Reason enum

--- Sources/PayPal/Models/Order/OrderUnit.swift
+++ Sources/PayPal/Models/Order/OrderUnit.swift
@@ -25,3 +25,34 @@ extension Order.Unit {
         case captured = "CAPTURED"
     }
 }
+
+extension Order.Unit {
+    
+    /// The reason code for a transaction status of `PENDING` or `REVERSED`. 
+    public enum Reason: String, Hashable, CaseIterable, Content {
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to an unconfirmed payer shipping address.
+        case unconfirmedAddress = "PAYER_SHIPPING_UNCONFIRMED"
+        
+        /// The transaction state is `PENDING` or `REVERSED` because it is a multi-currency transaction.
+        case multiCurrency = "MULTI_CURRENCY"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a risk review.
+        case risk = "RISK_REVIEW"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to a regulatory review.
+        case regulatory = "REGULATORY_REVIEW"
+        
+        /// The transaction state is `PENDING` or `REVERSED` because verification is required.
+        case verification = "VERIFICATION_REQUIRED"
+        
+        /// The transaction state is `PENDING` or `REVERSED` because the transaction is an order.
+        case order = "ORDER"
+        
+        /// The transaction state is `PENDING` or `REVERSED` due to another reason.
+        case other = "OTHER"
+        
+        /// The transaction state is `PENDING` or `REVERSED` because it was declined by a policy.
+        case declinedByPolicy = "DECLINED_BY_POLICY"
+    }
+}
