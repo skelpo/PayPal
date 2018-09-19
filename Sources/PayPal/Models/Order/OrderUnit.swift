8b5a4bd52b8514ab0c7f14f6d1a8c58e3c45368f
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-19 08:48:35 -0500

    Created Order.Unit struct

--- Sources/PayPal/Models/Order/OrderUnit.swift
+++ Sources/PayPal/Models/Order/OrderUnit.swift
@@ -1,7 +1,59 @@
 import Vapor
 
 extension Order {
-    public struct Unit {}
+    public struct Unit: Content, Equatable {
+        public let status: Status?
+        public let reason: Reason?
+        
+        public var reference: String
+        public var amount: DetailedAmount
+        public var payee: Payee?
+        public var description: String?
+        public var invoice: String?
+        public var paymentDescriptor: String?
+        public var items: [Item]?
+        public var notify: String?
+        public var shippingAddress: Address?
+        public var shippingMethod: String?
+        public var partnerFee: PartnerFee?
+        public var paymentGroup: Int?
+        public var metadata: Metadata?
+        public var payment: Payment?
+        
+        public init(
+            reference: String,
+            amount: DetailedAmount,
+            payee: Payee?,
+            description: String?,
+            invoice: String?,
+            paymentDescriptor: String?,
+            items: [Item]?,
+            notify: String?,
+            shippingAddress: Address?,
+            shippingMethod: String?,
+            partnerFee: PartnerFee?,
+            paymentGroup: Int?,
+            metadata: Metadata?,
+            payment: Payment?
+        ) {
+            self.status = nil
+            self.reason = nil
+            self.reference = reference
+            self.amount = amount
+            self.payee = payee
+            self.description = description
+            self.invoice = invoice
+            self.paymentDescriptor = paymentDescriptor
+            self.items = items
+            self.notify = notify
+            self.shippingAddress = shippingAddress
+            self.shippingMethod = shippingMethod
+            self.partnerFee = partnerFee
+            self.paymentGroup = paymentGroup
+            self.metadata = metadata
+            self.payment = payment
+        }
+    }
 }
 
 extension Order.Unit {
