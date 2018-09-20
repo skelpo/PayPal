746d31d699a36bdb594dc52aea04b97546581266
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-20 15:40:12 -0500

    Documented Order.Payer struct

--- Sources/PayPal/Models/Order/Payer/OrderPayer.swift
+++ Sources/PayPal/Models/Order/Payer/OrderPayer.swift
@@ -1,13 +1,37 @@
 import Vapor
 
 extension Order {
+    
+    /// The source of the funds for a payment.
     public struct Payer: Content, Equatable {
+        
+        /// The status of payer's PayPal account.
         public let status: Status?
         
+        
+        /// The payment method.
+        ///
+        /// - Warning: The use of the PayPal REST `/payments` APIs to accept credit card payments is restricted.
+        ///   Instead, you can accept credit card payments with:
+        ///   - [Guest payments](https://developer.paypal.com/docs/integration/direct/payments/guest-payments/) with a
+        ///     [credit card that is stored in the PayPal vault](https://developer.paypal.com/docs/api/vault/#credit-cards_create).
+        ///   - [Braintree Direct](https://www.braintreepayments.com/products/braintree-direct)
+        ///   - [PayPal business products](https://www.paypal.com/us/webapps/mpp/merchant)
         public var method: Method?
+        
+        /// An array of a single funding instrument for the current payment. Valid only and required for the credit card payment method.
+        /// The array must include either a `credit_card` or `credit_card_token` object. If the array contains more than one instrument, the payment is declined.
         public var funding: [FundingInstrument]?
+        
+        /// The payer information.
         public var info: Info?
         
+        
+        /// Creates a new `Order.Payer` instance.
+        ///
+        /// - Parameters:
+        ///   - method: The payment method.
+        ///   - funding: An array of a single funding instrument for the current payment.
         public init(method: Method?, funding: [FundingInstrument]?, info: Info?) {
             self.status = nil
             self.method = method
