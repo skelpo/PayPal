e29415e5d626b8d7deb19614ba90e39c1b05ff1c
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-20 09:36:00 -0500

    Created Orders.create(order:partnerID:) method

--- Sources/PayPal/API/Controllers/Orders.swift
+++ Sources/PayPal/API/Controllers/Orders.swift
@@ -20,5 +20,33 @@ public final class Orders: PayPalController {
         self.container = container
         self.resource = "checkout/orders"
     }
+    
+    /// Creates an order.
+    ///
+    /// A successful request returns the HTTP `200 OK` status code and a JSON response body that includes the PayPal-generated order ID,
+    /// an array of purchase unit objects, payment details, customer information, metadata, and order status.
+    ///
+    /// - Parameters:
+    ///   - order: The data for the new order to create. `units` and `redirects` properties are required.
+    ///   - id: For tracking transactions which are associtaed with the partner who the ID belongs to.
+    ///
+    /// - Returns: The new order created by PayPal, wrapped in a future. If an error response was sent back instead,
+    ///   it gets converted to a Swift error and the future wraps that instead.
+    public func create(order: Order, partnerID id: String?) -> Future<Order> {
+        return Future.flatMap(on: self.container) { () -> Future<Order> in
+            let client = try self.container.make(PayPalClient.self)
+            let headers: HTTPHeaders = id == nil ? [:] : ["PayPal-Partner-Attribution-Id": id!]
+            
+            if order.units == nil || order.redirects == nil {
+                var order = order
+                if order.units == nil { order.units = [] }
+                if order.redirects == nil { order.redirects = Order.Redirects(return: nil, cancel: nil) }
+                
+                return try client.post(self.path(), headers: headers, body: order, as: Order.self)
+            } else {
+                return try client.post(self.path(), headers: headers, body: order, as: Order.self)
+            }
+        }
+    }
 }
 
