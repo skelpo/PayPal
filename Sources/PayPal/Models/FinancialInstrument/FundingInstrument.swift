8796b028e2201fa288bfa652bbf4a528a56160c7
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-20 14:39:45 -0500

    Documented FundingInstrument struct

--- Sources/PayPal/Models/FinancialInstrument/FundingInstrument.swift
+++ Sources/PayPal/Models/FinancialInstrument/FundingInstrument.swift
@@ -1,8 +1,14 @@
 import Vapor
 
+/// A funding instrument for a payment.
 public struct FundingInstrument: Content, Equatable {
+    
+    /// The tokenized credit card details. You can use this instrument to fund a payment.
     public var token: CreditCard.Token?
     
+    /// Creates a new `FundingInstrument` instance.
+    ///
+    /// - Parameter token: The tokenized credit card details.
     public init(token: CreditCard.Token?) {
         self.token = token
     }
