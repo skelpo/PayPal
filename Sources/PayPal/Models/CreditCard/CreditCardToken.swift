55e190c51f4609380a21526251d5261f659dd8c0
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-20 14:32:55 -0500

    Documented CreditCard.Token struct

--- Sources/PayPal/Models/CreditCard/CreditCardToken.swift
+++ Sources/PayPal/Models/CreditCard/CreditCardToken.swift
@@ -1,15 +1,40 @@
 import Vapor
 
 extension CreditCard {
+    
+    /// Tokenized credit card details.
     public struct Token: Content, Equatable {
-        public let suffix: String?
-        public let type: String?
-        public var expireMonth: Int?
-        public var expiredYear: Int?
         
+        /// The last four digits of the stored credit card number.
+        public let suffix: String?
+        
+        /// The credit card type. Value is `visa`, `mastercard`, `discover`, or `amex`. Do not use these lowercase values for display.
+        public let type: String?
+        
+        /// The expiration month with no leading zero. Value is from `1` to `12`.
+        public let expireMonth: Int?
+        
+        /// The four-digit expiration year.
+        public let expiredYear: Int?
+        
+        
+        /// The ID of credit card that is stored in the PayPal vault.
+        ///
+        /// - Warning: The use of the PayPal REST `/payments` APIs to accept credit card payments is restricted.
+        ///   Instead, you can accept credit card payments with [Braintree Direct](https://www.braintreepayments.com/products/braintree-direct).
         public var creditCard: String
+        
+        /// A unique ID that you can assign and track when you store a credit card in the vault or use a vaulted credit card.
+        /// This ID can help to avoid unintentional use or misuse of credit cards and can be any value, such as a UUID, user name, or email address.
+        /// **Required** when you use a vaulted credit card and if a `payer_id` was originally provided when you vaulted the credit card.
         public var payer: String?
         
+        
+        /// Creates a new `CreditCard.Token` instance.
+        ///
+        /// - Parameters:
+        ///   - creditCard: The ID of credit card that is stored in the PayPal vault.
+        ///   - payer: A unique ID that you can assign and track when you store a credit card in the vault or use a vaulted credit card.
         public init(creditCard: String, payer: String?) {
             self.suffix = nil
             self.type = nil
