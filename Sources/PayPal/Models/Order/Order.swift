60ce97075a8bf699ca9c21b0a17cd6a415941bcd
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-20 07:19:52 -0500

    Created Order struct

--- Sources/PayPal/Models/Order/Order.swift
+++ Sources/PayPal/Models/Order/Order.swift
@@ -1,1 +1,41 @@
-public struct Order {}
+import Vapor
+
+public struct Order: Content, Equatable {
+    public let id: String?
+    public let status: Status?
+    public let created: String?
+    public let updated: String?
+    public let links: [LinkDescription]?
+    
+    public var intent: Intent?
+    public var units: [Unit]?
+    public var payment: Payment?
+    public var total: Amount?
+    public var context: AppContext?
+    public var metadata: Metadata?
+    public var redirects: Redirects?
+    
+    public init(
+        intent: Intent?,
+        units: [Unit]?,
+        payment: Payment?,
+        total: Amount?,
+        context: AppContext?,
+        metadata: Metadata?,
+        redirects: Redirects?
+    ) {
+        self.id = nil
+        self.status = nil
+        self.created = nil
+        self.updated = nil
+        self.links = nil
+        
+        self.intent = intent
+        self.units = units
+        self.payment = payment
+        self.total = total
+        self.context = context
+        self.metadata = metadata
+        self.redirects = redirects
+    }
+}
