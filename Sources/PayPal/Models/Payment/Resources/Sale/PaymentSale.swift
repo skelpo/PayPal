e8960935914e30a14ff9eb590b1c55392b30221e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-24 11:52:17 -0500

    Created RelatedResource.Sale.Reason enum

--- Sources/PayPal/Models/Payment/Resources/Sale/PaymentSale.swift
+++ Sources/PayPal/Models/Payment/Resources/Sale/PaymentSale.swift
@@ -44,3 +44,49 @@ extension RelatedResource.Sale {
         case denied
     }
 }
+
+extension RelatedResource.Sale {
+    
+    /// A reason code that describes why the transaction state is pending or reversed.
+    public enum Reason: String, Hashable, CaseIterable, Content {
+        
+        /// `CHARGEBACK`.
+        case chargeback = "CHARGEBACK"
+        
+        /// `GUARANTEE`.
+        case guarantee = "GUARANTEE"
+        
+        /// `BUYER_COMPLAINT`.
+        case complaint = "BUYER_COMPLAINT"
+        
+        /// `REFUND`.
+        case refund = "REFUND"
+        
+        /// `UNCONFIRMED_SHIPPING_ADDRESS`.
+        case unconfirmedAddress = "UNCONFIRMED_SHIPPING_ADDRESS"
+        
+        /// `ECHECK`.
+        case echeck = "ECHECK"
+        
+        /// `INTERNATIONAL_WITHDRAWAL`.
+        case withdrawl = "INTERNATIONAL_WITHDRAWAL"
+        
+        /// `RECEIVING_PREFERENCE_MANDATES_MANUAL_ACTION`.
+        case manualAction = "RECEIVING_PREFERENCE_MANDATES_MANUAL_ACTION"
+        
+        /// `PAYMENT_REVIEW`.
+        case payment = "PAYMENT_REVIEW"
+        
+        /// `REGULATORY_REVIEW`.
+        case regulatory = "REGULATORY_REVIEW"
+        
+        /// `UNILATERAL`.
+        case unilateral = "UNILATERAL"
+        
+        /// `VERIFICATION_REQUIRED`.
+        case verification = "VERIFICATION_REQUIRED"
+        
+        /// `TRANSACTION_APPROVED_AWAITING_FUNDING`.
+        case awaitingFunding = "TRANSACTION_APPROVED_AWAITING_FUNDING"
+    }
+}
