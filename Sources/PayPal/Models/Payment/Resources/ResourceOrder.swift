bbd42fea9091fb87f6ef523e44bb33b5a1fd46cc
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-25 12:31:12 -0500

    Created RelatedResource.Order.State enum

--- Sources/PayPal/Models/Payment/Resources/ResourceOrder.swift
+++ Sources/PayPal/Models/Payment/Resources/ResourceOrder.swift
@@ -22,3 +22,26 @@ extension RelatedResource.Order {
         case echeck = "ECHECK"
     }
 }
+
+extension RelatedResource.Order {
+    
+    /// The state of an order transaction
+    public enum State: String, Hashable, CaseIterable, Content {
+        
+        ///  The order was created but no authorizations or captures were made against the order.
+        case pending
+        
+        ///  The order has only been authorized. No capture was made against the order.
+        case authorized
+        
+        ///  The order has at least one capture initiated.
+        case captured
+        
+        ///  The order is complete. A capture was made against the order with `is_final_capture` set to `TRUE`.
+        /// No more authorizations or captures can be made against this order.
+        case completed
+        
+        ///  The order was voided. No more authorizations or captures can be made against this order. captures case can be made against this order.
+        case voided
+    }
+}
