3248381891b3b632f941e1d575a200b1b383ade8
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-25 10:56:40 -0500

    Created RelatedResource.Authorization.State enum

--- Sources/PayPal/Models/Payment/Resources/RelatedAuthorization.swift
+++ Sources/PayPal/Models/Payment/Resources/RelatedAuthorization.swift
@@ -15,3 +15,28 @@ extension RelatedResource.Authorization {
         case instant = "INSTANT_TRANSFER"
     }
 }
+
+extension RelatedResource.Authorization {
+    
+    /// The authorized payment state.
+    public enum State: String, Hashable, CaseIterable, Content {
+        
+        /// The authorized payment is pending.
+        case pending
+        
+        /// The authorized payment is authorized.
+        case authorized
+        
+        /// The authorized payment is partially captured.
+        case partiallyCaptured = "partially_captured"
+        
+        /// The authorized payment is captured.
+        case captured
+        
+        /// The authorized payment is expired.
+        case expired
+        
+        /// The authorized payment is voided.
+        case voided
+    }
+}
