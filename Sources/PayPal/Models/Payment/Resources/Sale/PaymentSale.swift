d02bf4b229a95b032d24dc4ed86c4b453814c794
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-25 10:00:41 -0500

    Added .all enum case to RelatedResource.Sale.ProtectionType enum

--- Sources/PayPal/Models/Payment/Resources/Sale/PaymentSale.swift
+++ Sources/PayPal/Models/Payment/Resources/Sale/PaymentSale.swift
@@ -262,10 +262,19 @@ extension RelatedResource.Sale {
     /// The merchant protection type in effect for a transaction.
     public enum ProtectionType: String, Hashable, CaseIterable, Content {
         
+        /// Sellers are protected against claims for items not received.
+        ///
         /// `ITEM_NOT_RECEIVED_ELIGIBLE`.
         case itemNotReceived = "ITEM_NOT_RECEIVED_ELIGIBLE"
         
+        /// Sellers are protected against claims for unauthorized payments.
+        ///
         /// `UNAUTHORIZED_PAYMENT_ELIGIBLE`.
         case unauthorizedPayment = "UNAUTHORIZED_PAYMENT_ELIGIBLE"
+        
+        /// Sellers are protected against claims for items not received and sellers are protected against claims for unauthorized payments.
+        ///
+        /// `ITEM_NOT_RECEIVED_ELIGIBLE,UNAUTHORIZED_PAYMENT_ELIGIBLE`.
+        case all = "ITEM_NOT_RECEIVED_ELIGIBLE,UNAUTHORIZED_PAYMENT_ELIGIBLE"
     }
 }
