5a529b9a4aa0da76058b1a15a6bfb1eefe47cbad
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-25 11:29:21 -0500

    Documented RelatedResource.Authorization struct

--- Sources/PayPal/Models/Payment/Resources/RelatedAuthorization.swift
+++ Sources/PayPal/Models/Payment/Resources/RelatedAuthorization.swift
@@ -1,24 +1,70 @@
 import Vapor
 
 extension RelatedResource {
+    
+    /// An authorization that is related to a payment's transaction.
     public struct Authorization {
+        
+        /// The ID of the authorization.
         public let id: String?
+        
+        /// The payment mode of the authorization.
         public let mode: PaymentMode?
+        
+        /// The authorized payment state.
         public let state: State?
+        
+        /// The reason code for the pending transaction state.
         public let reason: Reason?
+        
+        /// The level of seller protection present for the transaction. Supported for the PayPal payment method only.
         public let protection: Protection?
+        
+        /// The type of seller protection for the transaction. Returned only when the `protection_eligibility`
+        /// property is `ELIGIBLE` or `PARTIALLY_ELIGIBLE`. Supported for the PayPal payment method only
         public let protectionType: ProtectionType?
+        
+        /// The ID of the payment on which this transaction is based.
         public let payment: String?
+        
+        /// The date and time when the authorization expires, in [Internet date and time format](https://tools.ietf.org/html/rfc3339#section-5.6).
         public let expiration: String?
+        
+        /// The date and time when the authorization was created, in [Internet date and time format](https://tools.ietf.org/html/rfc3339#section-5.6).
         public let created: String?
+        
+        /// The date and time when the authorization was last updated,
+        /// in [Internet date and time format](https://tools.ietf.org/html/rfc3339#section-5.6).
         public let updated: String?
+        
+        /// The receipt ID, which identifies the payment. Value is 16-digit numeric payment ID number that is returned for guest users.
+        ///
+        /// Pattern: `^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$`.
         public let receipt: String?
+        
+        /// An array of request-related [HATEOAS links](https://developer.paypal.com/docs/api/overview/#hateoas-links).
         public let links: [LinkDescription]?
         
+        
+        /// The amount being authorized.
         public var amount: DetailedAmount
+        
+        /// The Fraud Management Filter (FMF) details that are applied to the payment that result in an accept, deny, or pending action.
+        /// Returned in a payment response only if the merchant has enabled FMF in the profile settings and one of the fraud filters was
+        /// triggered based on those settings. For more information, see
+        /// [Fraud Management Filters Summary](https://developer.paypal.com/docs/classic/fmf/integration-guide/FMFSummary/).
         public var fmf: FraudManagementFilter?
+        
+        /// The processor-provided response codes that describe the submitted payment. Supported only when the `payment_method` is `credit_card`.
         public var processor: ProcessorResponse?
         
+        
+        /// Creates a new `RelatedResource.Authorization` instance.
+        ///
+        /// - Parameters:
+        ///   - amount: The amount being authorized.
+        ///   - fmf: The Fraud Management Filter (FMF) details that are applied to the payment that result in an accept, deny, or pending action.
+        ///   - processor: The processor-provided response codes that describe the submitted payment.
         public init(amount: DetailedAmount, fmf: FraudManagementFilter?, processor: ProcessorResponse?) {
             self.id = nil
             self.mode = nil
