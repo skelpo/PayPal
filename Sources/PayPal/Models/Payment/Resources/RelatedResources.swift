fdb16b4fbf30e3156a2ee4ed5ad6a70e59dca25d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-26 12:17:56 -0500

    Created RelatedResource struct

--- Sources/PayPal/Models/Payment/Resources/RelatedResources.swift
+++ Sources/PayPal/Models/Payment/Resources/RelatedResources.swift
@@ -1,1 +1,17 @@
-public struct RelatedResource {}
+import Vapor
+
+public struct RelatedResource: Content, Equatable {
+    public var sale: Sale?
+    public var authorization: Authorization?
+    public var order: Order?
+    public var capture: Capture?
+    public var refund: Refund?
+    
+    public init(sale: Sale?, authorization: Authorization?, order: Order?, capture: Capture?, refund: Refund?) {
+        self.sale = sale
+        self.authorization = authorization
+        self.order = order
+        self.capture = capture
+        self.refund = refund
+    }
+}
