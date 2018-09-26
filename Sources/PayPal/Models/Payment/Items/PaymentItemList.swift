d7f81d354bc8625ebaf6d3b36eaaa22153768543
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-26 16:42:22 -0500

    Documented Payment.ItemList struct

--- Sources/PayPal/Models/Payment/Items/PaymentItemList.swift
+++ Sources/PayPal/Models/Payment/Items/PaymentItemList.swift
@@ -1,11 +1,32 @@
 import Vapor
 
 extension Payment {
+    
+    /// An array of items that are being purchased in a PayPal payment.
     public struct ItemList: Content, Equatable {
+        
+        /// An array of items that are being purchased.
         public var items: [Item]?
+        
+        /// The shipping address details.
         public var address: Address?
+        
+        /// The shipping phone number, in its canonical international format as defined by the
+        /// [E.164](https://en.wikipedia.org/wiki/E.164) numbering plan.
+        ///
+        /// Enables merchants to share payer’s contact number with PayPal for the current payment.
+        /// The final contact number for the payer who is associated with the transaction might be
+        /// the same as or different from the shipping_phone_number based on the payer’s action on PayPal.
+        ///
+        /// Minimum length: 1. Maximum length: 50.
         public var phoneNumber: String?
         
+        /// Creates a new `Payment.ItemList` instance.
+        ///
+        /// - Parameters:
+        ///   - items: An array of items that are being purchased.
+        ///   - address: The shipping address details.
+        ///   - phoneNumber: The shipping phone number, in its canonical international format as defined by the E.164 numbering plan.
         public init(items: [Item]?, address: Address?, phoneNumber: String?) {
             self.items = items
             self.address = address
