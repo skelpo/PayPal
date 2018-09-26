ddaa733831d13265e7a0b5fb45d7dbdcfa31f901
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-26 07:21:20 -0500

    Created RelatedResource.Capture.State enum

--- Sources/PayPal/Models/Payment/Resources/ResourceCapture.swift
+++ Sources/PayPal/Models/Payment/Resources/ResourceCapture.swift
@@ -4,8 +4,28 @@ extension RelatedResource {
     public struct Capture {}
 }
 
-extension RelatedResource.Authorization {}
+extension RelatedResource.Capture {
+    
+    /// The state of a capture
+    public enum State: String, Hashable, CaseIterable, Content {
+        
+        /// The capture is pending.
+        case pending
+        
+        /// The capture has successfully completed.
+        case completed
+        
+        /// The capture was fully refunded.
+        case refunded
+        
+        /// The capture was partially refunded.
+        case partiallyRefunded = "partially_refunded"
+        
+        /// PayPal has declined to process this transaction.
+        case denied
+    }
+}
 
-extension RelatedResource.Authorization {}
+extension RelatedResource.Capture {}
 
-extension RelatedResource.Authorization {}
+extension RelatedResource.Capture {}
