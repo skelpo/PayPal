d3e57985b0e5775415abf87d676f18defc6f4661
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-26 10:34:57 -0500

    Created RelatedResource.Refund struct

--- Sources/PayPal/Models/Payment/Resources/ResourceRefund.swift
+++ Sources/PayPal/Models/Payment/Resources/ResourceRefund.swift
@@ -1,7 +1,37 @@
 import Vapor
 
 extension RelatedResource {
-    public struct Refund {}
+    public struct Refund: Content, Equatable {
+        public let id: String?
+        public let state: State?
+        public let sale: String?
+        public let capture: String?
+        public let payment: String?
+        public let created: String?
+        public let updated: String?
+        public let links: [LinkDescription]?
+        
+        public var amount: DetailedAmount?
+        public var reason: String?
+        public var invoice: String?
+        public var description: String?
+        
+        public init(amount: DetailedAmount?, reason: String?, invoice: String?, description: String?) {
+            self.id = nil
+            self.state = nil
+            self.sale = nil
+            self.capture = nil
+            self.payment = nil
+            self.created = nil
+            self.updated = nil
+            self.links = nil
+            
+            self.amount = amount
+            self.reason = reason
+            self.invoice = invoice
+            self.description = description
+        }
+    }
 }
 
 extension RelatedResource.Refund {
