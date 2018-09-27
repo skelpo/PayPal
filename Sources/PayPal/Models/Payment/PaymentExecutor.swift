57a8f5e7401848df1e554d82923f6d71ef6a52f6
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-27 12:01:09 -0500

    Created custom encode/decoding implementations for Payment.Executor struct. Renamed .transactions proeprty to .amounts

--- Sources/PayPal/Models/Payment/PaymentExecutor.swift
+++ Sources/PayPal/Models/Payment/PaymentExecutor.swift
@@ -12,17 +12,49 @@ extension Payment {
         ///
         /// > An array of transaction details. Includes the amount and item details. For update and execute payment calls,
         /// > the transactions object accepts only the amount object.
-        public var transactions: [DetailedAmount]?
+        public var amounts: [DetailedAmount]?
         
         
         /// Creates a new `Payment.Executor` instance.
         ///
         /// - Parameters:
         ///   - payer: The ID of the payer that PayPal passes in the `return_url`.
-        ///   - transactions: The `amount` objects for the transaction details.
-        public init(payer: String?, transactions: [DetailedAmount]?) {
+        ///   - amounts: The `amount` objects for the transaction details.
+        public init(payer: String?, amounts: [DetailedAmount]?) {
             self.payer = payer
-            self.transactions = transactions
+            self.amounts = amounts
+        }
+        
+        /// See [`Decodable.init(from:)`](https://developer.apple.com/documentation/swift/decodable/2894081-init).
+        public init(from decoder: Decoder)throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            self.payer = try container.decodeIfPresent(String.self, forKey: .payer)
+        
+            var transactions = try container.nestedUnkeyedContainer(forKey: .transactions)
+            var amounts: [DetailedAmount] = []
+            while !transactions.isAtEnd {
+                let transaction = try transactions.nestedContainer(keyedBy: CodingKeys.self)
+                try amounts.append(transaction.decode(DetailedAmount.self, forKey: .amount))
+            }
+            
+            self.amounts = amounts
+        }
+        
+        /// See [`Encodable.encode(to:)`](https://developer.apple.com/documentation/swift/encodable/2893603-encode).
+        public func encode(to encoder: Encoder)throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            try container.encodeIfPresent(self.payer, forKey: .payer)
+            
+            var transactions = container.nestedUnkeyedContainer(forKey: .transactions)
+            try self.amounts?.forEach { amount in
+                var transaction = transactions.nestedContainer(keyedBy: CodingKeys.self)
+                try transaction.encode(amount, forKey: .amount)
+            }
+        }
+        
+        enum CodingKeys: String, CodingKey {
+            case transactions, amount
+            case payer = "payer_id"
         }
     }
 }
