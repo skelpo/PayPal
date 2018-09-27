07b6b0a68718bc4be7e70be5ba03622fa9415e7a
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-27 10:27:34 -0500

    Documented PaymentList struct

--- Sources/PayPal/Models/Payment/Response/PaymentList.swift
+++ Sources/PayPal/Models/Payment/Response/PaymentList.swift
@@ -1,9 +1,19 @@
 import Vapor
 
+/// The response returned from the `GET /v1/payments/payment` API endpoint.
 public struct PaymentList: Content, Equatable {
+    
+    /// The ID of the element to use to get the next range of results.
     public let next: String?
     
+    
+    /// An array of payments.
     public var payments: [Payment]?
+    
+    /// The number of items returned in each range of results. Note that the last
+    /// results range might have fewer items than the requested number of items.
+    ///
+    /// Maximum value: 20.
     public var count: Int?
     
     enum CodingKeys: String, CodingKey {
