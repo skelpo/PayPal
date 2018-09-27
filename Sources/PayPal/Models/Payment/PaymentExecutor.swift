f332febc1c33c545a17947126adfc081c9690e12
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-27 11:46:29 -0500

    Documented Payment.Executor struct

--- Sources/PayPal/Models/Payment/PaymentExecutor.swift
+++ Sources/PayPal/Models/Payment/PaymentExecutor.swift
@@ -1,10 +1,25 @@
 import Vapor
 
 extension Payment {
+    
+    /// The Request body sent to the `POST /v1/payments/payment/{payment_id}/execute` API endpoint.
     public struct Executor: Content, Equatable {
+        
+        /// The ID of the payer that PayPal passes in the `return_url`.
         public var payer: String?
+        
+        /// The `amount` objects for the transaction details.
+        ///
+        /// > An array of transaction details. Includes the amount and item details. For update and execute payment calls,
+        /// > the transactions object accepts only the amount object.
         public var transactions: [DetailedAmount]?
         
+        
+        /// Creates a new `Payment.Executor` instance.
+        ///
+        /// - Parameters:
+        ///   - payer: The ID of the payer that PayPal passes in the `return_url`.
+        ///   - transactions: The `amount` objects for the transaction details.
         public init(payer: String?, transactions: [DetailedAmount]?) {
             self.payer = payer
             self.transactions = transactions
