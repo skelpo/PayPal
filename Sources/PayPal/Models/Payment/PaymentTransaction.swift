c9586e07d356c59221015b851e7ae3b732ff6c17
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-27 06:32:08 -0500

    Created Payment.Transaction struct

--- Sources/PayPal/Models/Payment/PaymentTransaction.swift
+++ Sources/PayPal/Models/Payment/PaymentTransaction.swift
@@ -1,7 +1,45 @@
 import Vapor
 
 extension Payment {
-    public struct Transaction {}
+    public struct Transaction: Content, Equatable {
+        public let resources: [RelatedResource]?
+        
+        public var amount: DetailedAmount?
+        public var payee: Payee?
+        public var description: String?
+        public var payeeNote: String?
+        public var custom: String?
+        public var invoice: String?
+        public var softDescriptor: String?
+        public var payment: PaymentMethod?
+        public var itemList: ItemList?
+        public var notify: String?
+        
+        public init(
+            amount: DetailedAmount?,
+            payee: Payee?,
+            description: String?,
+            payeeNote: String?,
+            custom: String?,
+            invoice: String?,
+            softDescriptor: String?,
+            payment: PaymentMethod?,
+            itemList: ItemList?,
+            notify: String?
+        ) {
+            self.resources = nil
+            self.amount = amount
+            self.payee = payee
+            self.description = description
+            self.payeeNote = payeeNote
+            self.custom = custom
+            self.invoice = invoice
+            self.softDescriptor = softDescriptor
+            self.payment = payment
+            self.itemList = itemList
+            self.notify = notify
+        }
+    }
 }
 
 extension Payment.Transaction {
