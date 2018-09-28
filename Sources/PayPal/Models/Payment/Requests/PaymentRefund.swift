970e28aded7fe097c8854490ab11962bbfbddd7d
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-28 06:23:24 -0500

    Documented Payments.Refund struct

--- Sources/PayPal/Models/Payment/Requests/PaymentRefund.swift
+++ Sources/PayPal/Models/Payment/Requests/PaymentRefund.swift
@@ -1,12 +1,36 @@
 import Vapor
 
 extension Payment {
+    
+    /// The request body used to refund payments, on API endpoints such as `POST /v1/payments/sale/{sale_id}/refund`.
     public struct Refund: Content, Equatable {
+        
+        /// The refund amount. Includes both the amount to refund to the payer and the fee amount to refund to the payee.
         public var amount: DetailedAmount?
+        
+        /// The refund description. Value is a string of single-byte alphanumeric characters.
+        ///
+        /// Maximum length: 255.
         public var description: String?
+        
+        /// The refund reason description.
+        ///
+        /// Maximum length: 30.
         public var reason: String?
+        
+        /// The invoice number that tracks this payment. Value is a string of single-byte alphanumeric characters.
+        ///
+        /// Maximum length: 127.
         public var invoice: String?
         
+        
+        /// Creates a new `Payment.Refund` instance.
+        ///
+        /// - Parameters:
+        ///   - amount: The refund amount.
+        ///   - description: The refund description.
+        ///   - reason: The refund reason description.
+        ///   - invoice: The invoice number that tracks this payment.
         public init(amount: DetailedAmount?, description: String?, reason: String?, invoice: String?) {
             self.amount = amount
             self.description = description
