c53eda72b6aa454643422ccbe2ede4a9a1fe7dee
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-09-28 06:19:00 -0500

    Created Payment.Refund struct

---
+++ Sources/PayPal/Models/Payment/Requests/PaymentRefund.swift
@@ -0,0 +1,17 @@
+import Vapor
+
+extension Payment {
+    public struct Refund: Content, Equatable {
+        public var amount: DetailedAmount?
+        public var description: String?
+        public var reason: String?
+        public var invoice: String?
+        
+        public init(amount: DetailedAmount?, description: String?, reason: String?, invoice: String?) {
+            self.amount = amount
+            self.description = description
+            self.reason = reason
+            self.invoice = invoice
+        }
+    }
+}
