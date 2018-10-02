2e47050a58f5b29fdbf32ceb94f32a5e01d04cde
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-10-02 10:25:53 -0500

    Changed Payment.Transaction.PaymentMethod enum to Payment.Options.Method enum

---
+++ Sources/PayPal/Models/Payment/PaymentOptions.swift
@@ -0,0 +1,23 @@
+import Vapor
+
+extension Payment {
+    public struct Options {}
+}
+
+extension Payment.Options {
+        
+    /// The payment methods for a transaction.
+    public enum Method: String, Hashable, CaseIterable, Content {
+        
+        /// Merchant does not have a preference on how they want the customer to pay.
+        case unrestricted = "UNRESTRICTED"
+        
+        /// Merchant requires that the customer pays with an instant funding source, such as a credit card or PayPal balance.
+        /// All payments are processed instantly. However, payments that require a manual review are marked as pending.
+        /// Merchants must handle the pending state as if the payment is not yet complete.
+        case instantFunding = "INSTANT_FUNDING_SOURCE"
+        
+        /// Processes all payments immediately. Any payment that requires a manual review is marked failed.
+        case immediate = "IMMEDIATE_PAY"
+    }
+}
--- Sources/PayPal/Models/Payment/PaymentTransaction.swift
+++ Sources/PayPal/Models/Payment/PaymentTransaction.swift
@@ -189,21 +189,3 @@ extension Payment {
         }
     }
 }
-
-extension Payment.Transaction {
-    
-    /// The payment methods for a transaction.
-    public enum PaymentMethod: String, Hashable, CaseIterable, Content {
-        
-        /// Merchant does not have a preference on how they want the customer to pay.
-        case unrestricted = "UNRESTRICTED"
-        
-        /// Merchant requires that the customer pays with an instant funding source, such as a credit card or PayPal balance.
-        /// All payments are processed instantly. However, payments that require a manual review are marked as pending.
-        /// Merchants must handle the pending state as if the payment is not yet complete.
-        case instantFunding = "INSTANT_FUNDING_SOURCE"
-        
-        /// Processes all payments immediately. Any payment that requires a manual review is marked failed.
-        case immediate = "IMMEDIATE_PAY"
-    }
-}
