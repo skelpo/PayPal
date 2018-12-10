161a712c4eff2b6df911883b935069c078900109
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-10 15:24:51 -0600

    Removed ValidationSetable conformance from AppContext model

--- Sources/PayPal/Utilities/Validation.swift
+++ Sources/PayPal/Utilities/Validation.swift
@@ -1,8 +1,19 @@
 @_exported import Failable
 
+// MARK: - Collection Length
+
+public typealias Optional127String = Failable<String?, NotNilValidate<Length127<String>>>
+
 public struct Length127<C>: LengthValidation where C: Collection {
     public typealias Supported = C
     
     public static var maxLength: Int { return 127 }
     public static var minLength: Int { return 0 }
+}
+
+
+// MARK: - String Regex
+
+public struct LocaleString: RegexValidation {
+    public static var pattern: String = "^[a-z]{2}(?:-[A-Z][a-z]{3})?(?:-(?:[A-Z]{2}))?$"
 }
