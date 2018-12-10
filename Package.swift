b281a1967280d79378922e4aafd9f6fa8539776a
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-10 14:49:12 -0600

    Installed skelpo/Failable package

--- Package.swift
+++ Package.swift
@@ -8,12 +8,13 @@ let package = Package(
         .library(name: "PayPal", targets: ["PayPal"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.5"),
+        .package(url: "https://github.com/skelpo/Failable.git", from: "0.2.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
         .package(url: "https://github.com/skelpo/JSON.git", from: "0.13.1"),
         .package(url: "https://github.com/twostraws/SwiftGD.git", from: "2.2.0")
     ],
     targets: [
-        .target(name: "PayPal", dependencies: ["Vapor", "JSON"]),
+        .target(name: "PayPal", dependencies: ["Vapor", "JSON", "Validation"]),
         .testTarget(name: "PayPalTests", dependencies: ["PayPal", "Vapor", "JSON", "SwiftGD"]),
     ]
 )
