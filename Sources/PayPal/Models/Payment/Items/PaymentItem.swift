34424da933daf4fe0a6c03d16fb58fdd1e47b649
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-11 13:23:19 -0600

    Removed ValidationSetable conformance from Payment.Item model

--- Sources/PayPal/Models/Payment/Items/PaymentItem.swift
+++ Sources/PayPal/Models/Payment/Items/PaymentItem.swift
@@ -3,23 +3,17 @@ import Vapor
 extension Payment {
     
     /// An item that is being purchased with its parent payment.
-    public struct Item: Content, ValidationSetable, Equatable {
+    public struct Item: Content, Equatable {
         
-        /// The item quantity. Must be a whole number.
-        ///
-        /// This property can be set using the `Item.set(_:)` method.
-        /// This method validates the new value before assigning it to the property.
+        /// The item quantity.
         ///
         /// Maximum length: 10. Pattern: `^[0-9]{0,10}$`.
-        public private(set) var quantity: String
+        public var quantity: Failable<Int, TenDigits<Int>>
         
         /// The item cost. Supports two decimal places.
         ///
-        /// This property can be set using the `Item.set(_:)` method.
-        /// This method validates the new value before assigning it to the property.
-        ///
         /// Maximum length: 10. Pattern: `^[0-9]{0,10}(\.[0-9]{0,2})?$`.
-        public private(set) var price: String
+        public var price: Failable<Decimal, TenDigits<Decimal>>
         
         /// The [three-character ISO-4217 currency code](https://developer.paypal.com/docs/integration/direct/rest/currency-codes/)
         /// that identifies the currency.
@@ -28,27 +22,18 @@ extension Payment {
         
         /// The stock keeping unit (SKU) for the item.
         ///
-        /// This property can be set using the `Item.set(_:)` method.
-        /// This method validates the new value before assigning it to the property.
-        ///
         /// Maximum length: 127.
-        public private(set) var sku: String?
+        public var sku: Optional127String
         
         /// The item name. If this value is greater than the maximum allowed length, the API truncates the string.
         ///
-        /// This property can be set using the `Item.set(_:)` method.
-        /// This method validates the new value before assigning it to the property.
-        ///
         /// Maximum length: 127.
-        public private(set) var name: String?
+        public var name: Optional127String
         
         /// The item description. Supported for only the PayPal payment method.
         ///
-        /// This property can be set using the `Item.set(_:)` method.
-        /// This method validates the new value before assigning it to the property.
-        ///
         /// Maximum length: 127.
-        public private(set) var description: String?
+        public var description: Optional127String
         
         /// The item tax. Supported only for the PayPal payment method.
         public var tax: String?
@@ -65,14 +50,14 @@ extension Payment {
         ///   - description: The item description.
         ///   - tax: The item tax.
         public init(
-            quantity: String,
-            price: String,
+            quantity: Failable<Int, TenDigits<Int>>,
+            price: Failable<Decimal, TenDigits<Decimal>>,
             currency: Currency,
-            sku: String?,
-            name: String?,
-            description: String?,
+            sku: Optional127String,
+            name: Optional127String,
+            description: Optional127String,
             tax: String?
-        )throws {
+        ) {
             self.quantity = quantity
             self.price = price
             self.currency = currency
@@ -80,70 +65,45 @@ extension Payment {
             self.name = name
             self.description = description
             self.tax = tax
-            
-            try self.set(\.quantity <~ quantity)
-            try self.set(\.price <~ price)
-            try self.set(\.sku <~ sku)
-            try self.set(\.name <~ name)
-            try self.set(\.description <~ description)
         }
         
         /// See [`Decodable.init(from:)`](https://developer.apple.com/documentation/swift/decodable/2894081-init).
         public init(from decoder: Decoder)throws {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            let quantity: String
-            do {
-                quantity = try container.decode(String.self, forKey: .quantity)
-            } catch {
-                quantity = try String(describing: container.decode(Int.self, forKey: .quantity))
+            let container = try decoder.container(keyedBy: Payment.Item.CodingKeys.self)
+         
+            let strInt = try container.decode(String.self, forKey: .quantity)
+            let strDec = try container.decode(String.self, forKey: .price)
+            guard let quantity = Int(strInt) else {
+                throw DecodingError.dataCorruptedError(forKey: .quantity, in: container, debugDescription: "String not convertible to int")
+            }
+            guard let price = Decimal(string: strDec) else {
+                throw DecodingError.dataCorruptedError(forKey: .price, in: container, debugDescription: "String not convertible to decimal")
             }
             
-            try self.init(
-                quantity: quantity,
-                price: container.decode(String.self, forKey: .price),
-                currency: container.decode(Currency.self, forKey: .currency),
-                sku: container.decodeIfPresent(String.self, forKey: .sku),
-                name: container.decodeIfPresent(String.self, forKey: .name),
-                description: container.decodeIfPresent(String.self, forKey: .description),
-                tax: container.decodeIfPresent(String.self, forKey: .tax))
+            self.quantity = try quantity.failable()
+            self.price = try price.failable()
+            self.currency = try container.decode(Currency.self, forKey: .currency)
+            self.sku = try container.decode(Optional127String.self, forKey: .sku)
+            self.name = try container.decode(Optional127String.self, forKey: .name)
+            self.description = try container.decode(Optional127String.self, forKey: .description)
+            self.tax = try container.decodeIfPresent(String.self, forKey: .tax)
         }
         
-        /// See `ValidationSetable.setterValidations()`.
-        public func setterValidations() -> SetterValidations<Payment.Item> {
-            var validations = SetterValidations(Payment.Item.self)
-            
-            validations.set(\.quantity) { quantity in
-                let regex = "^[0-9]{0,10}$"
-                guard quantity.range(of: regex, options: .regularExpression) != nil else {
-                    throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`quantity` must match RegEx pattern `\(regex)`")
-                }
-            }
-            validations.set(\.price) { price in
-                let regex = "^[0-9]{0,10}(\\.[0-9]{0,2})?$"
-                guard price.count <= 10 else {
-                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`price` value must have a length of 10 or less")
-                }
-                guard price.range(of: regex, options: .regularExpression) != nil else {
-                    throw PayPalError(status: .badRequest, identifier: "malformedString", reason: "`price` must match RegEx pattern `\(regex)`")
-                }
-            }
-            validations.set(\.sku) { sku in
-                guard sku?.count ?? 0 <= 127 else {
-                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`sku` value must have a length of 127 or less")
-                }
-            }
-            validations.set(\.name) { name in
-                guard name?.count ?? 0 <= 127 else {
-                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`name` value must have a length of 127 or less")
-                }
-            }
-            validations.set(\.description) { description in
-                guard description?.count ?? 0 <= 127 else {
-                    throw PayPalError(status: .badRequest, identifier: "invalidLength", reason: "`description` value length must be 127 or less")
-                }
-            }
-            
-            return validations
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+
+            try container.encode(String(describing: self.quantity), forKey: .quantity)
+            try container.encode(String(describing: self.price), forKey: .price)
+            try container.encode(self.currency, forKey: .currency)
+            try container.encode(self.sku, forKey: .sku)
+            try container.encode(self.name, forKey: .name)
+            try container.encode(self.description, forKey: .description)
+            try container.encode(self.tax, forKey: .tax)
+        }
+        
+        
+        enum CodingKeys: String, CodingKey {
+            case quantity, price, currency, sku, name, description, tax
         }
     }
 }
