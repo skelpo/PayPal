ba0f758ad22c1217ca2d539d52091c734eb22292
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-11 10:41:10 -0600

    Removed ValidationSetable conformance from Message model

--- Sources/PayPal/Models/Message/Message.swift
+++ Sources/PayPal/Models/Message/Message.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 /// A communication between a customer and merchant of a transaction.
-public struct Message: Content, ValidationSetable, Equatable {
+public struct Message: Content, Equatable {
     
     /// Indicates whether the customer or merchant posted the message.
     public let poster: Poster?
@@ -15,43 +15,15 @@ public struct Message: Content, ValidationSetable, Equatable {
     /// validate the new value before assigning it to the property.
     ///
     /// Minimum length: 0. Maximum length: 2000.
-    public private(set) var content: String
+    public private(set) var content: Failable<String, Length2000>
     
     /// Creates a new `Message` instance.
     ///
-    ///     Message(content: "You might be a developer if you see a music album called J. S. Bach and wonder why it is not called Back.js")
-    public init(content: String)throws {
+    /// - Parameter content: The message text.
+    public init(content: Failable<String, Length2000>) {
         self.poster = nil
         self.posted = nil
         self.content = content
-        
-        try self.set(\.content <~ content)
-    }
-    
-    /// See [`Decoder.init(from:)`](https://developer.apple.com/documentation/swift/decodable/2894081-init).
-    public init(from decoder: Decoder)throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let content = try container.decode(String.self, forKey: .content)
-        
-        self.content = content
-        self.poster = try container.decodeIfPresent(Poster.self, forKey: .poster)
-        self.posted = try container.decodeIfPresent(String.self, forKey: .posted)
-        
-        try self.set(\.content <~ content)
-    }
-    
-    
-    /// See `ValidationSetable.setterValidations()`.
-    public func setterValidations() -> SetterValidations<Message> {
-        var validations = SetterValidations(Message.self)
-        
-        validations.set(\.content) { content in
-            guard content.count <= 2000 else {
-                throw PayPalError(identifier: "invalidLength", reason: "`content` property must have a length between 0 and 2000")
-            }
-        }
-        
-        return validations
     }
     
     enum CodingKeys: String, CodingKey {
