9f39400471cef0747dd3900ba78d78cd32918c42
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-11 10:45:14 -0600

    Added Validation to CustomAmount properties

--- Sources/PayPal/Models/Money/CustomAmount.swift
+++ Sources/PayPal/Models/Money/CustomAmount.swift
@@ -1,20 +1,22 @@
 import Vapor
 
 /// A monitary amount with a custom label.
-public struct CustomAmount<M>: Content, Equatable where M: Amount {
+public struct CustomAmount<Keys>: Content, Equatable where Keys: AmountCodingKey {
     
     /// The custom amount label.
     ///
     /// Maximum length: 50.
-    public var label: String?
+    public var label: Failable<String?, NotNilValidate<Length50>>
     
     /// The currency and amount of the custom value. Value is from `-1,000,000` to `1,000,000`. Supports up to two decimal places.
-    public var amount: M?
+    public var amount: Failable<AmountType<Keys>?, NotNilValidate<CurrencyMillion<Keys>>>
     
     /// Creates a new `CustomAmount` instance.
     ///
-    ///     CustomAmount<Amount>(label: "Some Text", amount: Amount(currency: .usd, value: "25.50"))
-    public init(label: String?, amount: M?) {
+    /// - Parameters:
+    ///   - label: The custom amount label.
+    ///   - amount: The currency and amount of the custom value.
+    public init(label: Failable<String?, NotNilValidate<Length50>>, amount: Failable<AmountType<Keys>?, NotNilValidate<CurrencyMillion<Keys>>>) {
         self.label = label
         self.amount = amount
     }
