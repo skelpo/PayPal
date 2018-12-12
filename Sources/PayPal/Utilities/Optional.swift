6ffe90feaa6f9e46e179cde1831404e896458ccd
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-12 14:52:02 -0600

    Made || error catching operator 'fallbask' paramter @autoclosure

--- Sources/PayPal/Utilities/Optional.swift
+++ Sources/PayPal/Utilities/Optional.swift
@@ -1,7 +1,7 @@
-func ||<T>(optional: @autoclosure ()throws -> T, fallback: T) -> T {
+func ||<T>(optional: @autoclosure ()throws -> T, fallback: @autoclosure () -> T) -> T {
     do {
         return try optional()
     } catch {
-        return fallback
+        return fallback()
     }
 }
