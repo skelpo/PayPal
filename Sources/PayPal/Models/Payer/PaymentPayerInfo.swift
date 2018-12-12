e9db150ea0d0fa2cdc792e6ebcb19a9a41491861
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-12 11:16:06 -0600

    Changed Order.Payer.Info.birthdate property type to Date

--- Sources/PayPal/Models/Payer/PaymentPayerInfo.swift
+++ Sources/PayPal/Models/Payer/PaymentPayerInfo.swift
@@ -28,8 +28,8 @@ extension Order.Payer {
         /// The payer's email address. Maximum length is 127 characters.
         public var email: Optional127String
         
-        /// The birth date of the payer, in [Internet date format](https://tools.ietf.org/html/rfc3339#section-5.6). For example, `1990-04-12`.
-        public var birthdate: String?
+        /// The birth date of the payer, in [Internet date format](https://tools.ietf.org/html/rfc3339#section-5.6).
+        public var birthdate: Date?
         
         /// The payer's tax ID. Supported for the PayPal payment method only.
         ///
@@ -57,7 +57,7 @@ extension Order.Payer {
         ///   - billing: The payer's billing address.
         public init(
             email: Optional127String,
-            birthdate: String?,
+            birthdate: Date?,
             tax: Failable<String?, NotNilValidate<Length14>>,
             taxType: TaxType?,
             country: Country?,
@@ -76,6 +76,54 @@ extension Order.Payer {
             self.taxType = taxType
             self.country = country
             self.billing = billing
+        }
+        
+        /// See [`Decodable.init(from:)`](https://developer.apple.com/documentation/swift/decodable/2894081-init).
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            
+            if
+                let string = try container.decodeIfPresent(String.self, forKey: .birthdate),
+                let date = BusinessOwner.birthdateFormatter.date(from: string)
+            {
+                self.birthdate = date
+            } else {
+                self.birthdate = nil
+            }
+            
+            self.salutation = try container.decodeIfPresent(String.self, forKey: .salutation)
+            self.firstname = try container.decodeIfPresent(String.self, forKey: .firstname)
+            self.middlename = try container.decodeIfPresent(String.self, forKey: .middlename)
+            self.lastname = try container.decodeIfPresent(String.self, forKey: .lastname)
+            self.suffix = try container.decodeIfPresent(String.self, forKey: .suffix)
+            self.payer = try container.decodeIfPresent(String.self, forKey: .payer)
+            self.email = try container.decode(Optional127String.self, forKey: .email)
+            self.tax = try container.decode(Failable<String?, NotNilValidate<Length14>>.self, forKey: .tax)
+            self.taxType = try container.decodeIfPresent(TaxType.self, forKey: .taxType)
+            self.country = try container.decodeIfPresent(Country.self, forKey: .country)
+            self.billing = try container.decodeIfPresent(Address.self, forKey: .billing)
+        }
+        
+        /// See [`Encodable.encode(to:)`](https://developer.apple.com/documentation/swift/encodable/2893603-encode).
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            
+            if let date = self.birthdate {
+                try container.encode(BusinessOwner.birthdateFormatter.string(from: date), forKey: .birthdate)
+            }
+            
+            try container.encodeIfPresent(self.salutation, forKey: .salutation)
+            try container.encodeIfPresent(self.firstname, forKey: .firstname)
+            try container.encodeIfPresent(self.middlename, forKey: .middlename)
+            try container.encodeIfPresent(self.lastname, forKey: .lastname)
+            try container.encodeIfPresent(self.suffix, forKey: .suffix)
+            try container.encodeIfPresent(self.payer, forKey: .payer)
+            try container.encode(self.email, forKey: .email)
+            try container.encode(self.tax, forKey: .tax)
+            try container.encodeIfPresent(self.taxType, forKey: .taxType)
+            try container.encodeIfPresent(self.country, forKey: .country)
+            try container.encodeIfPresent(self.billing, forKey: .billing)
+            
         }
         
         enum CodingKeys: String, CodingKey {
