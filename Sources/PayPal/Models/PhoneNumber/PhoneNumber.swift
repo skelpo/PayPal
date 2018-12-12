db77191d26ca6abe3447fdb4fba2a5636c7298c1
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-12 09:50:34 -0600

    Changed PhoneNumber and TypedPhoneNumber models to use Int properties

--- Sources/PayPal/Models/PhoneNumber/PhoneNumber.swift
+++ Sources/PayPal/Models/PhoneNumber/PhoneNumber.swift
@@ -6,21 +6,56 @@ public struct PhoneNumber: Content, Equatable {
     /// The country code portion of the phone number, in [E.164 format](https://www.itu.int/rec/T-REC-E.164-201011-I).
     ///
     /// Minimum length: 1. Maximum length: 3. Pattern: `^[0-9]{1,3}?$`.
-    public var country: Failable<String?, NotNilValidate<CountryNumber>>
+    public var country: Failable<Int?, NotNilValidate<CountryNumber>>
     
     /// The in-country phone number, in [E.164 format](https://www.itu.int/rec/T-REC-E.164-201011-I).
     ///
     /// Minimum length: 1. Maximum length: 14. Pattern: `^[0-9]{1,14}?$`.
-    public var number: Failable<String?, NotNilValidate<Number>>
+    public var number: Failable<Int?, NotNilValidate<Number>>
     
     /// Creates a new instance of `PhoneNumber`.
     ///
     /// - Parameters:
     ///   - country: The country code portion of the phone number, in E.164 format.
     ///   - number: The in-country phone number, in E.164 format.
-    public init(country: Failable<String?, NotNilValidate<CountryNumber>>, number: Failable<String?, NotNilValidate<Number>>) {
+    public init(country: Failable<Int?, NotNilValidate<CountryNumber>>, number: Failable<Int?, NotNilValidate<Number>>) {
         self.country = country
         self.number = number
+    }
+    
+    /// See [`Decodable.init(from:)`](https://developer.apple.com/documentation/swift/decodable/2894081-init).
+    public init(from decoder: Decoder)throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        if let countryStr = try container.decodeIfPresent(String.self, forKey: .country) {
+            guard let country = Int(countryStr) else {
+                throw DecodingError.dataCorruptedError(forKey: .country, in: container, debugDescription: "String must be convertible to integer")
+            }
+            self.country = try .init(country)
+        } else {
+            self.country = nil
+        }
+        
+        if let numberStr = try container.decodeIfPresent(String.self, forKey: .number) {
+            guard let number = Int(numberStr) else {
+                throw DecodingError.dataCorruptedError(forKey: .number, in: container, debugDescription: "String must be convertible to integer")
+            }
+            self.number = try .init(number)
+        } else {
+            self.number = nil
+        }
+    }
+    
+    /// See [`Encodable.encode(to:)`](https://developer.apple.com/documentation/swift/encodable/2893603-encode).
+    public func encode(to encoder: Encoder)throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        if let country = self.country.value {
+            try container.encode(String(describing: country), forKey: .country)
+        }
+        if let number = self.number.value {
+            try container.encode(String(describing: number), forKey: .number)
+        }
     }
     
     enum CodingKeys: String, CodingKey {
@@ -32,16 +67,28 @@ public struct PhoneNumber: Content, Equatable {
 extension PhoneNumber {
     
     /// The validation for the `PhoneNumber.country` property.
-    public struct CountryNumber: RegexValidation {
+    public struct CountryNumber: InRangeValidation {
         
-        /// See `RegexValidation.pattern`.
-        public static var pattern: String = "^[0-9]{1,3}?$"
+        /// See `Validation.Supported`.
+        public typealias Supported = Int
+        
+        /// The `PhoneNumber.country` value must be 999 or less.
+        public static var max: Int? = 999
+        
+        /// The `PhoneNumber.country` value must be 0 or more.
+        public static var min: Int? = 0
     }
     
     /// The validation for the `PhoneNumber.number` property.
-    public struct Number: RegexValidation {
+    public struct Number: InRangeValidation {
         
-        /// See `RegexValidation.pattern`.
-        public static var pattern: String = "^[0-9]{1,14}?$"
+        /// See `Validation.Supported`.
+        public typealias Supported = Int
+        
+        /// The `PhoneNumber.country` value must be 99,999,999,999,999 or less.
+        public static var max: Int? = 99_999_999_999_999
+        
+        /// The `PhoneNumber.country` value must be 0 or more.
+        public static var min: Int? = 0
     }
 }
--- Sources/PayPal/Models/PhoneNumber/TypedPhoneNumber.swift
+++ Sources/PayPal/Models/PhoneNumber/TypedPhoneNumber.swift
@@ -10,19 +10,19 @@ public struct TypedPhoneNumber: Content, Equatable {
     /// The combined length of the country code and the national number cannot exceed 15 digits.
     ///
     /// Minimum length: 1. Maximum length: 3. Pattern: `^[0-9]{1,3}?$`.
-    public var country: Failable<String, PhoneNumber.CountryNumber>
+    public var country: Failable<Int, PhoneNumber.CountryNumber>
     
     /// The national number, as defined by the [E.164 numbering plan](https://www.itu.int/rec/T-REC-E.164/en).
     /// The combined length of of the country code and the national number cannot exceed 15 digits.
     /// The national number consists of national destination code (NDC) and subscriber number (SN).
     ///
     /// Minimum length: 1. Maximum length: 14. Pattern: `^[0-9]{1,14}?$`.
-    public var nationalNumber: Failable<String, PhoneNumber.Number>
+    public var nationalNumber: Failable<Int, PhoneNumber.Number>
     
     /// The extension number.
     ///
     /// Minimum length: 1. Maximum length: 15. Pattern: `^[0-9]{1,15}?$`.
-    public var `extension`: Failable<String?, NotNilValidate<Extension>>
+    public var `extension`: Failable<Int?, NotNilValidate<Extension>>
     
     
     /// Creates a new `TypePhoneNumber` instance.
@@ -34,14 +34,52 @@ public struct TypedPhoneNumber: Content, Equatable {
     ///   - extension: The extension number.
     public init(
         type: PhoneType,
-        country: Failable<String, PhoneNumber.CountryNumber>,
-        nationalNumber: Failable<String, PhoneNumber.Number>,
-        `extension`: Failable<String?, NotNilValidate<Extension>>
+        country: Failable<Int, PhoneNumber.CountryNumber>,
+        nationalNumber: Failable<Int, PhoneNumber.Number>,
+        `extension`: Failable<Int?, NotNilValidate<Extension>>
     ) {
         self.type = type
         self.country = country
         self.nationalNumber = nationalNumber
         self.extension = `extension`
+    }
+    
+    /// See [`Decodable.init(from:)`](https://developer.apple.com/documentation/swift/decodable/2894081-init).
+    public init(from decoder: Decoder)throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        guard let country = try Int(container.decode(String.self, forKey: .country)) else {
+            throw DecodingError.dataCorruptedError(forKey: .country, in: container, debugDescription: "String must be convertible to integer")
+        }
+        guard let nationalNumber = try Int(container.decode(String.self, forKey: .nationalNumber)) else {
+            throw DecodingError.dataCorruptedError(forKey: .nationalNumber, in: container, debugDescription: "String must be convertible to integer")
+        }
+        
+        self.type = try container.decode(PhoneType.self, forKey: .type)
+        self.country = try country.failable()
+        self.nationalNumber = try nationalNumber.failable()
+        
+        if let `extension` = try container.decodeIfPresent(String.self, forKey: .`extension`) {
+            guard Int(`extension`) != nil else {
+                throw DecodingError.dataCorruptedError(forKey: .extension, in: container, debugDescription: "String must be convertible to integer")
+            }
+            self.extension = try .init(Int(`extension`))
+        } else {
+            self.extension = nil
+        }
+    }
+    
+    /// See [`Encodable.encode(to:)`](https://developer.apple.com/documentation/swift/encodable/2893603-encode).
+    public func encode(to encoder: Encoder)throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(self.type, forKey: .type)
+        try container.encode(String(describing: self.country.value), forKey: .country)
+        try container.encode(String(describing: self.nationalNumber.value), forKey: .nationalNumber)
+        
+        if let `extension` = self.extension.value {
+            try container.encode(String(describing: `extension`), forKey: .`extension`)
+        }
     }
     
     enum CodingKeys: String, CodingKey {
@@ -55,9 +93,15 @@ public struct TypedPhoneNumber: Content, Equatable {
 extension TypedPhoneNumber {
     
     /// The validation for the `TypedPhoneNumber.extension` property.
-    public struct Extension: RegexValidation {
+    public struct Extension: InRangeValidation {
         
-        /// See `RegexValidation.pattern`.
-        public static var pattern: String = "^[0-9]{1,15}?$"
+        /// See `Validation.Supported`.
+        public typealias Supported = Int
+        
+        /// The `TypedPhoneNumber.extension` value must be 999,999,999,999,999 or less.
+        public static var max: Int? = 999_999_999_999_999
+        
+        /// The `TypedPhoneNumber.extension` value must be 0 or more.
+        public static var min: Int? = 0
     }
 }
