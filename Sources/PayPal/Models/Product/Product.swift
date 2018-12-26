7832cdcdebaf9e2e7152e98435f52b7534c28317
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-26 12:12:44 -0600

    Created Product struct

--- Sources/PayPal/Models/Product/Product.swift
+++ Sources/PayPal/Models/Product/Product.swift
@@ -1,1 +1,31 @@
-public struct Product {}
+import Vapor
+
+/// Data that acts as a product's status that are integrated with a partner for a seller.
+public struct Product: Content {
+    
+    /// The name of the product.
+    public var name: Name?
+    
+    /// The vetting status of the product, if applicable.
+    public var vettingStatus: VettingStatus?
+    
+    /// Indicates whether the product is active.
+    public var active: Bool?
+    
+    /// Creates a new `Product` instance.
+    ///
+    /// - Parameters:
+    ///   - name: The name of the product.
+    ///   - vettingStatus: The vetting status of the product, if applicable.
+    ///   - active: Indicates whether the product is active.
+    public init(name: Name?, vettingStatus: VettingStatus?, active: Bool?) {
+        self.name = name
+        self.vettingStatus = vettingStatus
+        self.active = active
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name, active
+        case vettingStatus = "vetting_status"
+    }
+}
