669209ac469444f2f1752170d7e2bebad1db65c5
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-04 08:07:24 -0600

    Changed Template.List.emails property type to a failable String array

--- Sources/PayPal/Models/Template/TemplateList.swift
+++ Sources/PayPal/Models/Template/TemplateList.swift
@@ -13,7 +13,7 @@ extension Template {
         public var addresses: [Address]?
         
         /// An array of email addresses in the user's PayPal profile.
-        public var emails: [Email]?
+        public var emails: Failable<[String]?, NotNilValidate<ElementValidation<[String], EmailString>>>
         
         /// An array of phone numbers in the user's PayPal profile.
         public var phones: [PhoneNumber]?
@@ -26,7 +26,18 @@ extension Template {
         ///
         /// Instead of creating a `TemplateList` with this initializer, you will most likely want to create it
         /// be decoding it from a JSON response from the PayPal API.
-        public init(addresses: [Address]?, emails: [Email]?, phones: [PhoneNumber]?, templates: [Template]?) {
+        ///
+        /// - Parameters:
+        ///   - addresses: An array of postal addresses in the user's PayPal profile.
+        ///   - emails: An array of email addresses in the user's PayPal profile.
+        ///   - phones: An array of phone numbers in the user's PayPal profile.
+        ///   - temapltes: The details for each template.
+        public init(
+            addresses: [Address]?,
+            emails: Failable<[String]?, NotNilValidate<ElementValidation<[String], EmailString>>>,
+            phones: [PhoneNumber]?,
+            templates: [Template]?
+        ) {
             self.links = nil
             self.addresses = addresses
             self.emails = emails
