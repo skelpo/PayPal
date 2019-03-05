cef327cfffdb60624bb295c66718ae998d677006
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-05 15:59:46 -0600

    Documented V1 and V2 classes and API.v1 and .v2 properties

--- Sources/V1/V1.swift
+++ Sources/V1/V1.swift
@@ -1,11 +1,21 @@
 @_exported import PayPal
 
 extension API {
+    
+    /// Version 1 of the PayPal API resources.
     public var v1: V1 {
-        return V1()
+        return V1(client: self.client)
     }
 }
 
+/// A container for the controllers for version 1 of the PayPal API resources.
 public struct V1 {
     
+    /// The client used to send requests to PayPal.
+    internal let client: PayPalClient
+    
+    /// Creates a new `V1` instance with the `PayPalClient` instance it will use.
+    internal init(client: PayPalClient) {
+        self.client = client
+    }
 }
--- Sources/V2/V2.swift
+++ Sources/V2/V2.swift
@@ -1,11 +1,21 @@
 @_exported import PayPal
 
 extension API {
+    
+    /// Version 2 of the PayPal API resources.
     public var v2: V2 {
-        return V2()
+        return V2(client: self.client)
     }
 }
 
+/// A container for the controllers for version 2 of the PayPal API resources.
 public struct V2 {
     
+    /// The client used to send requests to PayPal.
+    internal let client: PayPalClient
+    
+    /// Creates a new `V2` instance with the `PayPalClient` instance it will use.
+    internal init(client: PayPalClient) {
+        self.client = client
+    }
 }
