48c447ccf06b17db9b6675895c05774ac89652ef
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-05 15:39:51 -0600

    Documented API class

--- Sources/PayPal/API/API.swift
+++ Sources/PayPal/API/API.swift
@@ -1,12 +1,23 @@
 import Vapor
 
+/// The wrapper for the whole PayPal REST API.
 public final class API {
-    let client: PayPalClient
     
+    /// The client used to send requests to PayPal.
+    public let client: PayPalClient
+    
+    /// Creates a new `API` instance with the dependencies for the `PayPalClient` instance.
+    ///
+    /// - Parameters:
+    ///   - client: The `Client` instance that the `PayPalClient` instance uses to send requests.
+    ///   - environment: The PayPal environment to send requests to.
     public init(client: Client, environment: Environment) {
         self.client = PayPalClient(client: client, env: environment)
     }
     
+    /// Creates a new `API` instance with the `PayPalClient` instance to use.
+    ///
+    /// - Parameter client: The `PayPalClient` instance that will be used to send requests to the PayPal API.
     public init(client: PayPalClient) {
         self.client = client
     }
