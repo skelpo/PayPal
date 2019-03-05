79dd35534c5c6e45b0a462e921e0228b38f319d6
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-05 16:27:20 -0600

    Created Payments controller class

---
+++ Sources/V2/Controllers/Payments.swift
@@ -0,0 +1,28 @@
+import PayPal
+
+/// Call the Payments API to authorize payments, capture authorized payments, refund payments that have already been captured,
+/// and show payment information. Use the Payments API in conjunction with the
+/// [Orders API](https://developer.paypal.com/docs/api/orders/v2/). For more information,
+/// see the [PayPal Checkout Overview](https://developer.paypal.com/docs/checkout/).
+///
+/// # Authorizations
+///
+/// Use the `/authorizations` resource to show details for, capture payment for, reauthorize, and void authorized payments.
+///
+/// # Captures
+///
+/// Use the `/captures` resource to show details for and refund a captured payment.
+///
+/// # Refunds
+///
+/// Use the `/refunds` resource to show refund details.
+public final class Payments: VersionedController {
+    
+    /// See `VersionedController.client`
+    public let client: PayPalClient
+    
+    /// See `VersionedController.init(client:)`.
+    public init(client: PayPalClient) {
+        self.client = client
+    }
+}
