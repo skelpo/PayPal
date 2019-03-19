e6ee3fbd3b889e9fe896855fcc7f38b3f5e754e5
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-19 07:32:42 -0500

    Created SellerProtection struct

--- Sources/V2/Models/Payment/Seller/SellerProtection.swift
+++ Sources/V2/Models/Payment/Seller/SellerProtection.swift
@@ -1,5 +1,30 @@
+/// A transcation's protection level.
+///
+/// This will be defined for a transaction by the
+/// [PayPal Seller Protection for Merchants](https://www.paypal.com/us/webapps/mpp/security/seller-protection).
 public struct SellerProtection: Codable {
     
+    /// Indicates whether the transaction is eligible for seller protection. For information,
+    /// see [PayPal Seller Protection for Merchants](https://www.paypal.com/us/webapps/mpp/security/seller-protection).
+    public let status: Status?
+    
+    /// An array of conditions that are covered for the transaction.
+    public let disputes: [DisputeCategories]?
+    
+    /// Creates a new `SellerProtection` instance
+    ///
+    /// - Parameters:
+    ///   - status: Indicates whether the transaction is eligible for seller protection.
+    ///   - disputes: An array of conditions that are covered for the transaction.
+    public init(status: Status?, disputes: [DisputeCategories]?) {
+        self.status = status
+        self.disputes = disputes
+    }
+    
+    internal enum CodingKeys: String, CodingKey {
+        case status
+        case disputes = "dispute_categories"
+    }
 }
 
 extension SellerProtection {
