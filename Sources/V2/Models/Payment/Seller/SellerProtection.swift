962af48fc772924c7b9b19b2b558dea3c95f4d5f
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-19 07:27:07 -0500

    Created SellerProtection.DisputeCategories enum

--- Sources/V2/Models/Payment/Seller/SellerProtection.swift
+++ Sources/V2/Models/Payment/Seller/SellerProtection.swift
@@ -1,3 +1,15 @@
 public struct SellerProtection: Codable {
     
 }
+
+extension SellerProtection {
+    
+    /// The dispute conditions that can be covered by a transaction.
+    public enum DisputeCategories: String, Hashable, Codable, CaseIterable {
+        /// The payer paid for an item that he or she did not receive.
+        case itemNotReceived = "ITEM_NOT_RECEIVED"
+        
+        /// The payer did not authorize the payment.
+        case unauthorizedTransaction = "UNAUTHORIZED_TRANSACTION"
+    }
+}
