ddada1712c11af2d47202223faecc9ba3da2edeb
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-28 13:49:46 -0500

    Created Payments.Authorizations.reauthorize(_:amount:prefer:requestID:) method. Fixed .capture endpoint called

--- Sources/V2/Controllers/Payments/Authorizations.swift
+++ Sources/V2/Controllers/Payments/Authorizations.swift
@@ -52,7 +52,53 @@ extension Payments {
                 headers.add(name: "PayPal-Request-Id", value: request)
             }
             
-            return self.client.post(self.path + authorization, headers: headers, body: capture, as: Capture.self)
+            return self.client.post(self.path + authorization + "/capture", headers: headers, body: capture, as: Capture.self)
+        }
+        
+        /// Reauthorizes an authorized PayPal account payment, by ID. To ensure that funds are still available,
+        /// reauthorize a payment after its initial three-day honor period expires.
+        /// You can reauthorize a payment only once from days four to 29.
+        ///
+        /// If 30 days have transpired since the date of the original authorization,
+        /// you must create an authorized payment instead of reauthorizing the original authorized payment.
+        ///
+        /// A reauthorized payment itself has a new honor period of three days.
+        ///
+        /// You can reauthorize an authorized payment once for up to 115% of the original authorized amount,
+        /// not to exceed an increase of $75 USD.
+        ///
+        /// Supports only the `amount` request parameter.
+        ///
+        /// - Note: This request is currently not supported for Partner use cases.
+        ///
+        /// A successful request returns the HTTP 201 Created status code and
+        /// a JSON response body that shows the reauthorized payment details.
+        ///
+        /// - Parameters:
+        ///   - authorization: The PayPal-generated ID for the authorized payment to reauthorize.
+        ///   - amount: The amount to reauthorize for an authorized payment.
+        ///   - prefer: The preferred server response upon successful completion of the request.
+        ///   - requestID: A unique user-generated ID that the server stores for a period of time.
+        ///     Use this header to enforce idempotency on REST API POST calls.
+        ///
+        /// - Returns: The reauthorizaed payment, wrapped in an `EventLoopFuture`.
+        public func reauthorize(
+            _ authorization: String,
+            amount: CurrencyCodeAmount,
+            prefer: ResponseReturn = .minimal,
+            requestID: String? = nil
+        ) -> EventLoopFuture<Authorization> {
+            var headers: HTTPHeaders = ["Prefer": prefer.rawValue]
+            if let request = requestID {
+                headers.add(name: "PayPal-Request-Id", value: request)
+            }
+            
+            return self.client.post(
+                self.path + authorization + "/reauthorize",
+                headers: headers,
+                body: amount,
+                as: Authorization.self
+            )
         }
         
         /// The preferred server response upon successful completion of a request.
