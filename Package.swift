16fa191ae4e5b2ea2825ddf4c691c83197c58ff9
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-28 07:49:46 -0500

    Update all dependencies to latest versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
@@ -8,10 +8,10 @@ let package = Package(
         .library(name: "PayPal", targets: ["PayPal"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/skelpo/Countries.git", from: "0.9.1"),
-        .package(url: "https://github.com/skelpo/Failable.git", from: "0.2.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
-        .package(url: "https://github.com/skelpo/JSON.git", from: "0.13.1"),
+        .package(url: "https://github.com/skelpo/Countries.git", from: "1.0.0"),
+        .package(url: "https://github.com/skelpo/Failable.git", from: "0.4.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
+        .package(url: "https://github.com/skelpo/JSON.git", from: "1.0.0"),
         .package(url: "https://github.com/twostraws/SwiftGD.git", from: "2.2.0")
     ],
     targets: [
