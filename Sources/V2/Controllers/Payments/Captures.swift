eef85ae6f3296cbf8ec955c16ee6a173c1332345
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-03-28 14:15:43 -0500

    Created Payments.Captures.get(_:) method

--- Sources/V2/Controllers/Payments/Captures.swift
+++ Sources/V2/Controllers/Payments/Captures.swift
@@ -16,5 +16,16 @@ extension Payments {
             self.client = client
             self.resource = ["payments", "captures"]
         }
+        
+        /// Shows details for a captured payment, by ID.
+        ///
+        /// A successful request returns the HTTP 200 OK status code and
+        /// a JSON response body that shows captured payment details.
+        ///
+        /// - Parameter capture: The PayPal-generated ID for the captured payment for which to show details.
+        /// - Returns: The captured payment for the ID, wrapped in an `EventLoopFuture`.
+        public func get(_ capture: String) -> EventLoopFuture<Capture> {
+            return self.client.get(self.path + capture, as: Capture.self)
+        }
     }
 }
