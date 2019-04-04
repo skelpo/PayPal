1967834dd397a12a9f939761ebee5ea1bb0b217e
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-04 10:37:11 -0500

    Created Webhook struct

---
+++ Sources/V1/Models/Webhook/Webhook.swift
@@ -0,0 +1,38 @@
+import PayPal
+
+/// The information for a webhook that will be fired by a PayPal action.
+public struct Webhook: Codable {
+    
+    /// The ID of the webhook.
+    public let id: String?
+    
+    /// An array of request-related [HATEOAS links](https://developer.paypal.com/docs/api/hateoas-links/).
+    public let links: [LinkDescription]?
+    
+    /// The URL that is configured to listen on `localhost` for incoming `POST` notification messages
+    /// that contain event information.
+    public var url: String
+    
+    /// An array of events to which to subscribe your webhook. To subscribe to all events, including events as they are added,
+    /// specify the asterisk wild card. To replace the `event_types` array, specify the asterisk wild card.  To list
+    /// all supported events, [list available events](https://developer.paypal.com/docs/api/webhooks/v1/#event-type_list).
+    public var events: [EventType]
+    
+    /// Creates a new `Webhook` instance.
+    ///
+    /// - Parameters:
+    ///   - url: The URL that is configured to listen on `localhost` for incoming `POST` notification messages.
+    ///   - events: An array of events to which to subscribe your webhook.
+    public init(url: String, events: [EventType]) {
+        self.id = nil
+        self.links = nil
+        
+        self.url = url
+        self.events = events
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, links, url
+        case events = "event_types"
+    }
+}
