33cb4ed992630231ad35f9e0e76c4bcc93888d7e
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-04 11:47:37 -0500

    Created Webhooks.get(by:) method

--- Sources/V1/Controllers/Webhooks/Webhooks.swift
+++ Sources/V1/Controllers/Webhooks/Webhooks.swift
@@ -32,4 +32,16 @@ public final class Webhooks: VersionedController {
     public func create(webhook: Webhook) -> EventLoopFuture<Webhook> {
         return self.client.post(self.path, body: webhook, as: Webhook.self)
     }
+    
+    /// Lists all webhooks for an app.
+    ///
+    /// A successful request returns the HTTP 200 OK status code and a JSON response body that lists
+    /// webhooks with webhook details.
+    ///
+    /// - Parameter anchor: Filters the webhooks in the response by the `anchor_id` entity type.
+    /// - Returns: The webhooks that match the given `anchor` value, wrapped in an `EventLoopFuture`.
+    public func get(by anchor: Webhook.AnchorType = .application) -> EventLoopFuture<[Webhook]> {
+        let parameters = QueryParamaters(custom: ["anchor_type": anchor.rawValue])
+        return self.client.get(self.path, parameters: parameters, as: [Webhook].self)
+    }
 }
