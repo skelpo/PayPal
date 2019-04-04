6702d1e69bf6d5546a562c4fb7a639fbd2dd639b
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-04 16:27:32 -0500

    Created Webhooks.Events.list(parameters:transaction:type:) method

--- Sources/V1/Controllers/Webhooks/Events.swift
+++ Sources/V1/Controllers/Webhooks/Events.swift
@@ -17,6 +17,33 @@ extension Webhooks {
             self.client = client
             self.resource = ["notifications", "webhooks-events"]
         }
+        
+        /// Lists webhook event notifications. Use query parameters to filter the response.
+        ///
+        /// A successful request returns the HTTP 200 OK status code and a JSON response body that
+        /// lists webhook event notifications.
+        ///
+        /// - Parameters:
+        ///   - parameters:
+        ///   - transaction: Filters the response to a single transaction, by ID.
+        ///   - type: Filters the response to a single event.
+        ///
+        /// - Returns: The list of `Event` objects, along with count and related links.
+        public func list(
+            parameters: QueryParamaters = QueryParamaters(),
+            transaction: String? = nil,
+            type: String? = nil
+        ) -> EventLoopFuture<Event.List> {
+            var parameters = parameters
+            if let transaction = transaction {
+                parameters.custom?["transaction_id"] = transaction
+            }
+            if let eventType = type {
+                parameters.custom?["event_type"] = eventType
+            }
+            
+            return self.client.get(self.path, parameters: parameters, as: Event.List.self)
+        }
     }
 }
 
