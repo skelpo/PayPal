1af80fcff3afe6f77dbd9b9a7ab8805d554c7e81
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-04 11:46:32 -0500

    Created Wwbhooks controller type

---
+++ Sources/V1/Controllers/Webhooks/Webhooks.swift
@@ -0,0 +1,25 @@
+import PayPal
+import Vapor
+
+/// The PayPal REST APIs use [webhooks](https://developer.paypal.com/docs/integration/direct/webhooks) for event notification.
+/// Webhooks are HTTP callbacks that receive notification messages for events. After you configure a webhook listener for your
+/// app, you can [create a webhook](https://developer.paypal.com/docs/api/webhooks/v1/#webhooks_create), which subscribes
+/// the webhook listener for your app to events. The `notifications` namespace contains resource collections for webhooks.
+///
+/// # Webhooks
+///
+/// Use the `/webhooks` resource to create, show details for, list all, update, and delete webhooks.
+public final class Webhooks: VersionedController {
+    
+    /// See `VersionedController.client`
+    public let client: PayPalClient
+    
+    /// See `VersionedController.resource`.
+    public let resource: [String]
+    
+    /// See `VersionedController.init(client:)`.
+    public init(client: PayPalClient) {
+        self.client = client
+        self.resource = ["notifications", "webhooks"]
+    }
+}
