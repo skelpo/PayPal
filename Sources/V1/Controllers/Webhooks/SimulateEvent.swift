f41babcc54457b35b86c4d384572718e9e42eb0a
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-04-05 06:31:56 -0500

    Created Webhooks.SimulateEvents.simulate(event:) method

--- Sources/V1/Controllers/Webhooks/SimulateEvent.swift
+++ Sources/V1/Controllers/Webhooks/SimulateEvent.swift
@@ -1,4 +1,5 @@
 import PayPal
+import Vapor
 
 extension Webhooks {
     
@@ -15,6 +16,17 @@ extension Webhooks {
         public init(client: PayPalClient) {
             self.client = client
             self.resource = ["notifications", "simulate-event"]
+        }
+        
+        /// Simulates a webhook event. In the JSON request body, specify a sample payload.
+        ///
+        /// A successful request returns the HTTP 202 Accepted status code and a JSON response body that
+        /// shows details for the mock event.
+        ///
+        /// - Parameter event: The webhook event to simulate. This is the body of the request to PayPal.
+        /// - Returns: The mocked `Event` object, wrapped in an `EventLoopFuture`.
+        public func simulate(event: Event.Simulated) -> EventLoopFuture<Event> {
+            return self.client.post(self.path, body: event, as: Event.self)
         }
     }
 }
