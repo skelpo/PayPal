02fbcdbe39a985180cb5896332d46a32eea5d711
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-17 07:27:18 -0500

    Added response proeprties to vault CreditCard type

--- Sources/V1/Models/Vault/CreditCard.swift
+++ Sources/V1/Models/Vault/CreditCard.swift
@@ -3,7 +3,10 @@ import PayPal
 
 /// The credit card data sotred in PayPal Vault.
 public struct CreditCard: Codable {
-    
+
+    /// The ID of the vaulted credit card, which you can use to fund a payment.
+    public let id: String?
+
     /// The credit card number. Value is a string of numeric characters with no spaces or punctuation.
     /// Must conform to modulo and the required length for the credit card type. Redacted in responses.
     public var number: Failable<String, Number>
@@ -32,7 +35,25 @@ public struct CreditCard: Codable {
     
     /// The externally-provided ID of the credit card.
     public var externalCard: Failable<String?, NotNilValidate<Length256>>
-    
+
+    /// The state of the credit card funding instrument.
+    public let state: State?
+
+    /// The date and time when the vaulted credit card was created.
+    public let created: ISO8601Date?
+
+    /// The date and time when the vaulted credit card was updated.
+    public let updated: ISO8601Date?
+
+    /// The date and time when the credit card becomes unusable from the vault. The `valid_until` parameter is not the same
+    /// as the expiration month and year. The expiration month and year might be later than the `valid_until` date.
+    /// For example, the card expires in November 2019 but the `valid_until` date is October 17th, 2019.
+    public let valueTo: ISO8601Date?
+
+    /// An array of request-related
+    /// [HATEOAS links](https://developer.paypal.com/docs/api/reference/api-responses/#hateoas-links).
+    public let links: [LinkDescription]?
+
     /// Creates a new `CreditCard` instance.
     ///
     /// - Parameters:
@@ -58,7 +79,14 @@ public struct CreditCard: Codable {
     ) {
         var calendar = Calendar(identifier: .iso8601)
         calendar.timeZone = TimeZone(secondsFromGMT: 0) ?? calendar.timeZone
-        
+
+        self.id = nil
+        self.state = nil
+        self.created = nil
+        self.updated = nil
+        self.valueTo = nil
+        self.links = nil
+
         self.number = number
         self.type = type
         self.expiry = calendar.date(from: calendar.dateComponents([.year, .month], from: expiry)) ?? expiry
@@ -90,6 +118,7 @@ public struct CreditCard: Codable {
         }
         
         self.expiry = expiry
+        self.id = try container.decodeIfPresent(String.self, forKey: .id)
         self.number = try container.decode(Failable<String, Number>.self, forKey: .number)
         self.type = try container.decode(CreditCardType.self, forKey: .type)
         self.firstName = try container.decode(Failable<String?, NotNilValidate<FirstName>>.self, forKey: .firstName)
@@ -98,6 +127,11 @@ public struct CreditCard: Codable {
         self.externalCustomer = try container.decode(Failable<String?, NotNilValidate<Length256>>.self, forKey: .externalCustomer)
         self.merchant = try container.decode(Failable<String?, NotNilValidate<Length256>>.self, forKey: .merchant)
         self.externalCard = try container.decode(Failable<String?, NotNilValidate<Length256>>.self, forKey: .externalCard)
+        self.state = try container.decodeIfPresent(State.self, forKey: .state)
+        self.created = try container.decodeIfPresent(ISO8601Date.self, forKey: .created)
+        self.updated = try container.decodeIfPresent(ISO8601Date.self, forKey: .updated)
+        self.valueTo = try container.decodeIfPresent(ISO8601Date.self, forKey: .validTo)
+        self.links = try container.decodeIfPresent([LinkDescription].self, forKey: .links)
     }
     
     /// See [`Encodable.encode(to:)`](https://developer.apple.com/documentation/swift/encodable/2893603-encode).
@@ -108,7 +142,8 @@ public struct CreditCard: Codable {
         let components = calendar.dateComponents([.year, .month], from: self.expiry)
         try container.encodeIfPresent(components.year.map(String.init(describing:)), forKey: .expiryYear)
         try container.encodeIfPresent(components.month.map(String.init(describing:)), forKey: .expiryMonth)
-        
+
+        try container.encodeIfPresent(self.id, forKey: .id)
         try container.encode(self.number, forKey: .number)
         try container.encode(self.type, forKey: .type)
         try container.encode(self.firstName, forKey: .firstName)
@@ -117,7 +152,11 @@ public struct CreditCard: Codable {
         try container.encode(self.externalCustomer, forKey: .externalCustomer)
         try container.encode(self.merchant, forKey: .merchant)
         try container.encode(self.externalCard, forKey: .externalCard)
-        
+        try container.encodeIfPresent(self.state, forKey: .state)
+        try container.encodeIfPresent(self.created, forKey: .created)
+        try container.encodeIfPresent(self.updated, forKey: .updated)
+        try container.encodeIfPresent(self.valueTo, forKey: .validTo)
+        try container.encodeIfPresent(self.links, forKey: .links)
     }
     
     /// The validator for the `CardCard.number` property value.
@@ -133,9 +172,19 @@ public struct CreditCard: Codable {
         /// See `StringLengthValidation.maxLength`.
         public static let maxLength: Int = 25
     }
-    
+
+    /// The states of a credit card funding instrument.
+    public enum State: String, Hashable, CaseIterable, Codable {
+
+        /// The credit card is past it's expiration date.
+        case expired
+
+        /// The creadit card has not expired yet.
+        case ok
+    }
+
     enum CodingKeys: String, CodingKey {
-        case number, type
+        case id, number, type, state, links
         case expiryMonth = "expire_month"
         case expiryYear = "expire_year"
         case firstName = "first_name"
@@ -144,6 +193,9 @@ public struct CreditCard: Codable {
         case externalCustomer = "external_customer_id"
         case merchant = "merchant_id"
         case externalCard = "external_card_id"
+        case created = "create_time"
+        case updated = "update_time"
+        case validTo = "valid_until"
     }
 }
 
