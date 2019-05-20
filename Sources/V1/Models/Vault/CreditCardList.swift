78542449ba8ccf004c9446a190b15d4ed55edfa8
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-20 14:01:37 -0500

    Fixed CrediCard.List type

--- Sources/V1/Models/Vault/CreditCardList.swift
+++ Sources/V1/Models/Vault/CreditCardList.swift
@@ -6,17 +6,17 @@ extension CreditCard {
     public struct List: Codable {
 
         /// An array of credit card objects.
-        public var cards: [CreditCard]
+        public var cards: [CreditCard]?
 
         /// The total number of items.
-        public var count: Int32
+        public var count: Int32?
 
         /// The total number of pages.
-        public var pages: Int32
+        public var pages: Int32?
 
         /// An array of request-specific
         /// [HATEOAS links](https://developer.paypal.com/docs/api/reference/api-responses/#hateoas-links).
-        public let links: [LinkDescription]
+        public let links: [LinkDescription]?
 
         /// Creates a new `CreditCard.List` struct.
         ///
@@ -24,13 +24,14 @@ extension CreditCard {
         ///   - cards: An array of credit card objects.
         ///   - count: The total number of items.
         ///   - pages: The total number of pages.
-        public init(cards: [CreditCard], count: Int32, pages: Int32) {
+        public init(cards: [CreditCard]?, count: Int32?, pages: Int32?) {
             self.cards = cards
             self.count = count
             self.pages = pages
+            self.links = nil
         }
 
-        enum CodingKeys: String, CodingKeys {
+        enum CodingKeys: String, CodingKey {
             case links
             case cards = "items"
             case count = "total_items"
