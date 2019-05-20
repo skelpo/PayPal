efacd9fc96cf4dcd487f969a70f26066c694ed96
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-05-20 12:36:15 -0500

    Created Vault.CreditCards.store(_:) method

--- Sources/V1/Controllers/Vault/Vault.swift
+++ Sources/V1/Controllers/Vault/Vault.swift
@@ -52,5 +52,21 @@ extension Valut {
             self.client = client
             self.resource = ["vault", "credit-cards"]
         }
+
+        /// Stores credit card details in the PayPal vault. To use the vaulted card to make a payment, specify this ID as the
+        /// `credit_card_id` in a
+        /// [`credit_card_token`](https://developer.paypal.com/docs/api/payments/#definition-credit_card_token) object.
+        /// If you include a payer_id when you store the credit card, you must also include that ID as the
+        /// `external_customer_id` in the `credit_card_token` object. To show details for, update, or delete the vaulted card,
+        /// use the ID of the vaulted credit card.
+        ///
+        /// A successful request returns the HTTP 201 Created status code and a JSON response body that shows credit card
+        /// details, including the ID of the vaulted card.
+        ///
+        /// - Parameter card: The credit card information to store in the customer's vault.
+        /// - Returns: The stored credit cardm wrapped in an `EventLoopFuture`.
+        public func store(_ card: CreditCard) -> EventLoopFuture<CreditCard> {
+            return self.client.post(self.path, body: card, as: CreditCard.self)
+        }
     }
 }
